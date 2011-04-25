c9fb8626de84484c9c030be7686562f95afa230f
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2011-04-25 11:44:55 +0000

    Debug = false
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@11 dcdbb401-323c-49cf-88a0-803402807c85

--- BootLoader/src/es/gob/afirma/install/AfirmaBootLoader.java
+++ BootLoader/src/es/gob/afirma/install/AfirmaBootLoader.java
@@ -20,7 +20,7 @@ import es.gob.afirma.misc.Platform;
  */
 public final class AfirmaBootLoader extends JApplet {
 	
-	public static final boolean DEBUG = true;
+	public static final boolean DEBUG = false;
 
 	private static final long serialVersionUID = -2570412953683244702L;
 
