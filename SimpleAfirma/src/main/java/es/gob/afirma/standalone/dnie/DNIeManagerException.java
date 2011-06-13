021a9b5a4f23094e430290d08c8986ec0ef04f57
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2011-06-13 10:20:48 +0000

    Se agrega la cabecera actualizada con la información de licencia del Cliente.
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@87 dcdbb401-323c-49cf-88a0-803402807c85

--- SimpleAfirma/src/main/java/es/gob/afirma/standalone/dnie/DNIeManagerException.java
+++ SimpleAfirma/src/main/java/es/gob/afirma/standalone/dnie/DNIeManagerException.java
@@ -1,3 +1,13 @@
+/*
+ * Este fichero forma parte del Cliente @firma. 
+ * El Cliente @firma es un aplicativo de libre distribucion cuyo codigo fuente puede ser consultado
+ * y descargado desde www.ctt.map.es.
+ * Copyright 2009,2010,2011 Gobierno de Espana
+ * Este fichero se distribuye bajo licencia GPL version 3 segun las
+ * condiciones que figuran en el fichero 'licence' que se acompana. Si se distribuyera este 
+ * fichero individualmente, deben incluirse aqui las condiciones expresadas alli.
+ */
+
 package es.gob.afirma.standalone.dnie;
 
 /** Excepci&oacute;n en la gesti&oacute;n de almac&eacute;n DNIe v&iacute;a PKCS#11 y JSR-268.
