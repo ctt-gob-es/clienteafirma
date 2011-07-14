0485b7e1b1de22318976f8f84f5e07e151a8e569
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2011-07-14 10:12:46 +0000

    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@225 dcdbb401-323c-49cf-88a0-803402807c85

---
+++ BootLoader/src/main/java/es/gob/afirma/install/package-info.java
@@ -0,0 +1,14 @@
+/*
+ * Este fichero forma parte del Cliente @firma. 
+ * El Cliente @firma es un aplicativo de libre distribucion cuyo codigo fuente puede ser consultado
+ * y descargado desde www.ctt.map.es.
+ * Copyright 2009,2010,2011 Gobierno de Espana
+ * Este fichero se distribuye bajo  bajo licencia GPL version 2 segun las
+ * condiciones que figuran en el fichero 'licence' que se acompana. Si se distribuyera este 
+ * fichero individualmente, deben incluirse aqui las condiciones expresadas alli.
+ */
+
+/**
+ * Clases de uso general en la instalaci&oacute;n de las dependencias del Cliente @firma. 
+ */
+package es.gob.afirma.install;
