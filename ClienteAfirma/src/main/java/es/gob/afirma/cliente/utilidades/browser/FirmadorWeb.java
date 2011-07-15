7851ce150734d537d285d456d682d6d2fde016af
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2011-07-15 06:56:39 +0000

    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@231 dcdbb401-323c-49cf-88a0-803402807c85

--- ClienteAfirma/src/main/java/es/gob/afirma/cliente/utilidades/browser/FirmadorWeb.java
+++ ClienteAfirma/src/main/java/es/gob/afirma/cliente/utilidades/browser/FirmadorWeb.java
@@ -3,7 +3,7 @@
  * El Cliente @firma es un aplicativo de libre distribucion cuyo codigo fuente puede ser consultado
  * y descargado desde www.ctt.map.es.
  * Copyright 2009,2010,2011 Gobierno de Espana
- * Este fichero se distribuye bajo licencia GPL version 3 segun las
+ * Este fichero se distribuye bajo  bajo licencia GPL version 2  segun las
  * condiciones que figuran en el fichero 'licence' que se acompana. Si se distribuyera este
  * fichero individualmente, deben incluirse aqui las condiciones expresadas alli.
  */
--- ClienteAfirma/src/main/java/es/gob/afirma/signers/aobinarysignhelper/EncryptedKeyDatas.java
+++ ClienteAfirma/src/main/java/es/gob/afirma/signers/aobinarysignhelper/EncryptedKeyDatas.java
@@ -1,3 +1,12 @@
+/*
+ * Este fichero forma parte del Cliente @firma.
+ * El Cliente @firma es un aplicativo de libre distribucion cuyo codigo fuente puede ser consultado
+ * y descargado desde www.ctt.map.es.
+ * Copyright 2009,2010,2011 Gobierno de Espana
+ * Este fichero se distribuye bajo  bajo licencia GPL version 2  segun las
+ * condiciones que figuran en el fichero 'licence' que se acompana. Si se distribuyera este
+ * fichero individualmente, deben incluirse aqui las condiciones expresadas alli.
+ */
 package es.gob.afirma.signers.aobinarysignhelper;
 
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
