2ffd2a51469f75ba1d61163e2990e086e0234b37
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2011-07-29 10:36:53 +0000

    Se actualizan los textos del método changeLanguage(String locale)
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@270 dcdbb401-323c-49cf-88a0-803402807c85

--- ClienteAfirma/src/main/java/es/gob/afirma/EntryPointsUtil.java
+++ ClienteAfirma/src/main/java/es/gob/afirma/EntryPointsUtil.java
@@ -227,13 +227,13 @@ public interface EntryPointsUtil {
      *         Complete route for the selected directory. */
     String selectDirectory();
 
-    /** Cambia el idioma del actual del Cliente. <br>
+    /** Cambia el idioma actual del Cliente. <br>
      * <br>
      * Change the current language.
      * @param locale
-     *        C&oacute;digo de idioma (por ejemplo: en, es, it...). <br>
-     *        Language code (p.e. en, es, it...) */
-    void changeLanguage(String locale);
+     *        C&oacute;digo de idioma (por ejemplo: en, en_GB, es, it...). <br>
+     *        Language code (p.e. en, en_GB, es, it...) */
+    void changeLanguage(String locale); 
 
     // /**
     // * Recupera un fragmento de datos en base 64.
