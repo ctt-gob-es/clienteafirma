66db55525d2f59d263dcfa65d224ec7dcd0abaaf
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2011-08-22 11:34:33 +0000

    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@291 dcdbb401-323c-49cf-88a0-803402807c85

--- CAdES/src/test/java/es/gob/afirma/test/cades/TestCAdES.java
+++ CAdES/src/test/java/es/gob/afirma/test/cades/TestCAdES.java
@@ -26,6 +26,18 @@ import es.gob.afirma.signers.cades.AOCAdESSigner;
  */
 public final class TestCAdES {
     
+    private static final String CERT_PATH = "ANF_PF_Activo.pfx"; //$NON-NLS-1$
+    private static final String CERT_PASS = "12341234"; //$NON-NLS-1$
+    private static final String CERT_ALIAS = "anf usuario activo"; //$NON-NLS-1$
+
+    private static final String CERT_PATH2 = "CATCERT GENCAT SAFP PF Identidad y Firma Reconocida de Clase 1 Caducado.pfx"; //$NON-NLS-1$
+    private static final String CERT_PASS2 = "1234"; //$NON-NLS-1$
+    private static final String CERT_ALIAS2 = "{71e526c4-0f27-4f32-8be0-90df52dcbc53}"; //$NON-NLS-1$
+    
+    private static final String CERT_PATH3 = "CAMERFIRMA_PF_SW_Clave_usuario_Activo.p12"; //$NON-NLS-1$
+    private static final String CERT_PASS3 = "1111"; //$NON-NLS-1$
+    private static final String CERT_ALIAS3 = "1"; //$NON-NLS-1$
+    
     private static final byte[] DATA = "usfusgfusgduifgsifiusdgfigsdiufgsid".getBytes(); //$NON-NLS-1$
     private static final Properties[] CADES_MODES;
     
@@ -64,9 +76,9 @@ public final class TestCAdES {
         final X509Certificate cert;
         try {
             KeyStore ks = KeyStore.getInstance("PKCS12"); //$NON-NLS-1$
-            ks.load(ClassLoader.getSystemResourceAsStream("ANF_PF_Activo.pfx"), "12341234".toCharArray()); //$NON-NLS-1$ //$NON-NLS-2$
-            pke = (PrivateKeyEntry) ks.getEntry("anf usuario activo", new KeyStore.PasswordProtection("12341234".toCharArray())); //$NON-NLS-1$ //$NON-NLS-2$
-            cert = (X509Certificate) ks.getCertificate("anf usuario activo");
+            ks.load(ClassLoader.getSystemResourceAsStream(CERT_PATH), CERT_PASS.toCharArray());
+            pke = (PrivateKeyEntry) ks.getEntry(CERT_ALIAS, new KeyStore.PasswordProtection(CERT_PASS.toCharArray()));
+            cert = (X509Certificate) ks.getCertificate(CERT_ALIAS);
         }
         catch(Exception e) {
             Assert.fail("No se ha podido obtener las claves de firma: " + e); //$NON-NLS-1$
@@ -124,5 +136,134 @@ public final class TestCAdES {
         }
     }
     
+    /**
+     * Prueba de cofirma.
+     */
+    @Test
+    public void testCoSignature() {
+        
+        Logger.getLogger("es.gob.afirma").setLevel(Level.WARNING); //$NON-NLS-1$
+        final PrivateKeyEntry pke1 = loadKeyEntry(CERT_PATH, CERT_ALIAS, CERT_PASS);
+        final PrivateKeyEntry pke2 = loadKeyEntry(CERT_PATH2, CERT_ALIAS2, CERT_PASS2);
+        final PrivateKeyEntry pke3 = loadKeyEntry(CERT_PATH3, CERT_ALIAS3, CERT_PASS3);
+        
+        AOSigner signer = new AOCAdESSigner();
+        
+        String prueba;
+        
+        for (final Properties extraParams : CADES_MODES) {
+            for (final String algo : ALGOS) {
+                
+                prueba = "Firma CAdES en modo '" +  //$NON-NLS-1$
+                extraParams.getProperty("mode") +  //$NON-NLS-1$
+                "' con el algoritmo ': " + //$NON-NLS-1$
+                algo +
+                "'"; //$NON-NLS-1$
+                
+                // Firma simple
+                byte[] sign1 = sign(signer, DATA, algo, pke1, extraParams);
+                
+                // Cofirma sin indicar los datos
+                byte[] sign2 = cosign(signer, sign1, algo, pke2, extraParams);
+                
+                Assert.assertNotNull(prueba, sign2);
+                Assert.assertTrue(signer.isSign(sign2));
+                Assert.assertTrue(AOCAdESSigner.isCADESValid(sign2, AOSignConstants.CMS_CONTENTTYPE_SIGNEDDATA));
+                
+                AOTreeModel tree = signer.getSignersStructure(sign2, false);
+                Assert.assertEquals("Datos", ((AOTreeNode) tree.getRoot()).getUserObject());
+                Assert.assertEquals("ANF Usuario Activo", ((AOTreeNode) tree.getRoot()).getChildAt(0).getUserObject());
+                
+                tree = signer.getSignersStructure(sign2, true);
+                Assert.assertEquals("Datos", ((AOTreeNode) tree.getRoot()).getUserObject());
+                AOSimpleSignInfo simpleSignInfo = (AOSimpleSignInfo) ((AOTreeNode) tree.getRoot()).getChildAt(0).getUserObject();
+                
+//                Assert.assertEquals("CAdES", simpleSignInfo.getSignFormat());
+//                Assert.assertEquals(algo, simpleSignInfo.getSignAlgorithm());
+                Assert.assertNotNull(simpleSignInfo.getSigningTime());
+                
+//                System.out.println("Certificado firmante:\n" + pke2.getCertificate());
+//                System.out.println("-------------");
+//                System.out.println("Certificado extraido:\n" + simpleSignInfo.getCerts()[0]);
+                
+                //Assert.assertEquals(pke2.getCertificate(), simpleSignInfo.getCerts()[0]);    
+                
+                
+                // Cofirma indicando los datos
+                byte[] sign3 = cosign(signer, DATA, sign2, algo, pke3, extraParams);
+                
+                
+                Assert.assertNotNull(prueba, sign3);
+                Assert.assertTrue(signer.isSign(sign3));
+                Assert.assertTrue(AOCAdESSigner.isCADESValid(sign3, AOSignConstants.CMS_CONTENTTYPE_SIGNEDDATA));
+                
+                tree = signer.getSignersStructure(sign3, false);
+                Assert.assertEquals("Datos", ((AOTreeNode) tree.getRoot()).getUserObject());
+                Assert.assertEquals("ANF Usuario Activo", ((AOTreeNode) tree.getRoot()).getChildAt(0).getUserObject());
+                
+                tree = signer.getSignersStructure(sign3, true);
+                Assert.assertEquals("Datos", ((AOTreeNode) tree.getRoot()).getUserObject());
+                simpleSignInfo = (AOSimpleSignInfo) ((AOTreeNode) tree.getRoot()).getChildAt(0).getUserObject();
+                
+//                Assert.assertEquals("CAdES", simpleSignInfo.getSignFormat());
+//                Assert.assertEquals(algo, simpleSignInfo.getSignAlgorithm());
+                Assert.assertNotNull(simpleSignInfo.getSigningTime());
+                //Assert.assertEquals(pke3.getCertificate(), simpleSignInfo.getCerts()[0]);    
+                
+                //System.out.println(prueba + ": OK"); //$NON-NLS-1$
+            }
+        }
+        
+        try {
+            signer.sign(DATA, "SHA1withRSA", pke1, null); //$NON-NLS-1$
+        }
+        catch(final Exception e) {
+            Assert.fail("Error al generar la firma: " + e); //$NON-NLS-1$
+        }
+    }
 
+    private PrivateKeyEntry loadKeyEntry(String pkcs12File, String alias, String password) {
+        final PrivateKeyEntry pke;
+        try {
+            KeyStore ks = KeyStore.getInstance("PKCS12"); //$NON-NLS-1$
+            ks.load(ClassLoader.getSystemResourceAsStream(pkcs12File), password.toCharArray());
+            pke = (PrivateKeyEntry) ks.getEntry(alias, new KeyStore.PasswordProtection(password.toCharArray()));
+        }
+        catch(Exception e) {
+            Assert.fail("No se ha podido obtener las claves de firma del certificado '" + pkcs12File + "': " + e); //$NON-NLS-1$
+            return null;
+        }
+        
+        return pke;
+    }
+    
+    private byte[] sign(AOSigner signer, byte[] data, String algorithm, PrivateKeyEntry pke, Properties params) {
+        try {
+            return signer.sign(data, algorithm, pke, params);
+        }
+        catch(final Exception e) {
+            Assert.fail("Error al firmar:" + e);
+            return null;
+        }
+    }
+    
+    private byte[] cosign(AOSigner signer, byte[] sign, String algorithm, PrivateKeyEntry pke, Properties params) {
+        try {
+            return signer.cosign(sign, algorithm, pke, params);
+        }
+        catch(final Exception e) {
+            Assert.fail("Error al cofirmar con datos:" + e);
+            return null;
+        }
+    }
+    
+    private byte[] cosign(AOSigner signer, byte[] data, byte[] sign, String algorithm, PrivateKeyEntry pke, Properties params) {
+        try {
+            return signer.cosign(data, sign, algorithm, pke, params);
+        }
+        catch(final Exception e) {
+            Assert.fail("Error al cofirmar con datos:" + e);
+            return null;
+        }
+    }
 }
