621c5e31bcfe78bb040041080c162ba373b11ad0
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2011-08-26 18:46:00 +0000

    Se presupone que no hay un SecurityManager (o que se gestiona externamente) y se prescinde de clases propietarias de Sun para el manejo de un Map externo, eliminandose así varias advertencias de compilación
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@335 dcdbb401-323c-49cf-88a0-803402807c85

--- KeyStores-CAPIAddressBook/src/main/java/es/gob/afirma/keystores/capiaddressbook/SunMSCAPIAddressBook.java
+++ KeyStores-CAPIAddressBook/src/main/java/es/gob/afirma/keystores/capiaddressbook/SunMSCAPIAddressBook.java
@@ -7,17 +7,11 @@
 
 package es.gob.afirma.keystores.capiaddressbook;
 
-import java.security.AccessController;
 import java.security.Provider;
-import java.util.HashMap;
-import java.util.Map;
-
-import sun.security.action.PutAllAction;
 
 /** Proveedor JCA para el acceso a los almacenes de claves de CAPI <i>CA</i> y
  * <i>ADDRESSBOOK</i>. Basado en SunMSCAPI.java
  * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s */
-@SuppressWarnings({ "unchecked", "rawtypes", "restriction" })
 final class SunMSCAPIAddressBook extends Provider {
 
     private static final long serialVersionUID = 2561943190524802403L;
@@ -28,18 +22,8 @@ final class SunMSCAPIAddressBook extends Provider {
      * <i>CA</i> y <i>ADDRESSBOOK</i>. */
     SunMSCAPIAddressBook() {
         super("SunMSCAPIAddressBook", 0.1d, INFO); //$NON-NLS-1$
-
-        // if there is no security manager installed, put directly into
-        // the provider. Otherwise, create a temporary map and use a
-        // doPrivileged() call at the end to transfer the contents
-        final Map map = (System.getSecurityManager() == null) ? (Map) this : new HashMap<String, String>();
-
-        map.put("KeyStore.Windows-ADDRESSBOOK", "sun.security.mscapi.KeyStoreAddressBook$ADDRESSBOOK"); //$NON-NLS-1$ //$NON-NLS-2$
-        map.put("KeyStore.Windows-CA", "sun.security.mscapi.KeyStoreAddressBook$CA"); //$NON-NLS-1$ //$NON-NLS-2$
-
-        if (map != this) {
-            AccessController.doPrivileged(new PutAllAction(this, map));
-        }
+        put("KeyStore.Windows-ADDRESSBOOK", "sun.security.mscapi.KeyStoreAddressBook$ADDRESSBOOK"); //$NON-NLS-1$ //$NON-NLS-2$
+        put("KeyStore.Windows-CA", "sun.security.mscapi.KeyStoreAddressBook$CA"); //$NON-NLS-1$ //$NON-NLS-2$
     }
 
 }
