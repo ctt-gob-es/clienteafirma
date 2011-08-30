81c9befde51dee2403ab88ffafecbeb52b32789c
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2011-08-30 15:27:25 +0000

    Cambios en la gestión de mensajes para evitar las conexiones de red adicionales buscando ficheros de recursos
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@373 dcdbb401-323c-49cf-88a0-803402807c85

--- Afirma-Core-JSE/src/main/java/es/gob/afirma/core/ui/jse/JSEUIMessages.java
+++ Afirma-Core-JSE/src/main/java/es/gob/afirma/core/ui/jse/JSEUIMessages.java
@@ -10,8 +10,11 @@
 
 package es.gob.afirma.core.ui.jse;
 
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.Vector;
 import java.util.logging.Logger;
 
 /** Clase para la obtencion de los recursos textuales del UI del n&uacute;cleo del
@@ -19,12 +22,21 @@ import java.util.logging.Logger;
 final class JSEUIMessages {
 
     private static final String BUNDLE_NAME = "uimessages"; //$NON-NLS-1$
-
     private static ResourceBundle RESOURCE_BUNDLE;
 
     static {
-        try {
-            RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, Locale.getDefault());
+        try {            
+            ClassLoader classLoader = JSEUIMessages.class.getClassLoader();
+            if (classLoader instanceof URLClassLoader) {
+                Vector<URL> urls = new Vector<URL>();
+                for (URL url : ((URLClassLoader)classLoader).getURLs()) {
+                    if (url.toString().endsWith(".jar")) { //$NON-NLS-1$
+                        urls.add(url);
+                    }
+                    classLoader = new URLClassLoader(urls.toArray(new URL[0]));
+                }
+            }
+            RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, Locale.getDefault(), classLoader);
         }
         catch (final Exception e) {
             try {
@@ -53,46 +65,46 @@ final class JSEUIMessages {
         }
     }
 
-    /** Recupera el texto identificado con la clave proporcionada y sustituye la
-     * subcadenas "%0" por el texto proporcionado.
-     * @param key
-     *        Clave del texto.
-     * @param text
-     *        Texto que se desea insertar.
-     * @return Recuerso textual con la subcadena sustituida. */
-    static String getString(final String key, final String text) {
-        try {
-            return RESOURCE_BUNDLE.getString(key).replace("%0", text); //$NON-NLS-1$
-        }
-        catch (final Exception e) {
-            return '!' + key + '!';
-        }
-    }
+//    /** Recupera el texto identificado con la clave proporcionada y sustituye la
+//     * subcadenas "%0" por el texto proporcionado.
+//     * @param key
+//     *        Clave del texto.
+//     * @param text
+//     *        Texto que se desea insertar.
+//     * @return Recuerso textual con la subcadena sustituida. */
+//    static String getString(final String key, final String text) {
+//        try {
+//            return RESOURCE_BUNDLE.getString(key).replace("%0", text); //$NON-NLS-1$
+//        }
+//        catch (final Exception e) {
+//            return '!' + key + '!';
+//        }
+//    }
 
-    /** Recupera el texto identificado con la clave proporcionada y sustituye las
-     * subcadenas de tipo "%i" por el texto en la posici&oacute;n 'i' del array
-     * proporcionado.
-     * @param key
-     *        Clave del texto.
-     * @param params
-     *        Par&aacute;metros que se desean insertar.
-     * @return Recuerso textual con las subcadenas sustituidas. */
-    static String getString(final String key, final String[] params) {
-
-        String text;
-        try {
-            text = RESOURCE_BUNDLE.getString(key);
-        }
-        catch (final Exception e) {
-            return '!' + key + '!';
-        }
-
-        if (params != null) {
-            for (int i = 0; i < params.length; i++) {
-                text = text.replace("%" + i, params[i]); //$NON-NLS-1$
-            }
-        }
-
-        return text;
-    }
+//    /** Recupera el texto identificado con la clave proporcionada y sustituye las
+//     * subcadenas de tipo "%i" por el texto en la posici&oacute;n 'i' del array
+//     * proporcionado.
+//     * @param key
+//     *        Clave del texto.
+//     * @param params
+//     *        Par&aacute;metros que se desean insertar.
+//     * @return Recuerso textual con las subcadenas sustituidas. */
+//    static String getString(final String key, final String[] params) {
+//
+//        String text;
+//        try {
+//            text = RESOURCE_BUNDLE.getString(key);
+//        }
+//        catch (final Exception e) {
+//            return '!' + key + '!';
+//        }
+//
+//        if (params != null) {
+//            for (int i = 0; i < params.length; i++) {
+//                text = text.replace("%" + i, params[i]); //$NON-NLS-1$
+//            }
+//        }
+//
+//        return text;
+//    }
 }
