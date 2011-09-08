d199c653f50fd44ae8f548c5f9c7195b35700c91
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2011-09-08 17:04:29 +0000

    Uso de las funcionalidades del Core para obtención de ClassLoader limpio
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@439 dcdbb401-323c-49cf-88a0-803402807c85

--- KeyStores-Mozilla/src/main/java/es/gob/afirma/keystores/mozilla/FirefoxKeyStoreMessages.java
+++ KeyStores-Mozilla/src/main/java/es/gob/afirma/keystores/mozilla/FirefoxKeyStoreMessages.java
@@ -1,40 +1,14 @@
 package es.gob.afirma.keystores.mozilla;
-import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
-import java.util.Vector;
-import java.util.logging.Logger;
+
+import es.gob.afirma.core.misc.AOUtil;
 
 class FirefoxKeyStoreMessages {
     
     private static final String BUNDLE_NAME = "firefoxmessages"; //$NON-NLS-1$
-    private static ResourceBundle RESOURCE_BUNDLE;
-
-    static {
-        try {            
-            ClassLoader classLoader = FirefoxKeyStoreMessages.class.getClassLoader();
-            if (classLoader instanceof URLClassLoader) {
-                Vector<URL> urls = new Vector<URL>();
-                for (URL url : ((URLClassLoader)classLoader).getURLs()) {
-                    if (url.toString().endsWith(".jar")) { //$NON-NLS-1$
-                        urls.add(url);
-                    }
-                    classLoader = new URLClassLoader(urls.toArray(new URL[0]));
-                }
-            }
-            RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, Locale.getDefault(), classLoader);
-        }
-        catch (final Exception e) {
-            try {
-                RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
-            }
-            catch (final Exception e1) {
-                Logger.getLogger("es.gob.afirma").severe("No ha podido cargarse el fichero de mensajes localizados: " + e1);  //$NON-NLS-1$//$NON-NLS-2$
-            }
-        }
-    }
+    private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, Locale.getDefault(), AOUtil.getCleanClassLoader());
     
     private FirefoxKeyStoreMessages() {
         // No permitimos la instanciacion
