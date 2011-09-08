52a47d78d6fb144a42e3777d298f3048b984ba2b
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2011-09-08 17:05:44 +0000

    Uso de las funcionalidades del Core para obtención de ClassLoader limpio
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@440 dcdbb401-323c-49cf-88a0-803402807c85

--- Afirma-MassiveSign/src/main/java/es/gob/afirma/massive/MassiveSignMessages.java
+++ Afirma-MassiveSign/src/main/java/es/gob/afirma/massive/MassiveSignMessages.java
@@ -1,12 +1,15 @@
 package es.gob.afirma.massive;
 
+import java.util.Locale;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
+
+import es.gob.afirma.core.misc.AOUtil;
 
 class MassiveSignMessages {
     private static final String BUNDLE_NAME = "massivesignmessages"; //$NON-NLS-1$
 
-    private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+    private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME, Locale.getDefault(), AOUtil.getCleanClassLoader());
 
     private MassiveSignMessages() {
         // No permitimos la instanciacion
