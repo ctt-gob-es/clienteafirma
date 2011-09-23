33c534a7c3f927cc848ac5ac493aa4c8186c82f7
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2011-09-23 16:03:22 +0000

    Ahora los OID también son clave para obtenerse a si mismos como algoritmos
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@585 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-crypto-core-pkcs7/src/main/java/es/gob/afirma/signers/pkcs7/AOAlgorithmID.java
+++ afirma-crypto-core-pkcs7/src/main/java/es/gob/afirma/signers/pkcs7/AOAlgorithmID.java
@@ -17,21 +17,28 @@ public final class AOAlgorithmID {
         oids.put("SHA1", "1.3.14.3.2.26"); //$NON-NLS-1$ //$NON-NLS-2$
         oids.put("SHA-1", "1.3.14.3.2.26"); //$NON-NLS-1$ //$NON-NLS-2$
         oids.put("SHA", "1.3.14.3.2.26"); //$NON-NLS-1$ //$NON-NLS-2$
+        oids.put("1.3.14.3.2.26", "1.3.14.3.2.26"); //$NON-NLS-1$ //$NON-NLS-2$
         
         oids.put("SHA-512", "2.16.840.1.101.3.4.2.3"); //$NON-NLS-1$ //$NON-NLS-2$
         oids.put("SHA512", "2.16.840.1.101.3.4.2.3"); //$NON-NLS-1$ //$NON-NLS-2$
+        oids.put("2.16.840.1.101.3.4.2.3", "2.16.840.1.101.3.4.2.3"); //$NON-NLS-1$ //$NON-NLS-2$
         
         oids.put("MD2", "1.2.840.113549.2.2"); //$NON-NLS-1$ //$NON-NLS-2$
+        oids.put("1.2.840.113549.2.2", "1.2.840.113549.2.2"); //$NON-NLS-1$ //$NON-NLS-2$
         
         oids.put("MD5", "1.2.840.113549.2.5"); //$NON-NLS-1$ //$NON-NLS-2$
+        oids.put("1.2.840.113549.2.5", "1.2.840.113549.2.5"); //$NON-NLS-1$ //$NON-NLS-2$
         
         oids.put("SHA-256", "2.16.840.1.101.3.4.2.1"); //$NON-NLS-1$ //$NON-NLS-2$
         oids.put("SHA256", "2.16.840.1.101.3.4.2.1"); //$NON-NLS-1$ //$NON-NLS-2$
+        oids.put("2.16.840.1.101.3.4.2.1", "2.16.840.1.101.3.4.2.1"); //$NON-NLS-1$ //$NON-NLS-2$
         
         oids.put("SHA-384", "2.16.840.1.101.3.4.2.2"); //$NON-NLS-1$ //$NON-NLS-2$
         oids.put("SHA384", "2.16.840.1.101.3.4.2.2"); //$NON-NLS-1$ //$NON-NLS-2$
+        oids.put("2.16.840.1.101.3.4.2.2", "2.16.840.1.101.3.4.2.2"); //$NON-NLS-1$ //$NON-NLS-2$
         
         oids.put("RSA", "1.2.840.113549.1.1.1"); //$NON-NLS-1$ //$NON-NLS-2$
+        oids.put("1.2.840.113549.1.1.1", "1.2.840.113549.1.1.1"); //$NON-NLS-1$ //$NON-NLS-2$
     }
     
     /** Obtiene el OID del algoritmo indicado.
