c3d21a5e8b8fb29063b52372cb2f7b33d8c8e1d8
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2011-09-29 15:56:39 +0000

    Ahora el botón muestra el borde al recibir el foco
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@672 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-ui-standalone/src/main/java/es/gob/afirma/ui/principal/ToggleImageButton.java
+++ afirma-ui-standalone/src/main/java/es/gob/afirma/ui/principal/ToggleImageButton.java
@@ -181,7 +181,7 @@ public class ToggleImageButton extends JToggleButton {
     
     @Override
     protected void paintComponent(Graphics g) {
-        if (this.mouseOver && !this.isSelected()) {
+        if ((this.mouseOver || this.isFocusOwner()) && !this.isSelected()) {
             super.paintComponent(g);
         }
     }
