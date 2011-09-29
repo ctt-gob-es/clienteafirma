dd6e319c1037e52b8dc2b59857298c1f9710f70d
Javier Morais Romero <javier.morais@inteco.es>
2011-09-29 08:22:23 +0000

    Test para el diálogo de guardado de ficheros que extiende de JFilechooser y es accesible.
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@664 dcdbb401-323c-49cf-88a0-803402807c85

---
+++ afirma-ui-standalone/src/test/java/es/gob/afirma/ui/utils/JAccessibilityFileChooserToSaveTest.java
@@ -0,0 +1,102 @@
+package es.gob.afirma.ui.utils;
+
+import static org.junit.Assert.*;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Logger;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JToggleButton;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Testeo de accesibilidad para la clase JAccessibilityFileChooserToSaveTest.
+ * @author lmerayo
+ *
+ */
+public class JAccessibilityFileChooserToSaveTest {
+
+	/**
+	 * Log.
+	 */
+	static Logger logger = Logger.getLogger(JAccessibilityFileChooserToSaveTest.class.getName());
+	
+
+	/*
+	 * Comprobación de que el campo Mnemocic de las etiquetas y botones no esté duplicado. 
+	 */
+	@Test
+	public void testNotDuplicatedDisplayedMnemonic() {
+		logger.info("testNotDuplicatedDisplayedMnemonic");
+
+		//Instancia del contenedor que se va a analizar
+		JAccessibilityFileChooserToSave fileChooser= new JAccessibilityFileChooserToSave(null);
+
+		//Lista de mnemónicos
+		List <Integer> keyCodes = new ArrayList<Integer>();
+		//Conjunto de mnemónicos
+		Set <Integer> keyCodesSet = null;
+		
+		//Se llama al método que obtiene una lista de códigos de atajos asociados a los componentes del panel
+		getKeyCodeList (fileChooser, keyCodes);
+
+		//Se crea un conjunto a partir de la lista para eliminar duplicados
+		keyCodesSet = new HashSet<Integer>(keyCodes);
+		//Si el tamaño de la lista y del conjunto no son iguales, no hay duplicados
+		assertTrue(keyCodesSet.size() == keyCodes.size());
+	}
+	
+	/**
+	 * Método que obtiene una lista de códigos de atajos a los componentes (Etiqueta, Botón) de un panel.
+	 */
+	@Ignore
+	private void getKeyCodeList(Container panel, List <Integer> keyCodeList) {
+		//Array de componentes del panel
+		Component[] components = panel.getComponents();
+		int keyCode = 0;
+		for (int i = 0; i < components.length; i++) {
+			//Se obtiene el componente
+			Component component = panel.getComponent(i);
+			//Se comprueba si es una etiqueta
+			if (component instanceof JLabel) {
+					JLabel label = (JLabel) component;
+					//Se obtiene el código del atajo asociado
+					keyCode = label.getDisplayedMnemonic();
+					//Se añade a la lista si existe este código, es decir, si es distinto de 0
+					if (keyCode != 0) {
+						keyCodeList.add(new Integer(keyCode));
+					}
+			} else if (component instanceof JButton) {
+					JButton button = (JButton) component;
+					//Se obtiene el código del atajo asociado
+					keyCode = button.getMnemonic();
+					//Se añade a la lista si existe este código, es decir, si es distinto de 0
+					if (keyCode != 0) {
+						keyCodeList.add(new Integer(keyCode));
+					}
+			} else if (component instanceof JToggleButton) {
+					JToggleButton toggleButton = (JToggleButton) component;
+
+					//Se obtiene el código del atajo asociado
+					keyCode = toggleButton.getMnemonic();
+					//Se añade a la lista si existe este código, es decir, si es distinto de 0
+					if (keyCode != 0) {
+						keyCodeList.add(new Integer(keyCode));
+					}
+			} else if (component instanceof Container) {
+				//Si es un contenedor se vuelve a llamar recursivamente al método
+				getKeyCodeList((Container)component, keyCodeList);
+			}
+		} //for
+	}//getKeyCodeList
+
+
+}
