d0dd2d9e0685823e434d339f2ba409849b0b5753
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2011-10-03 11:57:47 +0000

    Actualización del texto de licencia
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@710 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-core-massive/src/main/java/es/gob/afirma/massive/MassiveSignMessages.java
+++ afirma-core-massive/src/main/java/es/gob/afirma/massive/MassiveSignMessages.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Este fichero forma parte del Cliente @firma.
+ * El Cliente @firma es un aplicativo de libre distribucion cuyo codigo fuente puede ser consultado
+ * y descargado desde http://forja-ctt.administracionelectronica.gob.es/
+ * Copyright 2009,2010,2011 Gobierno de Espana
+ * Este fichero se distribuye bajo  bajo licencia GPL version 2  segun las
+ * condiciones que figuran en el fichero 'licence' que se acompana. Si se distribuyera este
+ * fichero individualmente, deben incluirse aqui las condiciones expresadas alli.
+ ******************************************************************************/
+
 package es.gob.afirma.massive;
 
 import java.util.Locale;
