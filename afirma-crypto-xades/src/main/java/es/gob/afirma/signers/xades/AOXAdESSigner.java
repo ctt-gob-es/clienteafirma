c2b7e96629072c57963ee41c1bd4da26937252c0
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2011-10-03 13:50:54 +0000

    Actualización del texto de licencia
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@719 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-crypto-xades/src/main/java/es/gob/afirma/signers/xades/AOXAdESSigner.java
+++ afirma-crypto-xades/src/main/java/es/gob/afirma/signers/xades/AOXAdESSigner.java
@@ -1,12 +1,12 @@
-/*
+/*******************************************************************************
  * Este fichero forma parte del Cliente @firma.
  * El Cliente @firma es un aplicativo de libre distribucion cuyo codigo fuente puede ser consultado
- * y descargado desde www.ctt.map.es.
+ * y descargado desde http://forja-ctt.administracionelectronica.gob.es/
  * Copyright 2009,2010,2011 Gobierno de Espana
  * Este fichero se distribuye bajo  bajo licencia GPL version 2  segun las
  * condiciones que figuran en el fichero 'licence' que se acompana. Si se distribuyera este
  * fichero individualmente, deben incluirse aqui las condiciones expresadas alli.
- */
+ ******************************************************************************/
 
 package es.gob.afirma.signers.xades;
 
@@ -2226,7 +2226,7 @@ public final class AOXAdESSigner implements AOSigner {
 
     /** Devuelve un nuevo documento con ra&iacute;z "AFIRMA" del que cuelga el
      * documento especificado.
-     * @param doc
+     * @param docu
      *        Documento que estar&aacute; contenido en el nuevo documento.
      * @return Documento con ra&iacute;z "AFIRMA".
      * @throws ParserConfigurationException */
--- afirma-crypto-xades/src/test/java/es/gob/afirma/test/xades/TestXAdES.java
+++ afirma-crypto-xades/src/test/java/es/gob/afirma/test/xades/TestXAdES.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Este fichero forma parte del Cliente @firma.
+ * El Cliente @firma es un aplicativo de libre distribucion cuyo codigo fuente puede ser consultado
+ * y descargado desde http://forja-ctt.administracionelectronica.gob.es/
+ * Copyright 2009,2010,2011 Gobierno de Espana
+ * Este fichero se distribuye bajo  bajo licencia GPL version 2  segun las
+ * condiciones que figuran en el fichero 'licence' que se acompana. Si se distribuyera este
+ * fichero individualmente, deben incluirse aqui las condiciones expresadas alli.
+ ******************************************************************************/
+
 package es.gob.afirma.test.xades;
 
 import java.io.ByteArrayInputStream;
@@ -16,7 +26,6 @@ import javax.xml.parsers.DocumentBuilderFactory;
 
 import junit.framework.Assert;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
