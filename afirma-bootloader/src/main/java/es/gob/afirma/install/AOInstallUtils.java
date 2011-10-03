f6f073e6bd915c9730012b4f06522c5ec2384e45
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2011-10-03 11:02:59 +0000

    Actualización del texto de licencia
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@687 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-bootloader/src/main/java/es/gob/afirma/install/AOInstallUtils.java
+++ afirma-bootloader/src/main/java/es/gob/afirma/install/AOInstallUtils.java
@@ -1,12 +1,13 @@
-/*
+/*******************************************************************************
  * Este fichero forma parte del Cliente @firma.
  * El Cliente @firma es un aplicativo de libre distribucion cuyo codigo fuente puede ser consultado
- * y descargado desde www.ctt.map.es.
+ * y descargado desde http://forja-ctt.administracionelectronica.gob.es/
  * Copyright 2009,2010,2011 Gobierno de Espana
- * Este fichero se distribuye bajo  bajo licencia GPL version 2 segun las
+ * Este fichero se distribuye bajo  bajo licencia GPL version 2  segun las
  * condiciones que figuran en el fichero 'licence' que se acompana. Si se distribuyera este
  * fichero individualmente, deben incluirse aqui las condiciones expresadas alli.
- */
+ ******************************************************************************/
+
 package es.gob.afirma.install;
 
 import java.io.File;
