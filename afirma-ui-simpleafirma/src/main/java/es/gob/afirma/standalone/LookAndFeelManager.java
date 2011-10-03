25bee21531d83d88cbfe620e4c50853adb34ef29
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2011-10-03 14:12:58 +0000

    Actualización del texto de licencia
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@722 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-ui-simpleafirma/src/main/java/es/gob/afirma/standalone/LookAndFeelManager.java
+++ afirma-ui-simpleafirma/src/main/java/es/gob/afirma/standalone/LookAndFeelManager.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Este fichero forma parte del Cliente @firma.
+ * El Cliente @firma es un aplicativo de libre distribucion cuyo codigo fuente puede ser consultado
+ * y descargado desde http://forja-ctt.administracionelectronica.gob.es/
+ * Copyright 2009,2010,2011 Gobierno de Espana
+ * Este fichero se distribuye bajo  bajo licencia GPL version 2  segun las
+ * condiciones que figuran en el fichero 'licence' que se acompana. Si se distribuyera este
+ * fichero individualmente, deben incluirse aqui las condiciones expresadas alli.
+ ******************************************************************************/
+
 package es.gob.afirma.standalone;
 
 import java.awt.Color;
@@ -57,10 +67,7 @@ public final class LookAndFeelManager {
         }
     }
     
-    /**
-     * Establece el decorado de la aplicaci&oacute;n.
-     * @param defaultLookAndFeel Indica que se aplique la decoraci&oacute;n por defecto del sistema.
-     */
+    /** Establece el decorado de la aplicaci&oacute;n. */
     static void applyLookAndFeel() {
 
         final boolean defaultLookAndFeel = HIGH_CONTRAST || LARGE_FONT;
--- afirma-ui-simpleafirma/src/main/java/es/gob/afirma/standalone/dnie/package-info.java
+++ afirma-ui-simpleafirma/src/main/java/es/gob/afirma/standalone/dnie/package-info.java
@@ -1,14 +1,12 @@
-/*
+/*******************************************************************************
  * Este fichero forma parte del Cliente @firma.
  * El Cliente @firma es un aplicativo de libre distribucion cuyo codigo fuente puede ser consultado
- * y descargado desde www.ctt.map.es.
+ * y descargado desde http://forja-ctt.administracionelectronica.gob.es/
  * Copyright 2009,2010,2011 Gobierno de Espana
- * Este fichero se distribuye bajo licencia GPL version 3 segun las
+ * Este fichero se distribuye bajo  bajo licencia GPL version 2  segun las
  * condiciones que figuran en el fichero 'licence' que se acompana. Si se distribuyera este
  * fichero individualmente, deben incluirse aqui las condiciones expresadas alli.
- */
+ ******************************************************************************/
 
-/**
- * Clases relacionadas con el DNIe y su uso como almac&eacute;n de claves.
- */
+/** Clases relacionadas con el DNIe y su uso como almac&eacute;n de claves. */
 package es.gob.afirma.standalone.dnie;
