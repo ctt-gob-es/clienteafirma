5025f7f65edf7b2788ee89f0775f89a7f113dded
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2011-10-03 13:48:42 +0000

    Actualización del texto de licencia
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@718 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-util/src/main/java/es/gob/afirma/util/signers/AOSignerFactory.java
+++ afirma-util/src/main/java/es/gob/afirma/util/signers/AOSignerFactory.java
@@ -1,12 +1,12 @@
-/*
+/*******************************************************************************
  * Este fichero forma parte del Cliente @firma.
  * El Cliente @firma es un aplicativo de libre distribucion cuyo codigo fuente puede ser consultado
- * y descargado desde www.ctt.map.es.
+ * y descargado desde http://forja-ctt.administracionelectronica.gob.es/
  * Copyright 2009,2010,2011 Gobierno de Espana
  * Este fichero se distribuye bajo  bajo licencia GPL version 2  segun las
  * condiciones que figuran en el fichero 'licence' que se acompana. Si se distribuyera este
  * fichero individualmente, deben incluirse aqui las condiciones expresadas alli.
- */
+ ******************************************************************************/
 
 package es.gob.afirma.util.signers;
 
@@ -168,8 +168,7 @@ public final class AOSignerFactory {
     }
 
     /** Obtiene el manejador del formato de firma por defecto establecido.
-     * @return Manejador de firma.
-     * @see es.gob.afirma.misc.AOConstants#DEFAULT_SIGN_FORMAT */
+     * @return Manejador de firma */
     public final AOSigner getDefaultSigner() {
         for (final String[] element : ID_SIGNERS) {
             if (element[0].equals(AOSignConstants.DEFAULT_SIGN_FORMAT)) {
