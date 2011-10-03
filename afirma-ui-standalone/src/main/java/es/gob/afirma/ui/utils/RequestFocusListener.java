877f10f7e5ffa4982e8f7795364b087df0964961
Javier Morais Romero <javier.morais@inteco.es>
2011-10-03 11:53:53 +0000

    Clase que se utilizará para dar el foco a un determinado componente en un diálogo.
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@704 dcdbb401-323c-49cf-88a0-803402807c85

---
+++ afirma-ui-standalone/src/main/java/es/gob/afirma/ui/utils/RequestFocusListener.java
@@ -0,0 +1,49 @@
+package es.gob.afirma.ui.utils;
+
+import javax.swing.*;
+import javax.swing.event.*;
+
+/**
+ * Clase para dar el foco a un componente determinado.
+ */
+public class RequestFocusListener implements AncestorListener
+{
+	private boolean removeListener;
+
+
+	/**
+	 * Constructor por defecto.
+	 */
+	public RequestFocusListener()
+	{
+		this(true);
+	}
+
+	/**
+	 * Constructor.
+	 * @param removeListener
+	 */
+	public RequestFocusListener(boolean removeListener)
+	{
+		this.removeListener = removeListener;
+	}
+
+	/**
+	 * Asigna el foco al componente que ha lanzado el evento.
+	 */
+	@Override
+	public void ancestorAdded(AncestorEvent e)
+	{
+		JComponent component = e.getComponent();
+		component.requestFocusInWindow();
+
+		if (removeListener)
+			component.removeAncestorListener( this );
+	}
+
+	@Override
+	public void ancestorMoved(AncestorEvent e) {}
+
+	@Override
+	public void ancestorRemoved(AncestorEvent e) {}
+}
