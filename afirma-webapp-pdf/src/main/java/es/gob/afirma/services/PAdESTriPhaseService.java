ed0bc8f5a7efbb7f8de3eb7ba3122a91bad9ff11
Ricardo Borillo <borillo@gmail.com>
2011-10-11 16:45:28 +0000

    Movidos servicios de firma trifásica a afirma-webapp-pdf
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@840 dcdbb401-323c-49cf-88a0-803402807c85

---
+++ afirma-webapp-pdf/src/main/java/es/gob/afirma/services/PAdESTriPhaseService.java
@@ -0,0 +1,190 @@
+package es.gob.afirma.services;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.lowagie.text.pdf.codec.Base64;
+
+import es.gob.afirma.core.AOException;
+import es.gob.afirma.core.signers.AOSignConstants;
+import es.gob.afirma.signers.pades.PAdESTriPhaseSigner;
+import es.gob.afirma.signers.pades.PAdESTriPhaseSigner.PdfPreSignResult;
+
+/** Implementaci&oaute;n de referencia del servicio servidor de firma trif&aacute;sica PAdES. */
+@Path("pades")
+public class PAdESTriPhaseService
+{
+    /** Prefirma.
+     * @param base64Data Datos a prefirmar codificados en Base64
+     * @param algorithm Algoritmo de firma
+     * @param base64CertificateChain Cadena de certificados del firmante codificada en Base64
+     * @param extraParamsNames Nombres de las opciones adicionales de prefirma
+     * @param extraParamsValues Valores de las opciones adicionales de prefirma
+     * @return Prefirma PAdES
+     * @throws Exception en caso de cualquier error
+     */
+    @POST
+    @Path("pre")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_XML)
+    public PreSignatureResult pre(@FormParam("base64Data")             final String base64Data,
+                                  @FormParam("algorithm")              final String algorithm,
+                                  @FormParam("base64CertificateChain") final List<String> base64CertificateChain,
+                                  @FormParam("extraParamsNames")       final List<String> extraParamsNames,
+                                  @FormParam("extraParamsValues")      final List<String> extraParamsValues) throws Exception
+    {
+        final byte[] data = Base64.decode(base64Data);
+        
+        return executePreOperation(algorithm, base64CertificateChain,
+				extraParamsNames, extraParamsValues, data);
+    }
+    
+    @POST
+    @Path("pre/{reference}")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_XML)
+    public PreSignatureResult preWithReference(@FormParam("reference") final String reference,
+                                  @FormParam("algorithm")              final String algorithm,
+                                  @FormParam("base64CertificateChain") final List<String> base64CertificateChain,
+                                  @FormParam("extraParamsNames")       final List<String> extraParamsNames,
+                                  @FormParam("extraParamsValues")      final List<String> extraParamsValues) throws Exception
+    {
+    	byte[] data = new byte[] {};
+    	
+    	if (reference != null)
+    	{
+    		if ("SIMPLE".equals(reference))
+    		{
+    			data = inputStreamToByteArray(ClassLoader.getSystemResourceAsStream("TEST_PDF.pdf"));
+    		}
+
+    		if ("CERTIFIED".equals(reference))
+    		{
+    			data = inputStreamToByteArray(ClassLoader.getSystemResourceAsStream("TEST_PDF_Certified.pdf"));
+    		}
+    		
+    		if ("PASSWORD".equals(reference))
+    		{
+    			data = inputStreamToByteArray(ClassLoader.getSystemResourceAsStream("TEST_PDF_Password.pdf"));
+    		}
+    	}
+        		
+        return executePreOperation(algorithm, base64CertificateChain,
+				extraParamsNames, extraParamsValues, data);
+    }
+
+	private PreSignatureResult executePreOperation(final String algorithm,
+			final List<String> base64CertificateChain,
+			final List<String> extraParamsNames,
+			final List<String> extraParamsValues, byte[] data)
+			throws CertificateException, IOException, AOException {
+		
+		final List<X509Certificate> certChain = buildCertificateChain(base64CertificateChain);
+        final Properties extraParamsProperties = namesAndValuesListToProperties(extraParamsNames, extraParamsValues);
+
+        final PAdESTriPhaseSigner padesTri = new PAdESTriPhaseSigner();
+        final PdfPreSignResult preSignature = padesTri.preSign(
+                AOSignConstants.getDigestAlgorithmName(algorithm), 
+                data,
+                certChain.toArray(new X509Certificate[] {}), 
+                null, 
+                extraParamsProperties
+        );
+
+        return new PreSignatureResult(preSignature.getPreSign(), preSignature.getFileID());
+	}
+    
+
+    /** Postfirma.
+     * @param base64Data Datos a prefirmar codificados en Base64
+     * @param algorithm Algoritmo de firma
+     * @param base64CertificateChain Cadena de certificados del firmante codificada en Base64
+     * @param extraParamsNames Nombres de las opciones adicionales de prefirma
+     * @param extraParamsValues Valores de las opciones adicionales de prefirma
+     * @param base64Signature Firma PKCS#1 v1.5 codificada en Base64
+     * @param base64PreSignData Atributos firmados CAdES de la prefirma, codificados en Base64
+     * @param fileID FileID del PDF prefirmado
+     * @return PDF firmado
+     * @throws Exception en caso de cualquier problema
+     */
+    @POST
+    @Path("post")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public String post(@FormParam("base64Data")             final String base64Data,
+                       @FormParam("algorithm")              final String algorithm,
+                       @FormParam("base64CertificateChain") final List<String> base64CertificateChain,
+                       @FormParam("extraParamsNames")       final List<String> extraParamsNames,
+                       @FormParam("extraParamsValues")      final List<String> extraParamsValues,
+                       @FormParam("base64Signature")        final String base64Signature,
+                       @FormParam("base64PreSignData")      final String base64PreSignData,
+                       @FormParam("fileID") String fileID) throws Exception {
+        
+        final byte[] data = Base64.decode(base64Data);
+        final List<X509Certificate> certChain = buildCertificateChain(base64CertificateChain);
+        final Properties extraParamsProperties = namesAndValuesListToProperties(extraParamsNames, extraParamsValues);
+
+        final PAdESTriPhaseSigner padesTri = new PAdESTriPhaseSigner();
+        final byte[] finalSignature = padesTri.postSign(
+            AOSignConstants.getDigestAlgorithmName(algorithm), data,
+            certChain.toArray(new X509Certificate[] {}), null, extraParamsProperties,
+            Base64.decode(base64Signature), Base64.decode(base64PreSignData), fileID
+        );
+
+        return Base64.encodeBytes(finalSignature);
+    }
+
+    private List<X509Certificate> buildCertificateChain(final List<String> base64CertificateChain) throws CertificateException {
+        final List<X509Certificate> certChain = new ArrayList<X509Certificate>();
+        final CertificateFactory cf = java.security.cert.CertificateFactory.getInstance("X.509"); //$NON-NLS-1$
+
+        for (final String base64Certificate : base64CertificateChain) {
+            final byte[] certificateData = Base64.decode(base64Certificate);
+            certChain.add((X509Certificate) cf.generateCertificate(new ByteArrayInputStream(certificateData)));
+        }
+        return certChain;
+    }
+    
+    /** Genera un <code>Properties</code> en base a listas separadas de nombres de propiedad y valores. Ambas listas deben ser del mismo tama&ntilde;o
+     * @param names Nombres de las propiedades
+     * @param values Valores de las propiedades
+     * @return Fichero de propiedades
+     */
+    private static Properties namesAndValuesListToProperties(final List<String> names, final List<String> values) {
+        final Properties p = new Properties();
+        for (int i = 0; i < names.size(); i++) {
+            p.put(names.get(i), values.get(i));
+        }
+        return p;
+    }
+    
+	public static byte[] inputStreamToByteArray(InputStream in)
+			throws IOException {
+		byte[] buffer = new byte[2048];
+		int length = 0;
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+		while ((length = in.read(buffer)) >= 0) {
+			baos.write(buffer, 0, length);
+		}
+
+		return baos.toByteArray();
+	}
+}
---
+++ afirma-webapp-pdf/src/test/java/es/gob/afirma/test/pades/TestPAdESTriPhaseHTTPStandalone.java
@@ -0,0 +1,240 @@
+package es.gob.afirma.test.pades;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.security.KeyStore;
+import java.security.KeyStore.PrivateKeyEntry;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.X509Certificate;
+import java.util.Map.Entry;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.lowagie.text.pdf.codec.Base64;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
+
+import es.gob.afirma.core.misc.AOUtil;
+import es.gob.afirma.core.signers.AOSignConstants;
+import es.gob.afirma.core.signers.AOSigner;
+import es.gob.afirma.core.signers.beans.AOSimpleSignInfo;
+import es.gob.afirma.core.util.tree.AOTreeModel;
+import es.gob.afirma.core.util.tree.AOTreeNode;
+import es.gob.afirma.services.PreSignatureResult;
+import es.gob.afirma.signers.cades.PKCS1ExternalizableSigner;
+import es.gob.afirma.signers.pades.AOPDFSigner;
+
+public class TestPAdESTriPhaseHTTPStandalone {
+
+	private static final String CERT_PATH = "ANF_PF_Activo.pfx";
+	private static final String CERT_PASS = "12341234";
+	private static final String CERT_ALIAS = "anf usuario activo";
+
+	private static final String[] TEST_FILES = { "TEST_PDF.pdf" };
+
+	private WebResource resource;
+
+	public TestPAdESTriPhaseHTTPStandalone() {
+
+		Client client = Client.create();
+		this.resource = client
+				.resource("http://localhost:9003/afirma-webapp-pdf/rest/");
+	}
+
+	private final static Properties p1;
+
+	static {
+		p1 = new Properties();
+		p1.setProperty("format", AOSignConstants.SIGN_FORMAT_PDF);
+		p1.setProperty("mode", AOSignConstants.SIGN_MODE_IMPLICIT);
+		p1.setProperty("signReason", "test");
+		p1.setProperty("signatureProductionCity", "madrid");
+		p1.setProperty("signerContact", "sink@usa.net");
+		p1.setProperty("policyIdentifier", "2.16.724.1.3.1.1.2");
+		p1.setProperty("policyQualifier", "http://google.com/");
+	}
+
+	private final static String[] ALGOS = new String[] {
+			AOSignConstants.SIGN_ALGORITHM_SHA1WITHRSA,
+			AOSignConstants.SIGN_ALGORITHM_SHA512WITHRSA,
+			AOSignConstants.SIGN_ALGORITHM_SHA256WITHRSA,
+			AOSignConstants.SIGN_ALGORITHM_SHA384WITHRSA };
+
+	private byte[] sign(final byte[] data, final String algorithm,
+			final PrivateKeyEntry keyEntry, final Properties extraParams)
+			throws Exception {
+
+		final X509Certificate[] certChain = (X509Certificate[]) keyEntry
+				.getCertificateChain();
+
+		final PreSignatureResult preSignatureResult = preSignature(data,
+				algorithm, extraParams, certChain);
+
+		final byte[] signature = PKCS1ExternalizableSigner.sign(algorithm,
+				keyEntry, preSignatureResult.getPreSignData());
+
+		return postSignature(data, algorithm, extraParams, certChain,
+				preSignatureResult, signature);
+	}
+
+	private PreSignatureResult preSignature(final byte[] data,
+			final String algorithm, final Properties extraParams,
+			final X509Certificate[] certChain)
+			throws CertificateEncodingException {
+
+		final MultivaluedMap<String, String> params = convertParamsToMultivalueMap(
+				data, algorithm, extraParams, certChain);
+
+		final ClientResponse response = this.resource.path("pades/pre")
+				.type(MediaType.APPLICATION_FORM_URLENCODED)
+				.post(ClientResponse.class, params);
+
+		Assert.assertEquals(200, response.getStatus());
+
+		return response.getEntity(PreSignatureResult.class);
+	}
+
+	private byte[] postSignature(final byte[] data, final String algorithm,
+			final Properties extraParams, final X509Certificate[] certChain,
+			final PreSignatureResult preSignatureResult, final byte[] signature)
+			throws CertificateEncodingException {
+
+		final MultivaluedMap<String, String> params = convertParamsToMultivalueMap(
+				data, algorithm, extraParams, certChain, signature,
+				preSignatureResult);
+		final ClientResponse response = this.resource.path("pades/post")
+				.type(MediaType.APPLICATION_FORM_URLENCODED)
+				.post(ClientResponse.class, params);
+
+		Assert.assertEquals(200, response.getStatus());
+
+		return Base64.decode(response.getEntity(String.class));
+	}
+
+	private MultivaluedMap<String, String> convertParamsToMultivalueMap(
+			final byte[] data, final String algorithm,
+			final Properties extraParams, final X509Certificate[] certChain,
+			final byte[] signature, final PreSignatureResult preSignatureResult)
+			throws CertificateEncodingException {
+
+		final MultivaluedMap<String, String> params = convertParamsToMultivalueMap(
+				data, algorithm, extraParams, certChain);
+
+		params.add("base64Signature", Base64.encodeBytes(signature));
+		params.add("base64PreSignData",
+				Base64.encodeBytes(preSignatureResult.getPreSignData()));
+		params.add("fileID", preSignatureResult.getFileID());
+
+		return params;
+	}
+
+	private MultivaluedMap<String, String> convertParamsToMultivalueMap(
+			final byte[] data, final String algorithm,
+			final Properties extraParams, final X509Certificate[] certChain)
+			throws CertificateEncodingException {
+
+		final MultivaluedMap<String, String> params = new MultivaluedMapImpl();
+		params.add("base64Data", Base64.encodeBytes(data));
+		params.add("algorithm", algorithm);
+
+		for (final X509Certificate certificate : certChain) {
+			params.add("base64CertificateChain",
+					Base64.encodeBytes(certificate.getEncoded()));
+		}
+
+		for (final Entry<Object, Object> entry : extraParams.entrySet()) {
+			params.add("extraParamsNames", (String) entry.getKey());
+			params.add("extraParamsValues", (String) entry.getValue());
+		}
+
+		return params;
+	}
+
+	/**
+	 * Pruabas de las firmas trif&aacute;sicas PAdES usando llamadas locales.
+	 * Necesita un iText modificado
+	 * 
+	 * @throws Exception
+	 *             en caso de cualquier error
+	 */
+	@Test
+	public void testTriPhaseSignature() throws Exception {
+		Assert.assertEquals("file.signed.pdf",
+				AOPDFSigner.getSignedName("file.pdf"));
+
+		Logger.getLogger("es.gob.afirma").setLevel(Level.WARNING);
+
+		final X509Certificate cert;
+
+		KeyStore ks = KeyStore.getInstance("PKCS12");
+		ks.load(ClassLoader.getSystemResourceAsStream(CERT_PATH),
+				CERT_PASS.toCharArray());
+		final PrivateKeyEntry pke = (PrivateKeyEntry) ks.getEntry(CERT_ALIAS,
+				new KeyStore.PasswordProtection(CERT_PASS.toCharArray()));
+		cert = (X509Certificate) ks.getCertificate(CERT_ALIAS);
+
+		AOSigner signer = new AOPDFSigner();
+
+		String prueba;
+
+		for (final String algo : ALGOS) {
+			for (final String file : TEST_FILES) {
+				final byte[] testPdf = AOUtil
+						.getDataFromInputStream(ClassLoader
+								.getSystemResourceAsStream(file));
+
+				Assert.assertTrue("No se ha reconocido como un PDF",
+						signer.isValidDataFile(testPdf));
+
+				prueba = "Firma trifasica PAdES con el algoritmo ': " + algo
+						+ "' y el fichero '" + file + "'";
+
+				System.out.println(prueba);
+
+				byte[] result = sign(testPdf, algo, pke, p1);
+
+				Assert.assertNotNull(prueba, result);
+				Assert.assertTrue(signer.isSign(result));
+
+				AOTreeModel tree = signer.getSignersStructure(result, false);
+				Assert.assertEquals("Datos",
+						((AOTreeNode) tree.getRoot()).getUserObject());
+				Assert.assertEquals("ANF Usuario Activo", ((AOTreeNode) tree
+						.getRoot()).getChildAt(0).getUserObject());
+
+				tree = signer.getSignersStructure(result, true);
+				Assert.assertEquals("Datos",
+						((AOTreeNode) tree.getRoot()).getUserObject());
+				AOSimpleSignInfo simpleSignInfo = (AOSimpleSignInfo) ((AOTreeNode) tree
+						.getRoot()).getChildAt(0).getUserObject();
+
+				Assert.assertNotNull(simpleSignInfo.getSigningTime());
+				Assert.assertEquals(cert, simpleSignInfo.getCerts()[0]);
+				Assert.assertEquals("application/pdf",
+						signer.getDataMimeType(result));
+				Assert.assertEquals(result, signer.getData(result));
+				Assert.assertEquals(AOSignConstants.SIGN_FORMAT_PDF, signer
+						.getSignInfo(result).getFormat());
+
+				final File saveFile = File.createTempFile(algo + "-", ".pdf");
+				final OutputStream os = new FileOutputStream(saveFile);
+				os.write(result);
+				os.flush();
+				os.close();
+				System.out.println("Temporal para comprobacion manual: "
+						+ saveFile.getAbsolutePath());
+			}
+		}
+	}
+}
