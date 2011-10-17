55c6010d00e2ec20925bbba44a7dabffdf7544a3
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2011-10-17 15:03:13 +0000

    Se corrige la visibilidad de la clase.
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@927 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-keystores-capiaddressbook/src/main/java/es/gob/afirma/keystores/capiaddressbook/SunMSCAPIAddressBook.java
+++ afirma-keystores-capiaddressbook/src/main/java/es/gob/afirma/keystores/capiaddressbook/SunMSCAPIAddressBook.java
@@ -12,7 +12,7 @@ import java.security.Provider;
 /** Proveedor JCA para el acceso a los almacenes de claves de CAPI <i>CA</i> y
  * <i>ADDRESSBOOK</i>. Basado en SunMSCAPI.java
  * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s */
-final class SunMSCAPIAddressBook extends Provider {
+public final class SunMSCAPIAddressBook extends Provider {
 
     private static final long serialVersionUID = 2561943190524802403L;
 
@@ -20,7 +20,7 @@ final class SunMSCAPIAddressBook extends Provider {
 
     /** Construye el proveedor JCA para el acceso a los almacenes de claves
      * <i>CA</i> y <i>ADDRESSBOOK</i>. */
-    SunMSCAPIAddressBook() {
+    public SunMSCAPIAddressBook() {
         super("SunMSCAPIAddressBook", 0.1d, INFO); //$NON-NLS-1$
         put("KeyStore.Windows-ADDRESSBOOK", "sun.security.mscapi.KeyStoreAddressBook$ADDRESSBOOK"); //$NON-NLS-1$ //$NON-NLS-2$
         put("KeyStore.Windows-CA", "sun.security.mscapi.KeyStoreAddressBook$CA"); //$NON-NLS-1$ //$NON-NLS-2$
