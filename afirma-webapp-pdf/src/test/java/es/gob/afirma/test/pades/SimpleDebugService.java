e9289209b0e52fcb993590c87d6f18ca31e3564b
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2011-10-21 12:46:59 +0000

    Servicio Web para pruebas y depuración
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@982 dcdbb401-323c-49cf-88a0-803402807c85

---
+++ afirma-webapp-pdf/src/test/java/es/gob/afirma/test/pades/SimpleDebugService.java
@@ -0,0 +1,44 @@
+package es.gob.afirma.test.pades;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Logger;
+
+/** Servicio HTTP simple para simplificar las pruebas desde dispositivos.
+ * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s */
+public final class SimpleDebugService {
+    
+    static final Logger LOGGER = Logger.getLogger("es.gob.afirma"); //$NON-NLS-1$
+
+    private final static int port = 80;
+
+    /** Punto de entrada.
+     * @param args No se aceptan argumentos
+     * @throws Exception */
+    public static void main(String args[]) throws Exception {
+        final ServerSocket serversocket = new ServerSocket(port);
+        while (true) {
+            final Socket connectionsocket = serversocket.accept();
+            LOGGER.info(connectionsocket.getInetAddress().getHostName() + " contectado"); //$NON-NLS-1$
+            final BufferedReader input = new BufferedReader(new InputStreamReader(connectionsocket.getInputStream()));
+            final DataOutputStream output = new DataOutputStream(connectionsocket.getOutputStream());
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        final String request = new String(input.readLine());
+                        LOGGER.info(request);
+                        output.close();
+                    }
+                    catch(final Exception e) {
+                        LOGGER.severe("Error en la obtencion de datos: " + e); //$NON-NLS-1$
+                    }
+                }
+            }).start();
+        }
+    }
+
+}
