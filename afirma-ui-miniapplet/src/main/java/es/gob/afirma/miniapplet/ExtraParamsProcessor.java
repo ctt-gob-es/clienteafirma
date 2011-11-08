7390d66422c8f05396a635d3fb549b0d4d5ecd55
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2011-11-08 15:42:42 +0000

    Se agrega la funcionalidad de expandir parámetros entradas para facilitar la labor de los integradores mediante parámetros clave
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@1230 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-ui-miniapplet/src/main/java/es/gob/afirma/miniapplet/ExtraParamsProcessor.java
+++ afirma-ui-miniapplet/src/main/java/es/gob/afirma/miniapplet/ExtraParamsProcessor.java
@@ -38,4 +38,52 @@ public final class ExtraParamsProcessor {
 		
 		return params;
 	}
+	
+	private static final String EXPANDIBLE_POLICY_KEY = "ExpPolicy"; //$NON-NLS-1$
+	
+	private static final String EXPANDIBLE_POLICY_VALUE_AGE = "FirmaAGE"; //$NON-NLS-1$
+	
+	/**
+	 * Devuelve la colecci&oacute;n de propiedades de entrada con las entradas que correspondan
+	 * expandidos. Se expandiran una serie de claves con valores predefinidos y se les
+	 * asignar&aacute; el valor correspondiente.
+	 * Una vez expandidos, se eliminaran estos par&aacute;metros de la lista. Si el expandir
+	 * los par&aacute;metros implica establer otras propiedades y estas ya est&aacute;n
+	 * definidas en el Properties, prevalecer&aacute;n los valores expandidos.<br/>
+	 * Entre los par&aacute;metros clave se encuentran:
+	 * <ul>
+     *  <li><b>ExpPolicy</b>: Configuracion de la pol&iacute;tica de firma. Posibles valores:
+     *   <ul><li><b>FirmaAGE</b>:
+     *    Establece los diversions par&aacute;metros para la configuraci&oacute;n de la
+     *    pol&iacute;tica de firma de la AGE.
+     *   </li>
+     *   </ul>
+     *  </li>
+     * </ul>
+	 * @param params Par&aacute;metros definidos para la operaci&oacute;n.
+	 * @return Propiedades expandidas.
+	 */
+	public static Properties expandProperties(Properties params) {
+		
+		Properties p = new Properties();
+		for (String key : params.keySet().toArray(new String[0])) {
+			p.setProperty(key, params.getProperty(key));
+		}
+		
+		if (p.containsKey(EXPANDIBLE_POLICY_KEY)) {
+			if (EXPANDIBLE_POLICY_VALUE_AGE.equals(p.getProperty(EXPANDIBLE_POLICY_KEY))) {
+				p.setProperty("policyIdentifier", //$NON-NLS-1$
+						"urn:oid:2.16.724.1.3.1.1.2.1.8");  //$NON-NLS-1$ 
+				p.setProperty("policyIdentifierHash", //$NON-NLS-1$
+						"tSbjbefbEoLcD06K/IR8FtuhhVE=");  //$NON-NLS-1$ 
+				p.setProperty("policyIdentifierHashAlgorithm", //$NON-NLS-1$
+						"http://www.w3.org/2000/09/xmldsig#sha1"); //$NON-NLS-1$ 
+				p.setProperty("policyQualifier", //$NON-NLS-1$
+						"http://administracionelectronica.gob.es/es/ctt/politicafirma/politica_firma_AGE_v1_8.pdf"); //$NON-NLS-1$ 
+			}
+			p.remove(EXPANDIBLE_POLICY_KEY);
+		}
+		
+		return p;
+	}
 }
