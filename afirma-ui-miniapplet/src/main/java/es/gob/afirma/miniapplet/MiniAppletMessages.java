d5daf79cf02affd1db0affd7fdedea2e113e805e
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2011-11-08 15:43:17 +0000

    Se agrega un método para especificar múltiples cadenas de sustitución en los textos extraídos
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@1231 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-ui-miniapplet/src/main/java/es/gob/afirma/miniapplet/MiniAppletMessages.java
+++ afirma-ui-miniapplet/src/main/java/es/gob/afirma/miniapplet/MiniAppletMessages.java
@@ -41,4 +41,32 @@ public final class MiniAppletMessages {
             return '!' + key + '!';
         }
     }
+    
+    /** Recupera el texto identificado con la clave proporcionada y sustituye las
+     * subcadenas de tipo "%i" por el texto en la posici&oacute;n 'i' del array
+     * proporcionado. Introducir m&aacute;s de 10 cadenas distintas para sustituir
+     * conllevar&aacute; errores en el resultado. 
+     * @param key
+     *        Clave del texto.
+     * @param params
+     *        Par&aacute;metros que se desean insertar.
+     * @return Recuerso textual con las subcadenas sustituidas. */
+    public static String getString(final String key, final String[] params) {
+
+        String text;
+        try {
+            text = RESOURCE_BUNDLE.getString(key);
+        }
+        catch (final Exception e) {
+            return '!' + key + '!';
+        }
+
+        if (params != null) {
+            for (int i = 0; i < params.length; i++) {
+                text = text.replace("%" + i, params[i]); //$NON-NLS-1$
+            }
+        }
+
+        return text;
+    }
 }
