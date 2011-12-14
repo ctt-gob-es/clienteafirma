46fc46cd95911dc55bba7bc168752f2640adb1e2
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2011-12-14 18:55:28 +0000

    Mejoras en el JavaDoc
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@1474 dcdbb401-323c-49cf-88a0-803402807c85

---
+++ afirma-crypto-odf/src/main/java/es/gob/afirma/signers/odf/package-info.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Este fichero forma parte del Cliente @firma.
+ * El Cliente @firma es un aplicativo de libre distribucion cuyo codigo fuente puede ser consultado
+ * y descargado desde http://forja-ctt.administracionelectronica.gob.es/
+ * Copyright 2009,2010,2011 Gobierno de Espana
+ * Este fichero se distribuye bajo  bajo licencia GPL version 2  segun las
+ * condiciones que figuran en el fichero 'licence' que se acompana. Si se distribuyera este
+ * fichero individualmente, deben incluirse aqui las condiciones expresadas alli.
+ ******************************************************************************/
+
+/**
+ *	M&oacute;dulo de generaci&oacute;n de firmas digitales Open Document Format (ODF),
+ *  variante de XMLDSig (XML Digital Signature).
+ *  <p>
+ *   El m&oacute;dulo es compatible con LibreOffice y OpenOffice.org 3.2 y superiores, 
+ *   pudiendo opcionalmente generar documentos firmados en formato OpenOffice.org 3.1.
+ *  </p>
+ *  <p align="center"><br><img src="doc-files/package-info-1.png"></p>
+ *  <p>Este m&oacute;dulo presenta las siguientes dependencias directas de primer nivel:</p>
+ *  <ul>
+ *   <li>Dependencia con el m&oacute;dulo N&uacute;cleo (<i>afirma-core</i>) del Cliente.</li>
+ *   <li>Dependencia con el m&oacute;dulo XML (<i>afirma-crypto-core-xml</i>) del Cliente.</li>
+ *  </ul>
+ *  <p>
+ *   Este m&oacute;dulo es compatible con cualquier entorno JSE 1.6 o superior.<br>
+ *   Para compatibilidad
+ *   con JSE 1.5 es necesario incluir las clases Java contenidas en el "Paquete de compatibilidad
+ *   con Java 5" del Ciente e instalar los productos Apache Xalan 2.7.1 o superior y Apache Xerces
+ *   2.11.0 o superior como API ENDORSED de Java.<br>
+ *   Consulte la p&aacute;gina 
+ *   <a href="http://docs.oracle.com/javase/1.5.0/docs/guide/standards/index.html">http://docs.oracle.com/javase/1.5.0/docs/guide/standards/index.html</a>
+ *   para informaci&oacute;n ampliada sobre los API ENDORSED de Java.
+ *  </p>
+ *  <p>
+ *   Desde este m&oacute;dulo no se realizan llamadas a interfaces gr&aacute;ficas.<br> 
+ *  </p>
+ */
+package es.gob.afirma.signers.odf;
