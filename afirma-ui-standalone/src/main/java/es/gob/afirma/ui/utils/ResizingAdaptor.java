69a7a144b7760efdd05221846ec21b1714b583f3
Javier Morais Romero <javier.morais@inteco.es>
2011-12-15 17:09:14 +0000

    Eliminada traza.
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@1499 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-ui-standalone/src/main/java/es/gob/afirma/ui/utils/ResizingAdaptor.java
+++ afirma-ui-standalone/src/main/java/es/gob/afirma/ui/utils/ResizingAdaptor.java
@@ -326,7 +326,7 @@ public class ResizingAdaptor extends ComponentAdapter {
 			        StyleConstants.setFontSize(link, (7 + (int)resizeFactor));
 					((HTMLDocument) editorPanel.getDocument()).setCharacterAttributes(221, 26, link, false);
 				} else if (actualComponent instanceof JComboBox){
-					System.out.println("combo");
+					//System.out.println("combo");
 				}
 				else{
 					// Si nos encontramos con un contenedor, redimensionamos sus hijos
