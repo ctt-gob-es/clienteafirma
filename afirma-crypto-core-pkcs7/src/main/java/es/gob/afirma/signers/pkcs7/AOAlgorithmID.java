9983e68c8605c3d7f6a9215cf898f9f70ae1a4d3
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2011-12-15 16:20:18 +0000

    Correcciones menores de código
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@1493 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-crypto-core-pkcs7/src/main/java/es/gob/afirma/signers/pkcs7/AOAlgorithmID.java
+++ afirma-crypto-core-pkcs7/src/main/java/es/gob/afirma/signers/pkcs7/AOAlgorithmID.java
@@ -18,37 +18,45 @@ import java.util.Hashtable;
  */
 public final class AOAlgorithmID {
     
+    private static final String OID_SHA1   = "1.3.14.3.2.26"; //$NON-NLS-1$
+    private static final String OID_SHA512 = "2.16.840.1.101.3.4.2.3"; //$NON-NLS-1$
+    private static final String OID_MD2    = "1.2.840.113549.2.2"; //$NON-NLS-1$
+    private static final String OID_MD5    = "1.2.840.113549.2.5"; //$NON-NLS-1$
+    private static final String OID_SHA256 = "2.16.840.1.101.3.4.2.1"; //$NON-NLS-1$
+    private static final String OID_SHA384 = "2.16.840.1.101.3.4.2.2"; //$NON-NLS-1$
+    private static final String OID_RSA    = "1.2.840.113549.1.1.1"; //$NON-NLS-1$
+    
     private AOAlgorithmID() {
         // No permitimos la instanciacion
     }
     
     private static final Dictionary<String, String> OIDS = new Hashtable<String, String>();
     static {
-        OIDS.put("SHA1", "1.3.14.3.2.26"); //$NON-NLS-1$ //$NON-NLS-2$
-        OIDS.put("SHA-1", "1.3.14.3.2.26"); //$NON-NLS-1$ //$NON-NLS-2$
-        OIDS.put("SHA", "1.3.14.3.2.26"); //$NON-NLS-1$ //$NON-NLS-2$
-        OIDS.put("1.3.14.3.2.26", "1.3.14.3.2.26"); //$NON-NLS-1$ //$NON-NLS-2$
+        OIDS.put("SHA1", OID_SHA1); //$NON-NLS-1$ 
+        OIDS.put("SHA-1", OID_SHA1); //$NON-NLS-1$ 
+        OIDS.put("SHA", OID_SHA1); //$NON-NLS-1$ 
+        OIDS.put(OID_SHA1, OID_SHA1); 
         
-        OIDS.put("SHA-512", "2.16.840.1.101.3.4.2.3"); //$NON-NLS-1$ //$NON-NLS-2$
-        OIDS.put("SHA512", "2.16.840.1.101.3.4.2.3"); //$NON-NLS-1$ //$NON-NLS-2$
-        OIDS.put("2.16.840.1.101.3.4.2.3", "2.16.840.1.101.3.4.2.3"); //$NON-NLS-1$ //$NON-NLS-2$
+        OIDS.put("SHA-512", OID_SHA512); //$NON-NLS-1$ 
+        OIDS.put("SHA512", OID_SHA512); //$NON-NLS-1$ 
+        OIDS.put(OID_SHA512, OID_SHA512); 
         
-        OIDS.put("MD2", "1.2.840.113549.2.2"); //$NON-NLS-1$ //$NON-NLS-2$
-        OIDS.put("1.2.840.113549.2.2", "1.2.840.113549.2.2"); //$NON-NLS-1$ //$NON-NLS-2$
+        OIDS.put("MD2", OID_MD2); //$NON-NLS-1$ 
+        OIDS.put(OID_MD2, OID_MD2); 
         
-        OIDS.put("MD5", "1.2.840.113549.2.5"); //$NON-NLS-1$ //$NON-NLS-2$
-        OIDS.put("1.2.840.113549.2.5", "1.2.840.113549.2.5"); //$NON-NLS-1$ //$NON-NLS-2$
+        OIDS.put("MD5", OID_MD5); //$NON-NLS-1$ 
+        OIDS.put(OID_MD5, OID_MD5); 
         
-        OIDS.put("SHA-256", "2.16.840.1.101.3.4.2.1"); //$NON-NLS-1$ //$NON-NLS-2$
-        OIDS.put("SHA256", "2.16.840.1.101.3.4.2.1"); //$NON-NLS-1$ //$NON-NLS-2$
-        OIDS.put("2.16.840.1.101.3.4.2.1", "2.16.840.1.101.3.4.2.1"); //$NON-NLS-1$ //$NON-NLS-2$
+        OIDS.put("SHA-256", OID_SHA256); //$NON-NLS-1$ 
+        OIDS.put("SHA256", OID_SHA256); //$NON-NLS-1$ 
+        OIDS.put(OID_SHA256, OID_SHA256); 
         
-        OIDS.put("SHA-384", "2.16.840.1.101.3.4.2.2"); //$NON-NLS-1$ //$NON-NLS-2$
-        OIDS.put("SHA384", "2.16.840.1.101.3.4.2.2"); //$NON-NLS-1$ //$NON-NLS-2$
-        OIDS.put("2.16.840.1.101.3.4.2.2", "2.16.840.1.101.3.4.2.2"); //$NON-NLS-1$ //$NON-NLS-2$
+        OIDS.put("SHA-384", OID_SHA384); //$NON-NLS-1$ 
+        OIDS.put("SHA384", OID_SHA384); //$NON-NLS-1$ 
+        OIDS.put(OID_SHA384, OID_SHA384); 
         
-        OIDS.put("RSA", "1.2.840.113549.1.1.1"); //$NON-NLS-1$ //$NON-NLS-2$
-        OIDS.put("1.2.840.113549.1.1.1", "1.2.840.113549.1.1.1"); //$NON-NLS-1$ //$NON-NLS-2$
+        OIDS.put("RSA", OID_RSA); //$NON-NLS-1$ 
+        OIDS.put(OID_RSA, OID_RSA); 
     }
     
     /** Obtiene el OID del algoritmo indicado.
