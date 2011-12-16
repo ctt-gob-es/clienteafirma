79b58e370c5d8d83332cc9788a62d8d892a5b6d1
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2011-12-16 14:08:28 +0000

    Mejoras en el JavaDoc
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@1516 dcdbb401-323c-49cf-88a0-803402807c85

---
+++ afirma-core/src/main/java/es/gob/afirma/core/package-info.java
@@ -0,0 +1,21 @@
+/* Copyright (C) 2011 [Gobierno de Espana]
+ * This file is part of "Cliente @Firma".
+ * "Cliente @Firma" is free software; you can redistribute it and/or modify it under the terms of:
+ *   - the GNU General Public License as published by the Free Software Foundation; 
+ *     either version 2 of the License, or (at your option) any later version.
+ *   - or The European Software License; either versión 1.1 or (at your option) any later version.
+ * Date: 11/01/11
+ * You may contact the copyright holder at: soporte.afirma5@mpt.es
+ */
+
+/** M&oacute;dulo N&uacute;cleo del Cliente @firma.
+ *  <p>
+ *   Este m&oacute;dulo proporciona los servicios comunes requeridos por el resto del m&oacute;dulos
+ *   del Cliente @firma, incoporando igualmente los interfaces base que pueden ser implementados
+ *   de forma concreta por distintos m&oacute;dulos.
+ *  </p>
+ *  <p>
+ *   Este m&oacute;dulo es compatible con cualquier entorno JSE 1.5 o superior y con Android 3 o superior.<br> 
+ *  </p>
+ */
+package es.gob.afirma.core;
