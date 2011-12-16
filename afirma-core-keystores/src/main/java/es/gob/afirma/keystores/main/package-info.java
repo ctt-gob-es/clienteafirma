12a0b69207063a9d2c0aa439d5ad8b1c76cb13d2
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2011-12-16 14:18:43 +0000

    Mejoras en el JavaDoc y cambios en el texto de licencia
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@1517 dcdbb401-323c-49cf-88a0-803402807c85

---
+++ afirma-core-keystores/src/main/java/es/gob/afirma/keystores/main/package-info.java
@@ -0,0 +1,21 @@
+/* Copyright (C) 2011 [Gobierno de Espana]
+ * This file is part of "Cliente @Firma".
+ * "Cliente @Firma" is free software; you can redistribute it and/or modify it under the terms of:
+ *   - the GNU General Public License as published by the Free Software Foundation; 
+ *     either version 2 of the License, or (at your option) any later version.
+ *   - or The European Software License; either versión 1.1 or (at your option) any later version.
+ * Date: 11/01/11
+ * You may contact the copyright holder at: soporte.afirma5@mpt.es
+ */
+
+/** M&oacute;dulo del Cliente Afirma de gesti&oacute;n de almacenes de claves y certificados.
+ *  <p>
+ *   Este m&oacute;dulo incoporando igualmente los interfaces base que pueden ser implementados
+ *   de forma concreta por distintos m&oacute;dulos que asuman funcionalidades relacionadas con
+ *   la gesti&oacute;n de almacenes de claves y certificados.
+ *  </p>
+ *  <p>
+ *   Este m&oacute;dulo es compatible con cualquier entorno JSE 1.5 o superior y con Android 3 o superior.<br> 
+ *  </p>
+ */
+package es.gob.afirma.keystores.main;
