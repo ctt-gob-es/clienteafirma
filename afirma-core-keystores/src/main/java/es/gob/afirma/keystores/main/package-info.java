c831f04c6d044b9cd140bee70183f648e3e861b0
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2011-12-16 19:31:00 +0000

    Mejoras en el JavaDoc
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@1540 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-core-keystores/src/main/java/es/gob/afirma/keystores/main/package-info.java
+++ afirma-core-keystores/src/main/java/es/gob/afirma/keystores/main/package-info.java
@@ -14,8 +14,23 @@
  *   de forma concreta por distintos m&oacute;dulos que asuman funcionalidades relacionadas con
  *   la gesti&oacute;n de almacenes de claves y certificados.
  *  </p>
+ *  <p align="center"><br><img src="doc-files/package-info-1.png"></p>
+ *  <p>Este m&oacute;dulo presenta las siguientes dependencias directas de primer nivel:</p>
+ *  <ul>
+ *   <li>Dependencia con el m&oacute;dulo N&uacute;cleo (<i>afirma-core</i>) del Cliente.</li>
+ *  </ul>
+ *  <p>Adicionalmente, se presentan las siguientes dependencias din&aacute;micas de primer nivel:</p>
+ *  <ul>
+ *   <li>
+ *    Dependencia din&aacute;mica con el m&oacute;dulo Núcleo UI JSE (<i>afirma-ui-core-jse</i>) del Cliente.
+ *    Este puede sustituirse por cualquier otra clase que implemente el interfaz <code>es.gob.afirma.core.ui.AOUIManager</code>.
+ *   </li>
+ *  </ul>
  *  <p>
  *   Este m&oacute;dulo es compatible con cualquier entorno JSE 1.5 o superior y con Android 3 o superior.<br> 
+ *  </p>
+ *  <p>
+ *   Desde este m&oacute;dulo es posible que se realicen llamadas a interfaces gr&aacute;ficas.<br> 
  *  </p>
  */
 package es.gob.afirma.keystores.main;
