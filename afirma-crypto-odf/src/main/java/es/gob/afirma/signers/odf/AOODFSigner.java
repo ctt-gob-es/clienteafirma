fac23e88e692672468415413612eb92222a0181e
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2011-12-16 15:08:07 +0000

    Cambio en el texto de licencia
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@1525 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-crypto-odf/src/main/java/es/gob/afirma/signers/odf/AOODFSigner.java
+++ afirma-crypto-odf/src/main/java/es/gob/afirma/signers/odf/AOODFSigner.java
@@ -1,12 +1,12 @@
-/*******************************************************************************
- * Este fichero forma parte del Cliente @firma.
- * El Cliente @firma es un aplicativo de libre distribucion cuyo codigo fuente puede ser consultado
- * y descargado desde http://forja-ctt.administracionelectronica.gob.es/
- * Copyright 2009,2010,2011 Gobierno de Espana
- * Este fichero se distribuye bajo  bajo licencia GPL version 2  segun las
- * condiciones que figuran en el fichero 'licence' que se acompana. Si se distribuyera este
- * fichero individualmente, deben incluirse aqui las condiciones expresadas alli.
- ******************************************************************************/
+/* Copyright (C) 2011 [Gobierno de Espana]
+ * This file is part of "Cliente @Firma".
+ * "Cliente @Firma" is free software; you can redistribute it and/or modify it under the terms of:
+ *   - the GNU General Public License as published by the Free Software Foundation; 
+ *     either version 2 of the License, or (at your option) any later version.
+ *   - or The European Software License; either versión 1.1 or (at your option) any later version.
+ * Date: 11/01/11
+ * You may contact the copyright holder at: soporte.afirma5@mpt.es
+ */
 
 package es.gob.afirma.signers.odf;
 
