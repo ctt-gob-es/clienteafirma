1a5e2c3c42a799843e1436e09c895a158141aa40
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2011-12-19 10:54:46 +0000

    Reubicación de la detección de factura electrónica
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@1560 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-ui-simpleafirma/src/main/java/es/gob/afirma/standalone/DataAnalizerUtil.java
+++ afirma-ui-simpleafirma/src/main/java/es/gob/afirma/standalone/DataAnalizerUtil.java
@@ -11,8 +11,14 @@
 package es.gob.afirma.standalone;
 
 import java.io.ByteArrayInputStream;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 import es.gob.afirma.signers.pades.AOPDFSigner;
 
@@ -20,7 +26,11 @@ import es.gob.afirma.signers.pades.AOPDFSigner;
  * M&eacute;todos de utilidad para el an&aacute;lisis de ficheros de datos.
  * @author Carlos Gamuci
  */
-public class DataAnalizerUtil {
+public final class DataAnalizerUtil {
+    
+    private DataAnalizerUtil() {
+        // No permitimos la instanciacion
+    }
 
     /**
      * Comprueba si los datos introducidos se corresponden a un fichero XML.
@@ -49,5 +59,48 @@ public class DataAnalizerUtil {
         catch(final Exception e) {
             return false;
         }
+    }
+    
+    /** Indica si los datos son una factura electr&oacute;nica.
+     * @param file Datos a comprobar
+     * @return <code>true</code> si los datos son una <a href="http://www.facturae.es/">factura electr&oacute;nica</a>,
+     *         <code>false</code> en caso contrario */
+    public static boolean isFacturae(final byte[] file) {
+        if (file == null || file.length == 0) {
+            return false;
+        }
+        final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+        
+        try {
+            final Document doc = dbf.newDocumentBuilder().parse(new ByteArrayInputStream(file));
+            final Element rootNode = doc.getDocumentElement();
+            final String rootNodePrefix = rootNode.getPrefix();
+            
+            if (!(((rootNodePrefix != null) ? (rootNodePrefix + ":") : "") + "Facturae").equals(rootNode.getNodeName())) { //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+                return false;
+            }
+            
+            final Set<String> childs = new HashSet<String>(3);
+            childs.add("FileHeader"); //$NON-NLS-1$
+            childs.add("Parties"); //$NON-NLS-1$
+            childs.add("Invoices"); //$NON-NLS-1$
+            
+            final NodeList nl = rootNode.getChildNodes();
+            for (int i=0;i<nl.getLength();i++) {
+                final String nodeName = nl.item(i).getNodeName();
+                if (childs.contains(nodeName)) {
+                    childs.remove(nodeName);
+                }
+            }
+            if (childs.size() > 0) {
+                return false;
+            }
+            
+        }
+        catch (final Exception e) {
+            return false;
+        }
+        return true;
     }
 }
