2ef3f59a927e0a6588fca5e37d91f978a4fde786
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2012-01-12 16:56:39 +0000

    Se elimina el parámetro que configuraba si debía aparecer la transformación XPATH que permitía la validación de las cofirmas.
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@1811 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-crypto-xades/src/main/java/es/gob/afirma/signers/xades/AOFacturaESigner.java
+++ afirma-crypto-xades/src/main/java/es/gob/afirma/signers/xades/AOFacturaESigner.java
@@ -44,7 +44,6 @@ public final class AOFacturaESigner implements AOSigner {
         EXTRA_PARAMS.setProperty("policyIdentifierHashAlgorithm", DigestMethod.SHA1);         //$NON-NLS-1$
         EXTRA_PARAMS.setProperty("policyDescription", "facturae31"); //$NON-NLS-1$ //$NON-NLS-2$
         EXTRA_PARAMS.setProperty("signerClaimedRole", "emisor"); //$NON-NLS-1$ //$NON-NLS-2$
-        EXTRA_PARAMS.setProperty("avoidTransformForEnvelopedCosign", "true"); //$NON-NLS-1$ //$NON-NLS-2$
     }
     
     /** Cofirma Facturas en formato XAdES Factura-E.
