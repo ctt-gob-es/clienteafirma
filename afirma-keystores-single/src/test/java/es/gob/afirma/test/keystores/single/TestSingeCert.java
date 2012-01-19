e2f215905ca400d9c0f42884750a29bb1a001897
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2012-01-19 17:50:16 +0000

    Correcciones menores de código
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@1922 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-keystores-single/src/test/java/es/gob/afirma/test/keystores/single/TestSingeCert.java
+++ afirma-keystores-single/src/test/java/es/gob/afirma/test/keystores/single/TestSingeCert.java
@@ -12,20 +12,21 @@ import es.gob.afirma.keystores.single.SingleCertKeyStoreProvider;
 /** Pruebas de apertura de certificados sueltos.
  * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s */
 public class TestSingeCert {
-    
+
     private static String[] TEST_FILES = new String[] {
            "b64cert1.txt" //$NON-NLS-1$
     };
-    
+
     /** Pruebas de apertura de certificados sueltos.
      * @throws Exception */
-    @Test
+    @SuppressWarnings("static-method")
+	@Test
     public void testSingleKeyStore() throws Exception {
-        
+
         final Provider p = new SingleCertKeyStoreProvider();
         Security.addProvider(p);
         final KeyStore ks = KeyStore.getInstance("PKCS7", p); //$NON-NLS-1$
-        
+
         for(final String filename : TEST_FILES) {
             ks.load(ClassLoader.getSystemResourceAsStream(filename), null);
             final Enumeration<String> aliases = ks.aliases();
