8f788a4bef8800e5a63bf4672e4db8445520bea9
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2012-01-25 14:37:14 +0000

    Test para la comprobación de la funcionalidad de la clase CMSInformation
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@1978 dcdbb401-323c-49cf-88a0-803402807c85

---
+++ afirma-ui-applet/src/test/java/es/gob/afirma/applet/pkcs7/CMSinformationTest.java
@@ -0,0 +1,58 @@
+package es.gob.afirma.applet.pkcs7;
+
+import java.io.IOException;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import es.gob.afirma.applet.GenerateAllSigns;
+import es.gob.afirma.core.misc.AOUtil;
+
+/**
+ * Prueba la clase para extraer informaci&oacute;n de los envoltorios CMS.
+ */
+public class CMSinformationTest {
+
+	private static final String[] FILES = {
+		"cms_signed_data", //$NON-NLS-1$
+//		"cms_encrypted_data", //$NON-NLS-1$
+		"cms_enveloped_data", //$NON-NLS-1$
+		"cms_signed_enveloped_data", //$NON-NLS-1$
+//		"cms_authenticated_data", //$NON-NLS-1$
+		"cms_auth_enveloped_data" //$NON-NLS-1$
+	};
+
+	/**
+	 * Extrae informacion de una firma CMS.
+	 */
+	@Test
+	public void cmsSignedDataTest() {
+
+		for (final String filename : FILES) {
+
+			final byte[] cmsStructure = getResource(filename);
+			Assert.assertNotNull("No se ha encontrado el fichero de recursos " + filename, cmsStructure); //$NON-NLS-1$
+
+			final String info;
+			try {
+				info = CMSInformation.getInformation(cmsStructure);
+				Assert.assertNotNull(info);
+			} catch (final Exception e) {
+				Assert.fail("No se ha podido recuperar la informacion del envoltorio: " + e); //$NON-NLS-1$
+				return;
+			}
+
+			System.out.println(info);
+			System.out.println("---"); //$NON-NLS-1$
+		}
+	}
+
+    private static byte[] getResource(final String filename) {
+    	try {
+			return AOUtil.getDataFromInputStream(GenerateAllSigns.class.getResourceAsStream("/" + filename)); //$NON-NLS-1$
+		} catch (final IOException e) {
+			return null;
+		}
+    }
+}
