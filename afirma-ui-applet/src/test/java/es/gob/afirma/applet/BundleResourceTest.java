9a1da9bc69de20a467c0338b373533666cf25ddf
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2012-01-25 17:42:35 +0000

    Clase de pruebas para localizar los textos que faltan en los ficheros de idioma secundarios, encontrar los textos sobrantes en los mismos y cadenas duplicadas en el fichero de idioma principal
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@1984 dcdbb401-323c-49cf-88a0-803402807c85

---
+++ afirma-ui-applet/src/test/java/es/gob/afirma/applet/BundleResourceTest.java
@@ -0,0 +1,126 @@
+package es.gob.afirma.applet;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Hashtable;
+import java.util.Properties;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+/**
+ * Valida que los ficheros de idioma contengan en todas los idiomas las mismas
+ * entradas que en espa&ntilde;ol.
+ */
+public class BundleResourceTest {
+
+	private final static String MAIN_LANGUAGE_FILE = "appletmessages.properties"; //$NON-NLS-1$
+
+	private final static String[] OTHER_LANGUAGES_FILES = {
+		"appletmessages_de.properties", //$NON-NLS-1$
+		"appletmessages_el.properties", //$NON-NLS-1$
+		"appletmessages_en.properties", //$NON-NLS-1$
+		"appletmessages_fr.properties", //$NON-NLS-1$
+		"appletmessages_it.properties", //$NON-NLS-1$
+		"appletmessages_lt.properties", //$NON-NLS-1$
+	};
+
+	/**
+	 * Comprueba que todos los recursos secundarios declarados tengan definidas
+	 * las mismas claves que el fichero principal.
+	 */
+	@Test
+	public void missingResourcesTest() {
+
+		final Properties mainResource = new Properties();
+		try {
+			mainResource.load(getResourceAsStream(MAIN_LANGUAGE_FILE));
+		} catch (final IOException e) {
+			Assert.fail("No se pudo cargar el fichero principal de recursos"); //$NON-NLS-1$
+		}
+
+		for (final String file : OTHER_LANGUAGES_FILES) {
+			final Properties secondaryResource = new Properties();
+			try {
+				secondaryResource.load(getResourceAsStream(file));
+			} catch (final IOException e) {
+				Assert.fail("No se pudo cargar el recurso " + file + ": " + e); //$NON-NLS-1$ //$NON-NLS-2$
+			}
+
+			String missingKeys = "\n"; //$NON-NLS-1$
+			for (final String key : mainResource.keySet().toArray(new String[0])) {
+				if (!secondaryResource.containsKey(key)) {
+					missingKeys += key + "\n"; //$NON-NLS-1$
+				}
+			}
+			Assert.assertTrue("En el fichero " + file + " faltan las entradas:" + missingKeys, //$NON-NLS-1$ //$NON-NLS-2$
+					missingKeys.trim().length() == 0);
+		}
+	}
+
+	/**
+	 * Comprueba que los recursos secundarios no tengan definidas entradas adicionales
+	 * con respecto al recurso principal.
+	 */
+	@Test
+	public void extraResourcesTest() {
+		final Properties mainResource = new Properties();
+		try {
+			mainResource.load(getResourceAsStream(MAIN_LANGUAGE_FILE));
+		} catch (final IOException e) {
+			Assert.fail("No se pudo cargar el fichero principal de recursos"); //$NON-NLS-1$
+		}
+
+		for (final String file : OTHER_LANGUAGES_FILES) {
+			final Properties secondaryResource = new Properties();
+			try {
+				secondaryResource.load(getResourceAsStream(file));
+			} catch (final IOException e) {
+				Assert.fail("No se pudo cargar el recurso " + file + ": " + e); //$NON-NLS-1$ //$NON-NLS-2$
+			}
+
+			String missingKeys = "\n"; //$NON-NLS-1$
+			for (final String key : secondaryResource.keySet().toArray(new String[0])) {
+				if (!mainResource.containsKey(key)) {
+					missingKeys += key + "\n"; //$NON-NLS-1$
+				}
+			}
+			Assert.assertTrue("El fichero " + file + //$NON-NLS-1$
+					" contiene las siguientes entradas no existentes en el idioma principal:" + missingKeys, //$NON-NLS-1$
+					missingKeys.trim().length() == 0);
+		}
+	}
+
+
+	/**
+	 * Comprueba que no existan cadenas duplicadas dentro del fichero principal.
+	 */
+	@Test
+	public void duplicateResourcesTest() {
+		final Properties mainResource = new Properties();
+		try {
+			mainResource.load(getResourceAsStream(MAIN_LANGUAGE_FILE));
+		} catch (final IOException e) {
+			Assert.fail("No se pudo cargar el fichero principal de recursos"); //$NON-NLS-1$
+		}
+
+		String duplicateStrings = "\n"; //$NON-NLS-1$
+		final Hashtable<String, String> resourceKeyRel = new Hashtable<String, String>();
+		for (final String key : mainResource.keySet().toArray(new String[0])) {
+			final String text = mainResource.getProperty(key);
+			if (!resourceKeyRel.containsKey(text)) {
+				resourceKeyRel.put(text, key);
+			} else {
+				duplicateStrings += (resourceKeyRel.get(text) + " --> " + key + "\n"); //$NON-NLS-1$ //$NON-NLS-2$
+			}
+		}
+		Assert.assertTrue("En el fichero principal (" + MAIN_LANGUAGE_FILE + //$NON-NLS-1$
+				") se han encontrado recursos cuplicados:" + duplicateStrings, //$NON-NLS-1$
+				duplicateStrings.trim().length() == 0);
+	}
+
+    private static InputStream getResourceAsStream(final String filename) {
+    	return GenerateAllSigns.class.getResourceAsStream("/" + filename); //$NON-NLS-1$
+    }
+}
