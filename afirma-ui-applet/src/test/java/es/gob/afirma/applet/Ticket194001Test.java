16bcf604cd0e989c2640e2e22abeb7a82b1a11e1
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2012-01-25 08:59:03 +0000

    Pruebas para la comprobación de la incidencia 194001
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@1964 dcdbb401-323c-49cf-88a0-803402807c85

---
+++ afirma-ui-applet/src/test/java/es/gob/afirma/applet/Ticket194001Test.java
@@ -0,0 +1,170 @@
+package es.gob.afirma.applet;
+
+import java.io.File;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import es.gob.afirma.core.signers.AOSignConstants;
+
+/**
+ * Genera firmas para la comprobaci&oacute;n manual de la resoluci&oacute;n del Ticket 194001.
+ */
+public class Ticket194001Test {
+
+	private static final String CERT_PATH = "ANF_PF_Activo.pfx"; //$NON-NLS-1$
+    private static final String CERT_PASS = "12341234"; //$NON-NLS-1$
+    private static final String CERT_ALIAS = "anf usuario activo"; //$NON-NLS-1$
+
+    private static final String DATA_FILE = "bin"; //$NON-NLS-1$
+
+    /** Genera una firma acorde al est&aacute;ndar XAdES 1.2.2. */
+    @Test
+	public void signTest() {
+
+    	final String path = new File("").getAbsolutePath(); //$NON-NLS-1$
+
+    	final SignApplet applet = new SignApplet();
+    	applet.initialize();
+    	final String ksPath = this.getResourcePath(CERT_PATH);
+    	applet.setKeyStore(ksPath, CERT_PASS, "PKCS12"); //$NON-NLS-1$
+    	applet.setSelectedCertificateAlias(CERT_ALIAS);
+
+    	applet.setSignatureFormat(AOSignConstants.SIGN_FORMAT_XADES);
+    	applet.setSignatureMode(AOSignConstants.SIGN_MODE_IMPLICIT);
+    	applet.addExtraParam("xadesNamespace", "http://uri.etsi.org/01903/v1.2.2#"); //$NON-NLS-1$ //$NON-NLS-2$
+    	applet.addExtraParam("signedPropertiesTypeUrl", "http://uri.etsi.org/01903/v1.2.2#SignedProperties"); //$NON-NLS-1$ //$NON-NLS-2$
+
+
+    	applet.setFileuri(this.getResourcePathToApplet(DATA_FILE));
+
+    	applet.sign();
+    	Assert.assertFalse(applet.getErrorMessage(), applet.isError());
+    	applet.setOutFilePath(path + "/" + "194001_sign.xsig"); //$NON-NLS-1$ //$NON-NLS-2$
+    	applet.saveSignToFile();
+	}
+
+    /** Genera una cofirma acorde al est&aacute;ndar XAdES 1.2.2. */
+    @Test
+	public void cosignTest() {
+
+    	final String path = new File("").getAbsolutePath(); //$NON-NLS-1$
+
+    	final SignApplet applet = new SignApplet();
+    	applet.initialize();
+    	final String ksPath = this.getResourcePath(CERT_PATH);
+    	applet.setKeyStore(ksPath, CERT_PASS, "PKCS12"); //$NON-NLS-1$
+    	applet.setSelectedCertificateAlias(CERT_ALIAS);
+
+    	applet.setSignatureFormat(AOSignConstants.SIGN_FORMAT_XADES);
+    	applet.setSignatureMode(AOSignConstants.SIGN_MODE_IMPLICIT);
+    	applet.addExtraParam("xadesNamespace", "http://uri.etsi.org/01903/v1.2.2#"); //$NON-NLS-1$ //$NON-NLS-2$
+    	applet.addExtraParam("signedPropertiesTypeUrl", "http://uri.etsi.org/01903/v1.2.2#SignedProperties"); //$NON-NLS-1$ //$NON-NLS-2$
+
+
+    	applet.setFileuri(this.getResourcePathToApplet(DATA_FILE));
+
+    	applet.sign();
+    	Assert.assertFalse(applet.getErrorMessage(), applet.isError());
+
+    	String signatureB64 = applet.getSignatureBase64Encoded();
+    	Assert.assertNotNull(signatureB64);
+
+    	applet.initialize();
+
+    	applet.setKeyStore(ksPath, CERT_PASS, "PKCS12"); //$NON-NLS-1$
+    	applet.setSelectedCertificateAlias(CERT_ALIAS);
+
+    	applet.setSignatureFormat(AOSignConstants.SIGN_FORMAT_XADES);
+    	applet.setSignatureMode(AOSignConstants.SIGN_MODE_IMPLICIT);
+    	applet.addExtraParam("xadesNamespace", "http://uri.etsi.org/01903/v1.2.2#"); //$NON-NLS-1$ //$NON-NLS-2$
+    	applet.addExtraParam("signedPropertiesTypeUrl", "http://uri.etsi.org/01903/v1.2.2#SignedProperties"); //$NON-NLS-1$ //$NON-NLS-2$
+
+    	applet.setFileuri(this.getResourcePathToApplet(DATA_FILE));
+    	applet.setElectronicSignature(signatureB64);
+
+    	applet.coSign();
+    	Assert.assertFalse(applet.getErrorMessage(), applet.isError());
+
+    	signatureB64 = applet.getSignatureBase64Encoded();
+    	Assert.assertNotNull(signatureB64);
+
+    	applet.setOutFilePath(path + "/" + "194001_cosign.xsig"); //$NON-NLS-1$ //$NON-NLS-2$
+    	applet.saveSignToFile();
+	}
+
+    /** Genera una cofirma acorde al est&aacute;ndar XAdES 1.2.2. */
+    @Test
+	public void countersignTest() {
+
+    	final String path = new File("").getAbsolutePath(); //$NON-NLS-1$
+
+    	final SignApplet applet = new SignApplet();
+    	applet.initialize();
+    	final String ksPath = this.getResourcePath(CERT_PATH);
+    	applet.setKeyStore(ksPath, CERT_PASS, "PKCS12"); //$NON-NLS-1$
+    	applet.setSelectedCertificateAlias(CERT_ALIAS);
+
+    	applet.setSignatureFormat(AOSignConstants.SIGN_FORMAT_XADES);
+    	applet.setSignatureMode(AOSignConstants.SIGN_MODE_IMPLICIT);
+    	applet.addExtraParam("xadesNamespace", "http://uri.etsi.org/01903/v1.2.2#"); //$NON-NLS-1$ //$NON-NLS-2$
+    	applet.addExtraParam("signedPropertiesTypeUrl", "http://uri.etsi.org/01903/v1.2.2#SignedProperties"); //$NON-NLS-1$ //$NON-NLS-2$
+
+    	applet.setFileuri(this.getResourcePathToApplet(DATA_FILE));
+
+    	applet.sign();
+    	Assert.assertFalse(applet.getErrorMessage(), applet.isError());
+
+    	String signatureB64 = applet.getSignatureBase64Encoded();
+    	Assert.assertNotNull(signatureB64);
+
+    	applet.initialize();
+
+    	applet.setKeyStore(ksPath, CERT_PASS, "PKCS12"); //$NON-NLS-1$
+    	applet.setSelectedCertificateAlias(CERT_ALIAS);
+
+    	applet.setSignatureFormat(AOSignConstants.SIGN_FORMAT_XADES);
+    	applet.setSignatureMode(AOSignConstants.SIGN_MODE_IMPLICIT);
+    	applet.addExtraParam("xadesNamespace", "http://uri.etsi.org/01903/v1.2.2#"); //$NON-NLS-1$ //$NON-NLS-2$
+    	applet.addExtraParam("signedPropertiesTypeUrl", "http://uri.etsi.org/01903/v1.2.2#SignedProperties"); //$NON-NLS-1$ //$NON-NLS-2$
+
+    	applet.setFileuri(this.getResourcePathToApplet(DATA_FILE));
+    	applet.setElectronicSignature(signatureB64);
+
+    	applet.coSign();
+    	Assert.assertFalse(applet.getErrorMessage(), applet.isError());
+
+    	signatureB64 = applet.getSignatureBase64Encoded();
+    	Assert.assertNotNull(signatureB64);
+
+    	applet.initialize();
+
+    	applet.setKeyStore(ksPath, CERT_PASS, "PKCS12"); //$NON-NLS-1$
+    	applet.setSelectedCertificateAlias(CERT_ALIAS);
+
+    	applet.setSignatureFormat(AOSignConstants.SIGN_FORMAT_XADES);
+    	applet.setSignatureMode(AOSignConstants.SIGN_MODE_IMPLICIT);
+    	applet.addExtraParam("xadesNamespace", "http://uri.etsi.org/01903/v1.2.2#"); //$NON-NLS-1$ //$NON-NLS-2$
+    	applet.addExtraParam("signedPropertiesTypeUrl", "http://uri.etsi.org/01903/v1.2.2#SignedProperties"); //$NON-NLS-1$ //$NON-NLS-2$
+
+    	applet.setElectronicSignature(signatureB64);
+
+    	applet.counterSignLeafs();
+    	Assert.assertFalse(applet.getErrorMessage(), applet.isError());
+
+    	signatureB64 = applet.getSignatureBase64Encoded();
+    	Assert.assertNotNull(signatureB64);
+
+    	applet.setOutFilePath(path + "/" + "194001_countersign.xsig"); //$NON-NLS-1$ //$NON-NLS-2$
+    	applet.saveSignToFile();
+	}
+
+    private String getResourcePath(final String filename) {
+    	return GenerateAllSigns.class.getResource("/" + filename).toString().substring(6); //$NON-NLS-1$
+    }
+
+    private String getResourcePathToApplet(final String filename) {
+    	return GenerateAllSigns.class.getResource("/" + filename).toString(); //$NON-NLS-1$
+    }
+}
