e1777f0f74731738904387ccd3f9f029f0b17ec0
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2012-01-26 18:54:53 +0000

    Correcciones menores de código
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@2020 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-ui-simpleafirma/src/main/java/es/gob/afirma/standalone/ui/ShowFileLinkAction.java
+++ afirma-ui-simpleafirma/src/main/java/es/gob/afirma/standalone/ui/ShowFileLinkAction.java
@@ -1,7 +1,7 @@
 /* Copyright (C) 2011 [Gobierno de Espana]
  * This file is part of "Cliente @Firma".
  * "Cliente @Firma" is free software; you can redistribute it and/or modify it under the terms of:
- *   - the GNU General Public License as published by the Free Software Foundation; 
+ *   - the GNU General Public License as published by the Free Software Foundation;
  *     either version 2 of the License, or (at your option) any later version.
  *   - or The European Software License; either version 1.1 or (at your option) any later version.
  * Date: 11/01/11
@@ -32,7 +32,7 @@ final class ShowFileLinkAction {
 
     ShowFileLinkAction(final String text, final byte[] data) {
         this.text = text;
-        this.data = data;
+        this.data = data.clone();
     }
 
     void action() {
