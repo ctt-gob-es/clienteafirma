28a89a36d2bd8331db466682d94d65f9f68fb17a
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2012-01-26 19:55:46 +0000

    Correcciones menores de código
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@2030 dcdbb401-323c-49cf-88a0-803402807c85

---
+++ afirma-ui-standalone/src/test/java/es/gob/afirma/ui/wizardutils/BotoneraInferiorAccessibilityTest.java
@@ -0,0 +1,98 @@
+package es.gob.afirma.ui.wizardutils;
+
+import static org.junit.Assert.assertTrue;
+
+import java.awt.Component;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Logger;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import es.gob.afirma.ui.wizardUtils.BotoneraInferior;
+import es.gob.afirma.ui.wizardUtils.JDialogWizard;
+
+
+/**
+ * Testeo de accesibilidad para la clase BotoneraInferior.
+ * @author lmerayo
+ *
+ */
+public class BotoneraInferiorAccessibilityTest {
+
+	/**
+	 * Log.
+	 */
+	static Logger logger = Logger.getLogger(BotoneraInferiorAccessibilityTest.class.getName());
+
+
+	/**
+	 * Comprobacion de que el campo Mnemocic de los botones no este duplicado.
+	 */
+	@Test
+	public void testNotDuplicatedDisplayedMnemonic() {
+		logger.info("testNotDuplicatedDisplayedMnemonic"); //$NON-NLS-1$
+
+		try {
+			//Instancia del panel que se va a analizar
+			//Lista de dialogos
+			final List<JDialogWizard> dialogs = new ArrayList<JDialogWizard>();
+			dialogs.add(new JDialogWizard());
+			//Panel botonera inferior
+			final BotoneraInferior botoneraInferior = new BotoneraInferior(dialogs, 1);
+
+			//Lista de mnemonicos
+			final List <Integer> keyCodes = new ArrayList<Integer>();
+			//Conjunto de mnemonicos
+			Set <Integer> keyCodesSet = null;
+
+			//Se llama al metodo que obtiene una lista de codigos de atajos asociados a los componentes del panel
+			getKeyCodeList (botoneraInferior, keyCodes);
+
+			//Se crea un conjunto a partir de la lista para eliminar duplicados
+			keyCodesSet = new HashSet<Integer>(keyCodes);
+			//Si el tamano de la lista y del conjunto no son iguales, no hay duplicados
+			assertTrue(keyCodesSet.size() == keyCodes.size());
+		}
+		catch(final java.awt.HeadlessException e) {
+			// Ignoramos este error, pero no otros, para evitar fallos en tests automaticos en servidor
+		}
+	}
+
+	/**
+	 * Metodo que obtiene una lista de codigos de atajos a los componentes botones de un panel.
+	 */
+	@Ignore
+	private void getKeyCodeList(final JPanel panel, final List <Integer> keyCodeList) {
+		//Array de componentes del panel
+		final Component[] components = panel.getComponents();
+		int keyCode = 0;
+		for (int i = 0; i < components.length; i++) {
+			//Se obtiene el componente
+			final Component component = panel.getComponent(i);
+			if (!(component instanceof JPanel)) {
+				if (component instanceof JButton) {
+					final JButton button = (JButton) component;
+					//Se obtiene el codigo del atajo asociado
+					keyCode = button.getMnemonic();
+					//Se anade a la lista si existe este codigo, es decir, si es distinto de 0
+					if (keyCode != 0) {
+						keyCodeList.add(new Integer(keyCode));
+					}
+				}
+
+			} else {
+				//Si es un panel se vuelve a llamar recursivamente al metodo
+				getKeyCodeList((JPanel)component, keyCodeList);
+			}
+		} //for
+	}//getKeyCodeList
+
+
+}
