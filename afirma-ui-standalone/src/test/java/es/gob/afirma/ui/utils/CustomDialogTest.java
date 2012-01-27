e6ce0746fe96654e90ae715880d4df40f7baa138
Javier Morais Romero <javier.morais@inteco.es>
2012-01-27 08:22:42 +0000

    test
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@2103 dcdbb401-323c-49cf-88a0-803402807c85

---
+++ afirma-ui-standalone/src/test/java/es/gob/afirma/ui/utils/CustomDialogTest.java
@@ -0,0 +1,160 @@
+package es.gob.afirma.ui.utils;
+
+import static org.junit.Assert.*;
+
+import java.awt.event.KeyEvent;
+import java.util.logging.Logger;
+
+import javax.swing.JButton;
+import javax.swing.JLayeredPane;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRootPane;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Testeo de funcionalidades relativas a la accesibilidad de la clase CustomDialog
+ * @author inteco
+ *
+ */
+public class CustomDialogTest {
+
+	/**
+	 * Log.
+	 */
+	private static Logger LOGGER = Logger.getLogger(CustomDialogTest.class.getName());
+	
+	/**
+	 * Comprobaci&oacute;n de las caracter&iacute;sticas de accesibilidad de los CustomDialog
+	 */
+	@Test
+	public void testShowMessageDialog(){
+		LOGGER.info("testShowMessageDialog"); //$NON-NLS-1$
+		
+		String message = new String("Test");
+		String title = new String("Test");
+		CustomDialog.showMessageDialog(null, true, message, title, JOptionPane.INFORMATION_MESSAGE);
+		CustomDialog cD = CustomDialog.getInstanceCustomDialog(null, true, message, title, JOptionPane.INFORMATION_MESSAGE, false);
+		assertTrue(checkComponentMessageDialog(cD));
+		
+	}
+	
+	/**
+	 * Comprobaci&oacute;n de las caracter&iacute;sticas de accesibilidad de los CustomDialog
+	 */
+	@Test
+	public void testShowConfirmDialog(){
+		LOGGER.info("testShowConfirmDialog"); //$NON-NLS-1$
+		
+		String message = new String("Test");
+		String title = new String("Test");
+		CustomDialog.showConfirmDialog(null, true, message, title,JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE);
+		CustomDialog cD = CustomDialog.getInstanceCustomDialog(null, true, message, title, JOptionPane.INFORMATION_MESSAGE, false);
+		assertTrue(checkComponentMessageDialog(cD));
+		CustomDialog.showConfirmDialog(null, true, message, title,JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE);
+		cD = CustomDialog.getInstanceCustomDialog(null, true, message, title, JOptionPane.INFORMATION_MESSAGE, false);
+		assertTrue(checkComponentMessageDialog(cD));
+		CustomDialog.showConfirmDialog(null, true, message, title,JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE);
+		cD = CustomDialog.getInstanceCustomDialog(null, true, message, title, JOptionPane.INFORMATION_MESSAGE, false);
+		assertTrue(checkComponentMessageDialog(cD));
+		
+	}
+	
+	/**
+	 * Comprobaci&oacute;n de las caracter&iacute;sticas de accesibilidad de los CustomDialog
+	 */
+	@Test
+	public void testShowInputDialog(){
+		LOGGER.info("testShowInputDialog"); //$NON-NLS-1$
+		
+		String message = new String("Test");
+		String title = new String("Test");
+		CustomDialog.showInputDialog(null, true, message,KeyEvent.VK_E ,title, JOptionPane.INFORMATION_MESSAGE);
+		CustomDialog cD = CustomDialog.getInstanceCustomDialog(null, true, message, title, JOptionPane.INFORMATION_MESSAGE, true);
+		assertTrue(checkComponentMessageDialog(cD));
+		
+	}
+	
+	/**
+	 * Comprobaci&oacute;n de las caracter&iacute;sticas de accesibilidad de los CustomDialog
+	 */
+	@Test
+	public void testShowInputPasswordDialog(){
+		LOGGER.info("testShowInputPasswordDialog"); //$NON-NLS-1$
+		
+		String message = new String("Test");
+		String title = new String("Test");
+		String charSet;
+		
+		charSet = null; 
+		CustomDialog.showInputPasswordDialog(null, true, charSet,false,message,KeyEvent.VK_E ,title, JOptionPane.QUESTION_MESSAGE);
+		CustomDialog cD = CustomDialog.getInstanceCustomDialog(null, true, message, title, JOptionPane.QUESTION_MESSAGE, true);
+		assertTrue(checkComponentMessageDialog(cD));
+		
+	}
+	
+	/**
+	 * Comprueba si la etiqueta del CustomDialog puede recibir el foco para ser leida por los lectores de pantalla.
+	 * Comprueba si los botones del CustomDialog tienen asiganado un mnem&oacute;nico. 
+	 * @param cD CustomDialog a comprobar
+	 * @return boolean Resultado de la validaci&oacute;n
+	 */
+	@Ignore
+	public boolean checkComponentMessageDialog(CustomDialog cD){
+		for (int i = 0; i<cD.getComponentCount();i++){
+			if (cD.getComponent(i) instanceof JRootPane){
+				JRootPane jRP = (JRootPane)cD.getComponent(i); 
+				for (int j = 0; j<jRP.getComponentCount();j++){
+					if (jRP.getComponent(j) instanceof JLayeredPane){
+						JLayeredPane jLP = (JLayeredPane)jRP.getComponent(j);
+						for (int k=0;k<jLP.getComponentCount();k++){
+							if (jLP.getComponent(k) instanceof JPanel){
+								JPanel jP = (JPanel)jLP.getComponent(k);
+								for (int l = 0;l<jP.getComponentCount();l++){
+									if (jP.getComponent(l) instanceof JPanel){
+										JPanel jP2 = (JPanel)jP.getComponent(l);
+										for (int m=0;m<jP2.getComponentCount();m++){
+											if (jP2.getComponent(m) instanceof InfoLabel){
+												InfoLabel iL = (InfoLabel)jP2.getComponent(m);
+												if (!iL.isFocusable()){
+													return false;
+												}
+											}
+											if (jP2.getComponent(m) instanceof JPanel){
+												JPanel jP3 = (JPanel)jP2.getComponent(m);
+												for (int n=0;n<jP3.getComponentCount();n++){
+													if (jP3.getComponent(n) instanceof JButton){
+														JButton jB = (JButton)jP3.getComponent(n);
+														if (jB.getMnemonic()==0){
+															return false;
+														}
+													}
+													if (jP3.getComponent(n) instanceof JPanel){
+														JPanel jP4 = (JPanel)jP3.getComponent(n);
+														for (int c=0;c<jP4.getComponentCount();c++){
+															if (jP4.getComponent(c) instanceof JButton){
+																JButton jB2 = (JButton)jP4.getComponent(c);
+																if (jB2.getMnemonic()==0){
+																	return false;
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			
+		}		
+		return true;
+	}
+	
+}
---
+++ afirma-ui-standalone/src/test/java/es/gob/afirma/ui/utils/UtilsTest.java
@@ -0,0 +1,412 @@
+package es.gob.afirma.ui.utils;
+
+import static org.junit.Assert.*;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JEditorPane;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.JToolBar;
+import javax.swing.JTree;
+import javax.swing.JWindow;
+import javax.swing.border.TitledBorder;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import es.gob.afirma.ui.principal.AccessibilityOptionsPane;
+
+
+/**
+ * Testeo de funcionalidades relativas a la accesibilidad de la clase Utils
+ * @author inteco
+ *
+ */
+public class UtilsTest {
+
+	/**
+	 * Log.
+	 */
+	private static Logger LOGGER = Logger.getLogger(UtilsTest.class.getName());
+	
+//	/**
+//	 * Comprobaci&oacute;n de que los componentes se remarcan al recibir el foco
+//	 */
+//	@Test	
+//	public void testRemarcar(){
+//		LOGGER.info("testRemarcar"); //$NON-NLS-1$
+//		
+//		
+//		//Se obtiene la cofiguracion general
+//		//Se anade el perfil por defecto
+//		GeneralConfig.loadConfig(GeneralConfig.getConfig());
+//		Properties config = GeneralConfig.getConfig();
+//		//Se activa la opcion de remarcar elementos con foco
+//		config.setProperty(AccessibilityOptionsPane.MAIN_FOCUS_VISIBLE, "true"); //$NON-NLS-1$
+//		//Se asigna
+//		GeneralConfig.loadConfig(config);
+//		JTextField component = new JTextField();
+//		
+//		
+//		
+//		
+//		Utils.remarcar(component);
+//		
+//		//component.addAncestorListener(new RequestFocusListener(false));
+//		component.requestFocus();
+//		
+//		//component.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+//		//System.out.println(component.getBorder().getClass().getName());
+////		assertTrue(remarcarJTextField(textField));
+//		
+//		
+//	}
+//	
+//	/**
+//	 * 
+//	 * @param component
+//	 * @return
+//	 */
+//	@Ignore
+//	private boolean remarcarJTextField(JTextField component){
+//		//JPanel panel = new JPanel();
+//		component.addAncestorListener(new RequestFocusListener(false));
+//		//panel.add(component);
+//		Utils.remarcar(component);
+//		//component.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+//		System.out.println(component.getBorder().getClass().getName());
+////		if (component.getBorder()){
+////			
+////		}
+//		return false;
+//	}
+	
+	/**
+	 * Comprobaci&oacute;n de la asignaci&oacute;n autom&aacute;tica de mnem&oacute;nicos
+	 */
+	@Test	
+	public void testGetLanguageMnemonic(){
+		LOGGER.info("testGetLanguageMnemonic"); //$NON-NLS-1$		
+		
+		// Lista de mnemonicos usados
+        final List<Character> mnemonicList = new ArrayList<Character>();
+        mnemonicList.add('a');
+        mnemonicList.add('b');
+        mnemonicList.add('c');
+        
+        // Nombre del texto al que asignar mnemonico
+        String nombreConLetraLibre = new String("abcd");
+        String nombreSinLetraLibre = new String("abc");		
+		
+		assertTrue(languageMnemonicSucces(mnemonicList, nombreConLetraLibre));
+		assertTrue(languageMnemonicFail(mnemonicList, nombreSinLetraLibre));
+	}
+	
+	/**
+	 * Comprobaci&oacute;n de que los componentes cambian al modo en alto contraste correctamente
+	 */
+	@Test	
+	public void testSetContrastColor(){
+		LOGGER.info("testSetContrastColor"); //$NON-NLS-1$		
+		
+		//Se obtiene la cofiguracion general
+		GeneralConfig.loadConfig(GeneralConfig.getConfig());
+		Properties config = GeneralConfig.getConfig();
+		//Se activa la opcion de alto contraste
+		config.setProperty(AccessibilityOptionsPane.MAIN_HIGHT_CONTRAST, "true"); //$NON-NLS-1$
+		//Se asigna
+		GeneralConfig.loadConfig(config);
+		//Se crean los componentes para testear la correcta aplicacion del alto contraste
+		JComboBox jComboBox = new JComboBox();
+		assertTrue(highContrastJComboBox(jComboBox));
+		JPasswordField jPasswordField = new JPasswordField();
+		assertTrue(highContrastJPasswordField(jPasswordField));
+		JTextField jTextField = new JTextField();
+		assertTrue(highContrastJTextField(jTextField));
+		JTree jTree = new JTree();
+		assertTrue(highContrastJTree(jTree));
+		JList jList = new JList();
+		assertTrue(highContrastJList(jList));
+		JPanel jPanel = new JPanel();
+		jPanel.setBorder(BorderFactory.createTitledBorder("Test")); // NOI18N //$NON-NLS-1$
+		assertTrue(highContrastJPanel(jPanel));
+		JStatusBar jStatusBar = new JStatusBar();
+		jStatusBar.setLabelWidth((int) Toolkit.getDefaultToolkit().getScreenSize().getWidth());
+		jStatusBar.setStatus("Test"); //$NON-NLS-1$
+		assertTrue(highContrastJStatusBar(jStatusBar));
+		JEditorPane jEditorPane = new JEditorPane();
+		assertTrue(highContrastJEditorPane(jEditorPane));
+	}
+	
+	/**
+	 * Comprobaci&oacute;n de que los componentes cambian al estilo negrita correctamente
+	 */
+	@Test	
+	public void testSetFontBold(){
+		LOGGER.info("testSetFontBold"); //$NON-NLS-1$		
+		
+		//Se obtiene la cofiguracion general
+		GeneralConfig.loadConfig(GeneralConfig.getConfig());
+		Properties config = GeneralConfig.getConfig();
+		//Se activa la opcion de fuente negrita
+		config.setProperty(AccessibilityOptionsPane.MAIN_FONT_STYLE, "true"); //$NON-NLS-1$
+		//Se asigna
+		GeneralConfig.loadConfig(config);
+		//Se crean los componentes para testear la correcta aplicacion del estilo de fuente en negrita
+		JComboBox jComboBox = new JComboBox();
+		assertTrue(fontBoldJComboBox(jComboBox));
+		
+		JPanel jPanel = new JPanel();
+		jPanel.setBorder(BorderFactory.createTitledBorder("Test")); // NOI18N //$NON-NLS-1$
+		assertTrue(fontBoldJPanel(jPanel));
+
+		JToolBar jToolBar = new JToolBar();
+		JButton jButton = new JButton("Test");
+		jToolBar.add(jButton);
+		assertTrue(fontBoldJToolBar(jToolBar));
+		
+	}
+	
+	/**
+	 * Comprobaci&oacute;n del correcto subrayado de los mnem&oacute;nicos para texto HTML
+	 */
+	@Test
+	public void testRemarkMnemonic(){
+		LOGGER.info("testRemarkMnemonic"); //$NON-NLS-1$
+		
+		assertTrue(Utils.remarkMnemonic("Test", 'T').equals("<u>T</u>est"));
+		assertTrue(Utils.remarkMnemonic("Test", 'a').equals("Test"));
+	}
+	
+	/**
+	 * Comprobaci&oacute;n del correcto visionado u ocultaci&oacute;n del tool tip
+	 */
+	@Test
+	public void testShowToolTip(){
+		LOGGER.info("testShowToolTip"); //$NON-NLS-1$
+		
+		boolean show;
+		JWindow tip = new JWindow();
+		JButton boton = new JButton();
+		JLabel tipText = new JLabel();
+		
+		show = true;
+		Utils.showToolTip(show, tip, boton, tipText);
+		assertTrue(tip.isVisible()==true);
+		show = false;
+		Utils.showToolTip(show, tip, boton, tipText);
+		assertTrue(tip.isVisible()==false);
+	}
+	
+	/**
+	 * Comprueba si el componente aplica el alto contraste correctamente
+	 * @param jComboBox Componente del tipo JComboBox para el que se comprueba el cambio de color
+	 * @return Boolean indicando si pasa la validaci&oacute;n
+	 */
+	@Ignore
+	private boolean highContrastJComboBox(JComboBox jComboBox){
+		Utils.setContrastColor(jComboBox);
+		if (jComboBox.getBackground()==Color.WHITE){
+			return true;
+		}		
+		return false;
+	}
+	
+	/**
+	 * Comprueba si el componente aplica el alto contraste correctamente
+	 * @param jPasswordField Componente del tipo JPasswordField para el que se comprueba el cambio de color
+	 * @return Boolean indicando si pasa la validaci&oacute;n
+	 */
+	@Ignore
+	private boolean highContrastJPasswordField(JPasswordField jPasswordField){
+		Utils.setContrastColor(jPasswordField);
+		if (jPasswordField.getBackground()==Color.WHITE){
+			return true;
+		}		
+		return false;
+	}
+	
+	/**
+	 * Comprueba si el componente aplica el alto contraste correctamente
+	 * @param jTextField Componente del tipo JTextField para el que se comprueba el cambio de color
+	 * @return Boolean indicando si pasa la validaci&oacute;n
+	 */
+	@Ignore
+	private boolean highContrastJTextField(JTextField jTextField){
+		Utils.setContrastColor(jTextField);
+		if (jTextField.getBackground()==Color.WHITE){
+			return true;
+		}		
+		return false;
+	}
+	
+	/**
+	 * Comprueba si el componente aplica el alto contraste correctamente
+	 * @param jTree Componente del tipo JTree para el que se comprueba el cambio de color
+	 * @return Boolean indicando si pasa la validaci&oacute;n
+	 */
+	@Ignore
+	private boolean highContrastJTree(JTree jTree){
+		Utils.setContrastColor(jTree);
+		if (jTree.getForeground()==Color.WHITE){
+			return true;
+		}		
+		return false;
+	}
+	
+	/**
+	 * Comprueba si el componente aplica el alto contraste correctamente
+	 * @param jList Componente del tipo JList para el que se comprueba el cambio de color
+	 * @return Boolean indicando si pasa la validaci&oacute;n
+	 */
+	@Ignore
+	private boolean highContrastJList(JList jList){
+		Utils.setContrastColor(jList);
+		if (jList.getForeground()==Color.BLACK){
+			return true;
+		}		
+		return false;
+	}
+	
+	/**
+	 * Comprueba si el componente aplica el alto contraste correctamente
+	 * @param jPanel Componente del tipo JPanel para el que se comprueba el cambio de color
+	 * @return Boolean indicando si pasa la validaci&oacute;n
+	 */
+	@Ignore
+	private boolean highContrastJPanel(JPanel jPanel){
+		Utils.setContrastColor(jPanel);
+		if (jPanel.getBorder() != null){
+			if (jPanel.getBorder().getClass().getName().equals("javax.swing.border.TitledBorder")){ //$NON-NLS-1$
+				if (((TitledBorder)jPanel.getBorder())!=null){
+					if (((TitledBorder)jPanel.getBorder()).getTitleColor()!=Color.WHITE){
+						return false;
+					}
+				}
+			}
+		}
+		if (jPanel.getForeground()==Color.WHITE && jPanel.getBackground()==Color.BLACK){
+			return true;
+		}		
+		return false;
+	}
+	
+	/**
+	 * Comprueba si el componente aplica el alto contraste correctamente
+	 * @param jStatusBar Componente del tipo JStatusBar para el que se comprueba el cambio de color
+	 * @return Boolean indicando si pasa la validaci&oacute;n
+	 */
+	@Ignore
+	private boolean highContrastJStatusBar(JStatusBar jStatusBar){
+		Utils.setContrastColor(jStatusBar);
+		if (((JLabel)jStatusBar.getComponent(0)).getForeground()==Color.WHITE){
+			return true;
+		}		
+		return false;
+	}
+	
+	/**
+	 * Comprueba si el componente aplica el alto contraste correctamente
+	 * @param jEditorPane Componente del tipo JEditorPane para el que se comprueba el cambio de color
+	 * @return Boolean indicando si pasa la validaci&oacute;n
+	 */
+	@Ignore
+	private boolean highContrastJEditorPane(JEditorPane jEditorPane){
+		Utils.setContrastColor(jEditorPane);
+		if (jEditorPane.getBackground()==Color.BLACK){
+			return true;
+		}		
+		return false;
+	}
+	
+	/**
+	 * Comprueba si el componente aplica el estilo de fuente negrita correctamente
+	 * @param jComboBox Componente del tipo JComboBox para el que se comprueba el cambio de estilo de fuente
+	 * @return Boolean indicando si pasa la validaci&oacute;n
+	 */
+	@Ignore
+	private boolean fontBoldJComboBox(JComboBox jComboBox){
+		Utils.setFontBold(jComboBox);
+		if (jComboBox.getFont().getStyle() == Font.BOLD){
+			return true;
+		}		
+		return false;
+	}
+	
+	/**
+	 * Comprueba si el componente aplica el estilo de fuente negrita correctamente
+	 * @param jPanel Componente del tipo JPanel para el que se comprueba el cambio de estilo de fuente
+	 * @return Boolean indicando si pasa la validaci&oacute;n
+	 */
+	@Ignore
+	private boolean fontBoldJPanel(JPanel jPanel){
+		Utils.setFontBold(jPanel);
+		if (jPanel.getBorder() != null){
+			if (jPanel.getBorder().getClass().getName().equals("javax.swing.border.TitledBorder")){ //$NON-NLS-1$
+				if (((TitledBorder)jPanel.getBorder())!=null){
+					if (((TitledBorder)jPanel.getBorder()).getTitleFont().getStyle() == Font.BOLD){
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Comprueba si el componente aplica el estilo de fuente negrita correctamente
+	 * @param jComboBox Componente del tipo JComboBox para el que se comprueba el cambio de estilo de fuente
+	 * @return Boolean indicando si pasa la validaci&oacute;n
+	 */
+	@Ignore
+	private boolean fontBoldJToolBar(JToolBar jToolBar){
+		Utils.setFontBold(jToolBar);
+		for (int i=0;i<jToolBar.getComponentCount();i++){
+			if (jToolBar.getComponent(i).getFont().getStyle() == Font.BOLD){
+				return true;
+			}
+		}	
+		return false;
+	}
+	
+	/**
+	 * Comprueba si el gestor de mnem&oacute;nicos libres asigna correctamente un mnem&oacute;nico
+	 * @param mnemonicList Lista con los mnem&oacute;nicos empleados
+	 * @param nombreConLetraLibre Nombre con una letra disponible a la que asignar mnem&oacute;nicos
+	 * @return Boolean indicando si pasa la validaci&oacute;n
+	 */
+	@Ignore
+	private boolean languageMnemonicSucces(List<Character> mnemonicList, String nombreConLetraLibre){
+		if (Utils.getLanguageMnemonic(mnemonicList, nombreConLetraLibre) == 'd'){
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Comprueba si el gestor de mnem&oacute;nicos libres indica que no hay un mnem&oacute;nico disponible
+	 * @param mnemonicList Lista con los mnem&oacute;nicos empleados
+	 * @param nombreConLetraLibre Nombre que no tiene letra disponible a la que asignar mnem&oacute;nicos
+	 * @return Boolean indicando si pasa la validaci&oacute;n
+	 */
+	@Ignore
+	private boolean languageMnemonicFail(List<Character> mnemonicList, String nombreConLetraLibre){
+		if (Utils.getLanguageMnemonic(mnemonicList, nombreConLetraLibre) == 0){
+			return true;
+		}
+		return false;
+	}
+}
