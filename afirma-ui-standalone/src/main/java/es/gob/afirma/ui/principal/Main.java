5b6ec4f8f6797087e842c714cde681c010ea0046
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2012-01-27 13:43:20 +0000

    Se actualiza la versión de la aplicación
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@2119 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-ui-standalone/src/main/java/es/gob/afirma/ui/principal/Main.java
+++ afirma-ui-standalone/src/main/java/es/gob/afirma/ui/principal/Main.java
@@ -39,7 +39,7 @@ public class Main {
     static boolean showHelp = false;
 
     /** Versi&oacute;n de la interfaz gr&aacute;fica de escritorio. */
-    public static final String VERSION = "1.3"; //$NON-NLS-1$
+    public static final String VERSION = "2.0"; //$NON-NLS-1$
 
     /** Arranca la interfaz de escritorio del Cliente @firma.
      * @param args Par&aacute;metros de entrada. */
