fe9687cf1f5b591840e0daabfcf34769c77f083a
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2012-02-06 09:54:05 +0000

    Mejora del JavaDoc
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@2199 dcdbb401-323c-49cf-88a0-803402807c85

---
+++ afirma-crypto-cipher/src/main/java/es/gob/afirma/ciphers/jce/package-info.java
@@ -0,0 +1,12 @@
+/* Copyright (C) 2011 [Gobierno de Espana]
+ * This file is part of "Cliente @Firma".
+ * "Cliente @Firma" is free software; you can redistribute it and/or modify it under the terms of:
+ *   - the GNU General Public License as published by the Free Software Foundation;
+ *     either version 2 of the License, or (at your option) any later version.
+ *   - or The European Software License; either version 1.1 or (at your option) any later version.
+ * Date: 11/01/11
+ * You may contact the copyright holder at: soporte.afirma5@mpt.es
+ */
+
+/** Funcionalidades de cifrado sim&eacute;trico dependientes de JCE (<i>Java Criptography Extension</i>). */
+package es.gob.afirma.ciphers.jce;
