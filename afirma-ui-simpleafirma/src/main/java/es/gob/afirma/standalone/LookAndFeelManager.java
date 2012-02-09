45bcc0474730f3cdd2bcc03c64b2739e85b8e096
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2012-02-09 12:51:38 +0000

    Se deja el color de fondo de los paneles por defecto
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@2236 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-ui-simpleafirma/src/main/java/es/gob/afirma/standalone/LookAndFeelManager.java
+++ afirma-ui-simpleafirma/src/main/java/es/gob/afirma/standalone/LookAndFeelManager.java
@@ -83,7 +83,7 @@ public final class LookAndFeelManager {
             UIManager.put("TextArea.background", WINDOW_COLOR); //$NON-NLS-1$
             UIManager.put("InternalFrameTitlePane.background", WINDOW_COLOR); //$NON-NLS-1$
             UIManager.put("InternalFrame.background", WINDOW_COLOR); //$NON-NLS-1$
-            UIManager.put("Panel.background", WINDOW_COLOR); //$NON-NLS-1$
+            //UIManager.put("Panel.background", WINDOW_COLOR); //$NON-NLS-1$
             //UIManager.put("Label.background", WINDOW_COLOR); //$NON-NLS-1$
             UIManager.put("PopupMenuSeparator.background", WINDOW_COLOR); //$NON-NLS-1$
         }
