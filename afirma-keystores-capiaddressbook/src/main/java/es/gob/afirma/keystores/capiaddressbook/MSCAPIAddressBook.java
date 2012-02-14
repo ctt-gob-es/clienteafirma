2b8f6aeeb221416bb537007ee80e28fcf8c25171
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2012-02-14 17:17:32 +0000

    Reorganización de código y cambio en los textos de licencia (OpenJDK)
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@2284 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-keystores-capiaddressbook/src/main/java/es/gob/afirma/keystores/capiaddressbook/MSCAPIAddressBook.java
+++ afirma-keystores-capiaddressbook/src/main/java/es/gob/afirma/keystores/capiaddressbook/MSCAPIAddressBook.java
@@ -1,9 +1,3 @@
-/*
- * @(#)SunMSCAPIAddressBook.java	1.6 09/11/08
- *
- * Copyright 2006 Sun Microsystems, Inc. All rights reserved.
- * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
 
 package es.gob.afirma.keystores.capiaddressbook;
 
@@ -12,17 +6,17 @@ import java.security.Provider;
 /** Proveedor JCA para el acceso a los almacenes de claves de CAPI <i>CA</i> y
  * <i>ADDRESSBOOK</i>. Basado en SunMSCAPI.java
  * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s */
-public final class SunMSCAPIAddressBook extends Provider {
+public final class MSCAPIAddressBook extends Provider {
 
     private static final long serialVersionUID = 2561943190524802403L;
-    
+
     private static final double VERSION = 0.1d;
 
-    private static final String INFO = "Provider for CAPI ADDRESSBOOK and CA KeyStores, based on SunMSCAPI"; //$NON-NLS-1$
+    private static final String INFO = "Proveedor para la libreta de direcciones (Other People) de CAPI"; //$NON-NLS-1$
 
     /** Construye el proveedor JCA para el acceso a los almacenes de claves
      * <i>CA</i> y <i>ADDRESSBOOK</i>. */
-    public SunMSCAPIAddressBook() {
+    public MSCAPIAddressBook() {
         super("SunMSCAPIAddressBook", VERSION, INFO); //$NON-NLS-1$
         put("KeyStore.Windows-ADDRESSBOOK", "sun.security.mscapi.KeyStoreAddressBook$ADDRESSBOOK"); //$NON-NLS-1$ //$NON-NLS-2$
         put("KeyStore.Windows-CA", "sun.security.mscapi.KeyStoreAddressBook$CA"); //$NON-NLS-1$ //$NON-NLS-2$
