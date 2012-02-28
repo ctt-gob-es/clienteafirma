c80b2825b790d3f7300107879c0888e90cb57d05
Javier Morais Romero <javier.morais@inteco.es>
2012-02-28 17:36:03 +0000

    Añadidas etiquetas @Ignore
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@2374 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-keystores-mozilla/src/test/java/es/gob/afirma/test/keystores/mozilla/SimpleTest.java
+++ afirma-keystores-mozilla/src/test/java/es/gob/afirma/test/keystores/mozilla/SimpleTest.java
@@ -1,5 +1,6 @@
 package es.gob.afirma.test.keystores.mozilla;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import es.gob.afirma.keystores.main.common.AOKeyStore;
@@ -9,23 +10,34 @@ import es.gob.afirma.keystores.main.common.AOKeyStoreManagerFactory;
 /** Pruebas simples de almacenes Mozilla NSS. */
 public final class SimpleTest {
     
-    /** Prueba de la obtenci&oacute;n de almac&eacute;n y alias con Mozilla NSS.
-     * @throws Exception */
-    @SuppressWarnings("static-method")
-	@Test
-    public void testKeyStoreManagerCreation() throws Exception {
-        final AOKeyStoreManager ksm = AOKeyStoreManagerFactory.getAOKeyStoreManager(
-          AOKeyStore.MOZ_UNI, 
-          null, 
-          "TEST-KEYSTORE",  //$NON-NLS-1$
-          null, // PasswordCallback 
-          null // Parent
-        );
-        
-        final String[] aliases = ksm.getAliases();
-        for (final String alias : aliases) {
-            System.out.println(alias);
-        }
+    public static void main(final String[] args) {
+	final SimpleTest test = new SimpleTest();
+	try {
+	    test.testKeyStoreManagerCreation();
+	} catch (final Exception e) {
+	    System.err.println(e.toString());
+	}
     }
-
+    
+    /**
+     * Prueba de la obtenci&oacute;n de almac&eacute;n y alias con Mozilla NSS.
+     * 
+     * @throws Exception
+     */
+    @SuppressWarnings("static-method")
+    @Test
+    @Ignore
+    public void testKeyStoreManagerCreation() throws Exception {
+	final AOKeyStoreManager ksm = AOKeyStoreManagerFactory
+		.getAOKeyStoreManager(AOKeyStore.MOZ_UNI, null,
+			"TEST-KEYSTORE",  //$NON-NLS-1$
+			null, // PasswordCallback
+			null // Parent
+		);
+	
+	final String[] aliases = ksm.getAliases();
+	for (final String alias : aliases) {
+	    System.out.println(alias);
+	}
+    }
 }
--- afirma-keystores-mozilla/src/test/java/es/gob/afirma/test/keystores/mozilla/TestFirefoxFriendlyNames.java
+++ afirma-keystores-mozilla/src/test/java/es/gob/afirma/test/keystores/mozilla/TestFirefoxFriendlyNames.java
@@ -1,11 +1,11 @@
-/* Copyright (C) 2011 [Gobierno de Espana]
- * This file is part of "Cliente @Firma".
- * "Cliente @Firma" is free software; you can redistribute it and/or modify it under the terms of:
- *   - the GNU General Public License as published by the Free Software Foundation; 
- *     either version 2 of the License, or (at your option) any later version.
- *   - or The European Software License; either version 1.1 or (at your option) any later version.
- * Date: 11/01/11
- * You may contact the copyright holder at: soporte.afirma5@mpt.es
+/*
+ * Copyright (C) 2011 [Gobierno de Espana] This file is part of
+ * "Cliente @Firma". "Cliente @Firma" is free software; you can redistribute it
+ * and/or modify it under the terms of: - the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version. - or The European Software License;
+ * either version 1.1 or (at your option) any later version. Date: 11/01/11 You
+ * may contact the copyright holder at: soporte.afirma5@mpt.es
  */
 
 package es.gob.afirma.test.keystores.mozilla;
@@ -15,6 +15,7 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import es.gob.afirma.keystores.main.common.AOKeyStore;
@@ -23,36 +24,48 @@ import es.gob.afirma.keystores.main.common.AOKeyStoreManagerFactory;
 import es.gob.afirma.keystores.main.common.AOKeystoreAlternativeException;
 import es.gob.afirma.keystores.main.common.KeyStoreUtilities;
 
-/** Prueba la conversi&oacute;n de alias en nombres significativos en CAPI.
- * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s */
+/**
+ * Prueba la conversi&oacute;n de alias en nombres significativos en CAPI.
+ * 
+ * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s
+ */
 public class TestFirefoxFriendlyNames {
-    
-    /** Prueba la conversi&oacute;n de alias en nombres significativos en CAPI. 
-     * @throws AOKeystoreAlternativeException 
-     * @throws IOException */
-    @SuppressWarnings("static-method")
-	@Test
-    public void testWindowsFriendlyNames() throws IOException, AOKeystoreAlternativeException {
-        Logger.getLogger("es.gob.afirma").setLevel(Level.WARNING); //$NON-NLS-1$
-        AOKeyStoreManager ksm = AOKeyStoreManagerFactory.getAOKeyStoreManager(
-               AOKeyStore.MOZ_UNI, 
-               null, 
-               "TEST",  //$NON-NLS-1$
-               null, 
-               null
-        );
-
-        System.out.println();
-        final Map<String, String> aliases = KeyStoreUtilities.getAliasesByFriendlyName(
-               ksm.getAliases(), 
-               ksm, 
-               true, // Check private keys
-               true, // Show expired
-               null  // filters
-       );
-        for (final String key : aliases.keySet()) {
-            System.out.println(key + "\n\t" + aliases.get(key)); //$NON-NLS-1$
-        }
+    public static void main(final String[] args) {
+	final TestFirefoxFriendlyNames test = new TestFirefoxFriendlyNames();
+	try {
+	    test.testWindowsFriendlyNames();
+	} catch (final IOException e) {
+	    System.err.println(e.toString() + e.getCause().toString());
+	} catch (final AOKeystoreAlternativeException e) {
+	    System.err.println(e.toString() + e.getCause().toString());
+	}
     }
-
+    
+    /**
+     * Prueba la conversi&oacute;n de alias en nombres significativos en CAPI.
+     * 
+     * @throws AOKeystoreAlternativeException
+     * @throws IOException
+     */
+    @Test
+    @Ignore
+    public void testWindowsFriendlyNames() throws IOException,
+	    AOKeystoreAlternativeException {
+	Logger.getLogger("es.gob.afirma").setLevel(Level.WARNING); //$NON-NLS-1$
+	final AOKeyStoreManager ksm = AOKeyStoreManagerFactory
+		.getAOKeyStoreManager(AOKeyStore.MOZ_UNI, null, "TEST",  //$NON-NLS-1$
+			null, null);
+	
+	System.out.println();
+	final Map<String, String> aliases = KeyStoreUtilities
+		.getAliasesByFriendlyName(ksm.getAliases(), ksm, true, // Check
+								       // private
+								       // keys
+			true, // Show expired
+			null  // filters
+		);
+	for (final String key : aliases.keySet()) {
+	    System.out.println(key + "\n\t" + aliases.get(key)); //$NON-NLS-1$
+	}
+    }
 }
