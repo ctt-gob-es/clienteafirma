a89835883879687ab3d53a0c6df330de33767c71
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2012-03-01 09:58:07 +0000

    Optimización menor
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@2395 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-ui-miniapplet/src/test/java/es/gob/afirma/keystores/filters/SSLFilterTest.java
+++ afirma-ui-miniapplet/src/test/java/es/gob/afirma/keystores/filters/SSLFilterTest.java
@@ -1,7 +1,7 @@
 /* Copyright (C) 2011 [Gobierno de Espana]
  * This file is part of "Cliente @Firma".
  * "Cliente @Firma" is free software; you can redistribute it and/or modify it under the terms of:
- *   - the GNU General Public License as published by the Free Software Foundation; 
+ *   - the GNU General Public License as published by the Free Software Foundation;
  *     either version 2 of the License, or (at your option) any later version.
  *   - or The European Software License; either version 1.1 or (at your option) any later version.
  * Date: 11/01/11
@@ -26,7 +26,7 @@ import es.gob.afirma.miniapplet.keystores.filters.SSLFilter;
 /**
  * Prueba el filtrado de certificado a partir de un numero de serie en hexadecimal. No prueba la
  * condicion de, si se indica el numero de serie del certificado de autenticacion de un DNIe
- * se seleccione en lugar el certificado de firma de ese DNIe. 
+ * se seleccione en lugar el certificado de firma de ese DNIe.
  */
 public class SSLFilterTest {
 
@@ -34,9 +34,9 @@ public class SSLFilterTest {
     private static final String CERT_PATH = "src" + File.separator + "test" + File.separator + "resources" + File.separator + "ANF_PF_Activo.pfx"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
     private static final String CERT_PASS = "12341234"; //$NON-NLS-1$
     private static final String CERT_SN = "03 ea"; //$NON-NLS-1$
-    
+
     private static final String CERT_ALIAS = "anf usuario activo"; //$NON-NLS-1$
-	
+
 	/**
 	 * Comprobaci&oacute;n del filtrado.
 	 * @throws IOException Clave no v&aacute;lida.
@@ -45,21 +45,23 @@ public class SSLFilterTest {
 	@SuppressWarnings("static-method")
 	@Test
 	public void match() throws IOException, AOKeystoreAlternativeException {
-		
-		SSLFilter filter = new SSLFilter(CERT_SN);
-		
-		System.out.println(new File(CERT_PATH).getAbsolutePath());
-		
-		AOKeyStoreManager manager = 
+
+		final SSLFilter filter = new SSLFilter(CERT_SN);
+
+		final String path = CERT_PATH == null ? null : new File(CERT_PATH).getAbsolutePath();
+
+		System.out.println("Ruta del almacen: " + path); //$NON-NLS-1$
+
+		final AOKeyStoreManager manager =
 			AOKeyStoreManagerFactory.getAOKeyStoreManager(
 					KEYSTORE_TYPE,
-					new File(CERT_PATH).getAbsolutePath(),
+					path,
 					"TEST", //$NON-NLS-1$
 					new CachePasswordCallback(CERT_PASS.toCharArray()),
 					null);
 
 		System.out.println("Alias filtrados:"); //$NON-NLS-1$
-		for (String alias : filter.matches(manager.getAliases(), manager)) {
+		for (final String alias : filter.matches(manager.getAliases(), manager)) {
 			System.out.println(alias);
 			Assert.assertEquals("El certificado recuperado no es el esperado", CERT_ALIAS, alias); //$NON-NLS-1$
 		}
