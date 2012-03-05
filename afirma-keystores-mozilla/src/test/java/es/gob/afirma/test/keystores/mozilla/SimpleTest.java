ea1f26d7549c5c6460aae9ac203b7dd87eb110e6
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2012-03-05 10:51:20 +0000

    Mejora del JavaDoc
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@2422 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-keystores-mozilla/src/test/java/es/gob/afirma/test/keystores/mozilla/SimpleTest.java
+++ afirma-keystores-mozilla/src/test/java/es/gob/afirma/test/keystores/mozilla/SimpleTest.java
@@ -9,35 +9,33 @@ import es.gob.afirma.keystores.main.common.AOKeyStoreManagerFactory;
 
 /** Pruebas simples de almacenes Mozilla NSS. */
 public final class SimpleTest {
-    
+
+    /** Inicio de las pruebas desde consola sin JUnit.
+     * @param args */
     public static void main(final String[] args) {
-	final SimpleTest test = new SimpleTest();
-	try {
-	    test.testKeyStoreManagerCreation();
-	} catch (final Exception e) {
-	    System.err.println(e.toString());
-	}
+		try {
+			new SimpleTest().testKeyStoreManagerCreation();
+		}
+		catch (final Exception e) {
+		    System.err.println(e.toString());
+		}
     }
-    
-    /**
-     * Prueba de la obtenci&oacute;n de almac&eacute;n y alias con Mozilla NSS.
-     * 
-     * @throws Exception
-     */
+
+    /** Prueba de la obtenci&oacute;n de almac&eacute;n y alias con Mozilla NSS.
+     * @throws Exception */
     @SuppressWarnings("static-method")
     @Test
     @Ignore
     public void testKeyStoreManagerCreation() throws Exception {
-	final AOKeyStoreManager ksm = AOKeyStoreManagerFactory
-		.getAOKeyStoreManager(AOKeyStore.MOZ_UNI, null,
+    	final AOKeyStoreManager ksm = AOKeyStoreManagerFactory
+		 .getAOKeyStoreManager(AOKeyStore.MOZ_UNI, null,
 			"TEST-KEYSTORE",  //$NON-NLS-1$
 			null, // PasswordCallback
 			null // Parent
 		);
-	
-	final String[] aliases = ksm.getAliases();
-	for (final String alias : aliases) {
-	    System.out.println(alias);
-	}
+    	final String[] aliases = ksm.getAliases();
+    	for (final String alias : aliases) {
+    		System.out.println(alias);
+    	}
     }
 }
