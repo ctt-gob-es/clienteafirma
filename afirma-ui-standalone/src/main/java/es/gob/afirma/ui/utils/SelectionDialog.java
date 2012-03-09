461cda0604f2e7bfb9b7e5eb2356ea61bec1b85b
Javier Morais Romero <javier.morais@inteco.es>
2012-03-09 08:42:16 +0000

    Solucionado un problema relativo a las ventanas de selección de fichero con JAVA 7
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@2447 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-ui-standalone/src/main/java/es/gob/afirma/ui/utils/SelectionDialog.java
+++ afirma-ui-standalone/src/main/java/es/gob/afirma/ui/utils/SelectionDialog.java
@@ -90,7 +90,7 @@ public final class SelectionDialog {
 		if(fc.showOpenDialog(parent) == JFileChooser.APPROVE_OPTION) {
 			filePath = fc.getSelectedFile();
 			if (filePath.getParentFile() != null) {
-			    Main.getPreferences().put("dialog.load.dir", filePath.getParentFile().getAbsolutePath()); //$NON-NLS-1$
+			    Main.getPreferences().put("dialog.load.dir", filePath.getAbsolutePath()); //$NON-NLS-1$
 			}
 		}
 
@@ -226,7 +226,7 @@ public final class SelectionDialog {
 
         try {
             if (resultFile != null && resultFile.getParentFile() != null) {
-                Main.getPreferences().put("dialog.save.dir", resultFile.getParentFile().getAbsolutePath()); //$NON-NLS-1$
+                Main.getPreferences().put("dialog.save.dir", resultFile.getAbsolutePath()); //$NON-NLS-1$
             }
         } catch (final Exception e) {
             /* No hacemos nada */
