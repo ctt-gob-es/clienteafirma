dada86529a470706086731995546c7c93790ec1a
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2012-04-09 17:29:34 +0000

    Se sustituye el uso de la clase WinRegistry por el de la biblioteca "deploy.jar", agregado al libraryPath y cargada mediante reflexión
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@2573 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-core/src/main/java/es/gob/afirma/core/util/windows/WinRegistryWrapper.java
+++ afirma-core/src/main/java/es/gob/afirma/core/util/windows/WinRegistryWrapper.java
@@ -10,9 +10,13 @@
 
 package es.gob.afirma.core.util.windows;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.logging.Logger;
-
-import com.sun.deploy.util.WinRegistry;
 
 import es.gob.afirma.core.misc.AOUtil;
 import es.gob.afirma.core.misc.Platform;
@@ -26,6 +30,14 @@ public final class WinRegistryWrapper {
     static {
 
         if (Platform.getOS().equals(Platform.OS.WINDOWS)) {
+
+        	try {
+        		WinRegistryWrapper.addURL(new File(Platform.getJavaHome() + "\\lib\\deploy.jar").toURI().toURL()); //$NON-NLS-1$
+            }
+            catch (final Exception e) {
+                LOGGER.warning("No se ha localizar y agregar al path la biblioteca java de despliegue 'deploy.jar': " + e);  //$NON-NLS-1$
+            }
+
             // Cargamos la libreria nativa 'deploy.dll'
             try {
                 AOUtil.loadNativeLibrary(Platform.getJavaHome() + "\\bin\\deploy.dll"); //$NON-NLS-1$
@@ -59,14 +71,22 @@ public final class WinRegistryWrapper {
      * @return Valor de la clave obtenida, <i>null</i> si no se encontr&oacute;
      *         la clave o si se produjeron errores durante la obtenci&oacute;n */
     public static Object get(final int hKey, final String path, final String name) {
+
         try {
-            return WinRegistry.get(hKey, path, name);
+        	final Method getMethod = AOUtil.classForName("com.sun.deploy.util.WinRegistry") //$NON-NLS-1$
+    			.getDeclaredMethod("get", new Class[] { Integer.TYPE, String.class, String.class }); //$NON-NLS-1$
+            return getMethod.invoke(null, new Object[] { new Integer(hKey), path, name });
         }
         catch(final NoSuchMethodError e) {
             LOGGER.severe(
         		"No se ha podido obtener la clave de registro por una discordancia de version con deploy.dll, se devolvera null: " + e //$NON-NLS-1$
             );
         }
+        catch (final InvocationTargetException e) {
+        	LOGGER.severe(
+            		"Error al invocar mediante reflexion al metodo get(int, String, String) de WinRegistry, se devolvera null: " + e //$NON-NLS-1$
+                );
+		}
         catch (final Exception e) {
             LOGGER.severe(
         		"No se ha podido obtener la clave de registro con ruta '" + path //$NON-NLS-1$
@@ -91,8 +111,15 @@ public final class WinRegistryWrapper {
      *         obtenci&oacute;n */
     public static String getString(final int hKey, final String path, final String name) {
         try {
-            return WinRegistry.getString(hKey, path, name);
+        	final Method getStringMethod = AOUtil.classForName("com.sun.deploy.util.WinRegistry") //$NON-NLS-1$
+				.getDeclaredMethod("getString", new Class[] { Integer.TYPE, String.class, String.class }); //$NON-NLS-1$
+        	return (String) getStringMethod.invoke(null, new Object[] { new Integer(hKey), path, name });
         }
+        catch (final InvocationTargetException e) {
+        	LOGGER.severe(
+            		"Error al invocar mediante reflexion al metodo getString(int, String, String) de WinRegistry, se devolvera null: " + e //$NON-NLS-1$
+                );
+		}
         catch (final Exception e) {
             LOGGER.severe("No se ha podido obtener la clave de registro con ruta '" + path  //$NON-NLS-1$
                                                      + "' y nombre '" //$NON-NLS-1$
@@ -116,8 +143,15 @@ public final class WinRegistryWrapper {
      * @return <code>true</code> si se estableci&oacute; el valor, <code>false</code> si no se pudo establecer */
     public static boolean setStringValue(final int hKey, final String path, final String name, final String value) {
         try {
-            return WinRegistry.setStringValue(hKey, path, name, value);
+        	final Method setStringValueMethod = AOUtil.classForName("com.sun.deploy.util.WinRegistry") //$NON-NLS-1$
+				.getDeclaredMethod("setStringValue", new Class[] { Integer.TYPE, String.class, String.class, String.class }); //$NON-NLS-1$
+        	return ((Boolean) setStringValueMethod.invoke(null, new Object[] { new Integer(hKey), path, name, value })).booleanValue();
         }
+        catch (final InvocationTargetException e) {
+        	LOGGER.severe(
+            		"Error al invocar mediante reflexion al metodo setStringValue(int, String, String, String) de WinRegistry, se devolvera false: " + e //$NON-NLS-1$
+                );
+		}
         catch (final Exception e) {
             LOGGER.severe("No se ha podido establecer la clave de registro con ruta '" + path //$NON-NLS-1$
                                                      + "' y nombre '" //$NON-NLS-1$
@@ -128,4 +162,23 @@ public final class WinRegistryWrapper {
         return false;
     }
 
+    /**
+     * Agrega una URL al PATH de bibliotecas de Java.
+     * @param u URL que se desea agregar al PATH.
+     * @throws IOException Cuando ocurre un error al agregar la ruta.
+     */
+	private static void addURL(final URL u) throws IOException {
+		final URLClassLoader sysloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+		final Class<?> sysclass = URLClassLoader.class;
+		try {
+			final Method method = sysclass.getDeclaredMethod("addURL", new Class[] { URL.class }); //$NON-NLS-1$
+			method.setAccessible(true);
+			method.invoke(sysloader, new Object[] { u });
+		}
+		catch (final Exception e) {
+			throw new IOException(
+				"No se ha podido anadir la URL al classloader( " + u + "): " + e //$NON-NLS-1$ //$NON-NLS-2$
+			);
+		}
+	}
 }
