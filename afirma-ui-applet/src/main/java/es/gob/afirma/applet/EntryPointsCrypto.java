487b3a08d3d87d1734c3e6b288275b69d2e58f81
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2012-04-11 11:23:08 +0000

    Corrección del Javadoc del método setPolicy
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@2583 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-ui-applet/src/main/java/es/gob/afirma/applet/EntryPointsCrypto.java
+++ afirma-ui-applet/src/main/java/es/gob/afirma/applet/EntryPointsCrypto.java
@@ -2510,7 +2510,8 @@ interface EntryPointsCrypto {
      *        ejemplo "urn:oid:2.16.724.1.3.2.2.3.1" <br>
      *        Signature policy's qualifying oid.
      * @param hashB64
-     * 		  Huella digital SHA-1 en base64 de la pol&acute;tica de firma. */
+     * 		  Huella digital SHA-1 en base64 de la pol&iacute;tica de firma.
+     * 		  Signature policy's hash.*/
     void setPolicy(String identifier, String description, String qualifier, String hashB64);
 
     /** Obtiene todos los alias de los certificados disponibles para firmar o
