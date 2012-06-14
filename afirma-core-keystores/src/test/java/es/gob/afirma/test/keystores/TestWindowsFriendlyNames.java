a7d2434518a9c86f8a793f2a3eb62a08a9425d55
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2012-06-14 15:29:33 +0000

    Se elimina código muerto del test
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@2762 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-core-keystores/src/test/java/es/gob/afirma/test/keystores/TestWindowsFriendlyNames.java
+++ afirma-core-keystores/src/test/java/es/gob/afirma/test/keystores/TestWindowsFriendlyNames.java
@@ -10,20 +10,12 @@
 
 package es.gob.afirma.test.keystores;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.security.auth.callback.PasswordCallback;
-
-import junit.framework.Assert;
-
 import org.junit.Test;
 
-import es.gob.afirma.core.misc.AOUtil;
 import es.gob.afirma.core.misc.Platform;
 import es.gob.afirma.keystores.main.common.AOKeyStore;
 import es.gob.afirma.keystores.main.common.AOKeyStoreManager;
@@ -35,8 +27,6 @@ import es.gob.afirma.keystores.main.common.KeyStoreUtilities;
 public class TestWindowsFriendlyNames {
 
 	private static final AOKeyStore KEYSTORE_TYPE = AOKeyStore.WINDOWS;
-	private static final String KEYSTORE_PATH = "ANF_PF_Activo.pfx"; //$NON-NLS-1$
-	private static final String KEYSTORE_PASS = "12341234"; //$NON-NLS-1$
 
     /** Prueba la conversi&oacute;n de alias en nombres significativos en CAPI.
      * @throws Exception */
@@ -47,29 +37,15 @@ public class TestWindowsFriendlyNames {
             return;
         }
 
-        final byte[] p12file = AOUtil.getDataFromInputStream(ClassLoader.getSystemResourceAsStream(KEYSTORE_PATH));
-        Assert.assertTrue("No se ha podido leer el P12", p12file.length > 0); //$NON-NLS-1$
-        final File tmpFile = File.createTempFile("temp", "afirma"); //$NON-NLS-1$ //$NON-NLS-2$
-        tmpFile.deleteOnExit();
-        final OutputStream os = new FileOutputStream(tmpFile);
-        os.write(p12file);
-        os.flush();
-        os.close();
-
-        final PasswordCallback pc = new PasswordCallback(">", false); //$NON-NLS-1$
-        pc.setPassword(KEYSTORE_PASS.toCharArray());
-
         Logger.getLogger("es.gob.afirma").setLevel(Level.WARNING); //$NON-NLS-1$
         final AOKeyStoreManager ksm = AOKeyStoreManagerFactory.getAOKeyStoreManager(
         		KEYSTORE_TYPE,
-                tmpFile.getAbsolutePath(),
-               "TEST",  //$NON-NLS-1$
-               pc,
+                null,
+               "Almacen Windows",  //$NON-NLS-1$
+               null,
                null
         );
-//        for(final String al : ksm.getAliases()) {
-//            System.out.println(al);
-//        }
+
         System.out.println();
         final Map<String, String> aliases = KeyStoreUtilities.getAliasesByFriendlyName(
                ksm.getAliases(),
