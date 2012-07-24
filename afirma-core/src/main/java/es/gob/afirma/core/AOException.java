219571ed26d8a8f42d651409ccf97652e1b248ac
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2012-07-24 06:04:43 +0000

    Corrección en el JavaDoc
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@2906 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-core/src/main/java/es/gob/afirma/core/AOException.java
+++ afirma-core/src/main/java/es/gob/afirma/core/AOException.java
@@ -1,7 +1,7 @@
 /* Copyright (C) 2011 [Gobierno de Espana]
  * This file is part of "Cliente @Firma".
  * "Cliente @Firma" is free software; you can redistribute it and/or modify it under the terms of:
- *   - the GNU General Public License as published by the Free Software Foundation; 
+ *   - the GNU General Public License as published by the Free Software Foundation;
  *     either version 2 of the License, or (at your option) any later version.
  *   - or The European Software License; either version 1.1 or (at your option) any later version.
  * Date: 11/01/11
@@ -17,28 +17,21 @@ public class AOException extends Exception {
     private static final long serialVersionUID = -662191654860389176L;
 
     /** Contruye una excepci&oacute;n gen&eacute;rica con mensaje.
-     * @param msg
-     *        Mensaje de la excepci&oacute;n */
+     * @param msg Mensaje de la excepci&oacute;n */
     public AOException(final String msg) {
         super(msg);
     }
 
-    /** Contruye una excepci&oacute;n gen&eacute;rica con mensaje y define su
-     * causa.
-     * @param msg
-     *        Descripcion del error.
-     * @param e
-     *        Causa del error. */
+    /** Contruye una excepci&oacute;n gen&eacute;rica con mensaje y define su causa.
+     * @param msg Descripci&oacute;n del error.
+     * @param e Causa del error. */
     public AOException(final String msg, final Exception e) {
         super(msg, e);
     }
 
-    /** Contruye una excepci&oacute;n gen&eacute;rica con mensaje y define su
-     * causa.
-     * @param msg
-     *        Descripcion del error.
-     * @param cause
-     *        Causa del error. */
+    /** Contruye una excepci&oacute;n gen&eacute;rica con mensaje y define su causa.
+     * @param msg Descripci&oacute;n del error.
+     * @param cause Causa del error. */
     public AOException(final String msg, final Throwable cause) {
         super(msg, cause);
     }
