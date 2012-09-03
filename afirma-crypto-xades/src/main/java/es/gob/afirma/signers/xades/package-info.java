afe52f765b64df4915af24abb46f71707c54c470
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2012-09-03 15:51:11 +0000

    Corrección de documentación
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@3050 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-crypto-xades/src/main/java/es/gob/afirma/signers/xades/package-info.java
+++ afirma-crypto-xades/src/main/java/es/gob/afirma/signers/xades/package-info.java
@@ -1,7 +1,7 @@
 /* Copyright (C) 2011 [Gobierno de Espana]
  * This file is part of "Cliente @Firma".
  * "Cliente @Firma" is free software; you can redistribute it and/or modify it under the terms of:
- *   - the GNU General Public License as published by the Free Software Foundation; 
+ *   - the GNU General Public License as published by the Free Software Foundation;
  *     either version 2 of the License, or (at your option) any later version.
  *   - or The European Software License; either version 1.1 or (at your option) any later version.
  * Date: 11/01/11
@@ -38,28 +38,18 @@
  *   <li>Dependencia con el m&oacute;dulo XML (<i>afirma-crypto-core-xml</i>) del Cliente.</li>
  *   <li>Dependencia con JXAdES revisi&oacute;n 5138 o superior.</li>
  *  </ul>
- *  <p>Adicionalmente, se presentan las siguientes dependencias din&aacute;micas de primer nivel:</p>
- *  <ul>
- *   <li>
- *    Dependencia din&aacute;mica con el m&oacute;dulo N&uacute;cleo UI JSE (<i>afirma-ui-core-jse</i>) del Cliente.
- *    Este puede sustituirse por cualquier otra clase que implemente el interfaz <code>es.gob.afirma.core.ui.AOUIManager</code>.
- *   </li>
- *   <li>
- *    Dependencia din&aacute;mica desde el n&uacute;cleo con la biblioteca JMIMEMagic.
- *   </li>
- *  </ul>
  *  <p>
  *   Este m&oacute;dulo es compatible con cualquier entorno JSE 1.6 o superior.<br>
  *   Para compatibilidad
  *   con JSE 1.5 es necesario incluir las clases Java contenidas en el "Paquete de compatibilidad
  *   con Java 5" del Ciente e instalar los productos Apache Xalan 2.7.1 o superior y Apache Xerces
  *   2.11.0 o superior como API ENDORSED de Java.<br>
- *   Consulte la p&aacute;gina 
+ *   Consulte la p&aacute;gina
  *   <a href="http://docs.oracle.com/javase/1.5.0/docs/guide/standards/index.html">http://docs.oracle.com/javase/1.5.0/docs/guide/standards/index.html</a>
  *   para informaci&oacute;n ampliada sobre los API ENDORSED de Java.
  *  </p>
  *  <p>
- *   Desde este m&oacute;dulo es posible que se realicen llamadas a interfaces gr&aacute;ficas.<br> 
+ *   Desde este m&oacute;dulo es posible que se realicen llamadas a interfaces gr&aacute;ficas.<br>
  *  </p>
  */
 package es.gob.afirma.signers.xades;
