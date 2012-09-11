f3c3a026b2913beff03671d48ad7bc2be62e741a
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2012-09-11 12:10:47 +0000

    Test de validación de firmas binarias
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@3082 dcdbb401-323c-49cf-88a0-803402807c85

---
+++ afirma-ui-standalone/src/test/java/es/gob/afirma/TestValidatebinarySignature.java
@@ -0,0 +1,143 @@
+package es.gob.afirma;
+
+import java.io.InputStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import es.gob.afirma.core.misc.AOUtil;
+import es.gob.afirma.signature.SignValidity;
+import es.gob.afirma.signature.SignValidity.SIGN_DETAIL_TYPE;
+import es.gob.afirma.signature.ValidateBinarySignature;
+
+public class TestValidatebinarySignature {
+
+	private static final String SIGNATURE_CADES_IMPLICIT_FILENAME = "cades_implicita.csig"; //$NON-NLS-1$
+	private static final String SIGNATURE_CMS_IMPLICIT_FILENAME = "cms_implicita.csig"; //$NON-NLS-1$
+	private static final String SIGNATURE_CADES_EXPLICIT_FILENAME = "cades_explicita.csig"; //$NON-NLS-1$
+	private static final String SIGNATURE_CMS_EXPLICIT_FILENAME = "cms_explicita.csig"; //$NON-NLS-1$
+	private static final String DATA_FILENAME = "data.jpg"; //$NON-NLS-1$
+
+
+	@Test
+	public void testValidarFirmaCadesImplicita() throws Exception {
+
+		final String signatureFile = SIGNATURE_CADES_IMPLICIT_FILENAME;
+
+		final InputStream is = AOUtil.getCleanClassLoader().getResourceAsStream(signatureFile);
+		final byte[] signature = AOUtil.getDataFromInputStream(is);
+
+		try {
+			is.close();
+		} catch (final Exception e) {
+			// No hacemos nada
+		}
+
+		SignValidity validity = null;
+		try {
+			validity = ValidateBinarySignature.validate(signature, null);
+		} catch (final Exception e) {
+			Assert.fail("Ocurrio el siguiente error durante la validacion de la firma " + signatureFile + ": " + e.toString()); //$NON-NLS-1$ //$NON-NLS-2$
+		}
+
+		Assert.assertNotNull("No se ha obtenido ningun resultado del proceso de validacion de la firma " + signatureFile, validity); //$NON-NLS-1$
+		Assert.assertEquals("El resultado de la validacion no es correcto para la firma " + signatureFile, SIGN_DETAIL_TYPE.OK, validity.getValidity()); //$NON-NLS-1$
+	}
+
+	@Test
+	public void testValidarFirmaCmsImplicita() throws Exception {
+
+		final String signatureFile = SIGNATURE_CMS_IMPLICIT_FILENAME;
+
+		final InputStream is = AOUtil.getCleanClassLoader().getResourceAsStream(signatureFile);
+		final byte[] signature = AOUtil.getDataFromInputStream(is);
+
+		try {
+			is.close();
+		} catch (final Exception e) {
+			// No hacemos nada
+		}
+
+		SignValidity validity = null;
+		try {
+			validity = ValidateBinarySignature.validate(signature, null);
+		} catch (final Exception e) {
+			Assert.fail("Ocurrio el siguiente error durante la validacion de la firma " + signatureFile + ": " + e.toString()); //$NON-NLS-1$ //$NON-NLS-2$
+		}
+
+		Assert.assertNotNull("No se ha obtenido ningun resultado del proceso de validacion de la firma " + signatureFile, validity); //$NON-NLS-1$
+		Assert.assertEquals("El resultado de la validacion no es correcto para la firma " + signatureFile, SIGN_DETAIL_TYPE.OK, validity.getValidity()); //$NON-NLS-1$
+	}
+
+	@Test
+	public void testValidarFirmaCadesExplicita() throws Exception {
+
+		final String signatureFile = SIGNATURE_CADES_EXPLICIT_FILENAME;
+
+		final InputStream signIs = AOUtil.getCleanClassLoader().getResourceAsStream(signatureFile);
+		final byte[] signature = AOUtil.getDataFromInputStream(signIs);
+
+		try {
+			signIs.close();
+		} catch (final Exception e) {
+			// No hacemos nada
+		}
+
+		final String dataFile = DATA_FILENAME;
+
+		final InputStream dataIs = AOUtil.getCleanClassLoader().getResourceAsStream(dataFile);
+		final byte[] data = AOUtil.getDataFromInputStream(dataIs);
+
+		try {
+			dataIs.close();
+		} catch (final Exception e) {
+			// No hacemos nada
+		}
+
+		SignValidity validity = null;
+		try {
+			validity = ValidateBinarySignature.validate(signature, data);
+		} catch (final Exception e) {
+			Assert.fail("Ocurrio el siguiente error durante la validacion de la firma " + signatureFile + ": " + e.toString()); //$NON-NLS-1$ //$NON-NLS-2$
+		}
+
+		Assert.assertNotNull("No se ha obtenido ningun resultado del proceso de validacion de la firma " + signatureFile, validity); //$NON-NLS-1$
+		Assert.assertEquals("El resultado de la validacion no es correcto para la firma " + signatureFile, SIGN_DETAIL_TYPE.OK, validity.getValidity()); //$NON-NLS-1$
+	}
+
+	@Test
+	public void testValidarFirmaCmsExplicita() throws Exception {
+
+		final String signatureFile = SIGNATURE_CMS_EXPLICIT_FILENAME;
+
+		final InputStream signIs = AOUtil.getCleanClassLoader().getResourceAsStream(signatureFile);
+		final byte[] signature = AOUtil.getDataFromInputStream(signIs);
+
+		try {
+			signIs.close();
+		} catch (final Exception e) {
+			// No hacemos nada
+		}
+
+		final String dataFile = DATA_FILENAME;
+
+		final InputStream dataIs = AOUtil.getCleanClassLoader().getResourceAsStream(dataFile);
+		final byte[] data = AOUtil.getDataFromInputStream(dataIs);
+
+		try {
+			dataIs.close();
+		} catch (final Exception e) {
+			// No hacemos nada
+		}
+
+		SignValidity validity = null;
+		try {
+			validity = ValidateBinarySignature.validate(signature, data);
+		} catch (final Exception e) {
+			Assert.fail("Ocurrio el siguiente error durante la validacion de la firma " + signatureFile + ": " + e.toString()); //$NON-NLS-1$ //$NON-NLS-2$
+		}
+
+		Assert.assertNotNull("No se ha obtenido ningun resultado del proceso de validacion de la firma " + signatureFile, validity); //$NON-NLS-1$
+		Assert.assertEquals("El resultado de la validacion no es correcto para la firma " + signatureFile, SIGN_DETAIL_TYPE.OK, validity.getValidity()); //$NON-NLS-1$
+	}
+}
