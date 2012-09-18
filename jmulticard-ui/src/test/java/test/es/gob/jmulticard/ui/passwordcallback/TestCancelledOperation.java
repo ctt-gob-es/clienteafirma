850a249d6ec500b45e68e660bc063b585fbe6c38
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2012-09-18 16:42:10 +0000

    Se agrega la etiqueta @Ignore a aquellos test con interfaz gráfica para no interrumpir el proceso de integración continua.
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@3124 dcdbb401-323c-49cf-88a0-803402807c85

--- jmulticard-ui/src/test/java/test/es/gob/jmulticard/ui/passwordcallback/TestCancelledOperation.java
+++ jmulticard-ui/src/test/java/test/es/gob/jmulticard/ui/passwordcallback/TestCancelledOperation.java
@@ -6,6 +6,7 @@ import javax.swing.JOptionPane;
 
 import junit.framework.Assert;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import es.gob.jmulticard.ui.passwordcallback.CancelledOperationException;
@@ -18,6 +19,7 @@ public class TestCancelledOperation {
 	/** Prueba de cancelaci&oacute;n de di&aacute;logo de confirmaci&oacute;n. */
 	@SuppressWarnings("static-method")
 	@Test
+	@Ignore
 	public void testCancelConfirmDialog() {
 		JOptionPane.showMessageDialog(null, "Pulse NO en los dialogos que se mostraran al cerrar este"); //$NON-NLS-1$
 		Assert.assertEquals(1, DialogBuilder.showSignatureConfirmDialog(null, true));
@@ -27,6 +29,7 @@ public class TestCancelledOperation {
 	/** Prueba de cancelaci&oacute;n de di&aacute;logo de petici&oacute;n de PIN. */
 	@SuppressWarnings("static-method")
 	@Test(expected=CancelledOperationException.class)
+	@Ignore
 	public void testCancelPinDialogRetry() {
 		JOptionPane.showMessageDialog(null, "Cancele el dialogo que se mostrara al cerrar este"); //$NON-NLS-1$
 		Logger.getAnonymousLogger().info(new String(CommonPasswordCallback.getDnieBadPinPasswordCallback(2).getPassword()));
@@ -35,6 +38,7 @@ public class TestCancelledOperation {
 	/** Prueba de cancelaci&oacute;n de di&aacute;logo de petici&oacute;n de PIN. */
 	@SuppressWarnings("static-method")
 	@Test(expected=CancelledOperationException.class)
+	@Ignore
 	public void testCancelPinDialogReadCert() {
 		JOptionPane.showMessageDialog(null, "Cancele el dialogo que se mostrara al cerrar este"); //$NON-NLS-1$
 		Logger.getAnonymousLogger().info(new String(CommonPasswordCallback.getDniePinForCertificateReadingPasswordCallback(null).getPassword()));
--- jmulticard-ui/src/test/java/test/es/gob/jmulticard/ui/passwordcallback/TestPinDialog.java
+++ jmulticard-ui/src/test/java/test/es/gob/jmulticard/ui/passwordcallback/TestPinDialog.java
@@ -2,6 +2,7 @@ package test.es.gob.jmulticard.ui.passwordcallback;
 
 import javax.swing.JOptionPane;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import es.gob.jmulticard.ui.passwordcallback.DialogBuilder;
@@ -14,6 +15,7 @@ public class TestPinDialog {
      * @throws Exception En caso de cualquier problema */
     @SuppressWarnings("static-method")
     @Test
+    @Ignore
     public void testPinInput() throws Exception {
         JOptionPane.showMessageDialog(null,
                                       "Redimensione el dialogo siguiente de introduccion de PIN, esciba caracteres hasta alcanzar el maximo, muestre y oculte los caracteres. Maximice y minimice"); //$NON-NLS-1$
@@ -24,6 +26,7 @@ public class TestPinDialog {
      * @throws Exception En caso de cualquier problema */
     @SuppressWarnings("static-method")
     @Test
+    @Ignore
     public void testConfirm() throws Exception {
         JOptionPane.showMessageDialog(null, "Redimensione el dialogo siguiente de confirmacion de firma. Maximice y minimice"); //$NON-NLS-1$
         System.out.println(DialogBuilder.showSignatureConfirmDialog(null, true));
