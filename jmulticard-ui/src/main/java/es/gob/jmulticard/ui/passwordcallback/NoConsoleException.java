b4fbcb7f4c58ed4e6ca0b3279e237ce90e2e5a8d
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2012-09-18 16:38:53 +0000

    Se eliminan las tildes de los comentarios
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@3122 dcdbb401-323c-49cf-88a0-803402807c85

--- jmulticard-ui/src/main/java/es/gob/jmulticard/ui/passwordcallback/NoConsoleException.java
+++ jmulticard-ui/src/main/java/es/gob/jmulticard/ui/passwordcallback/NoConsoleException.java
@@ -1,41 +1,41 @@
 /*
- * Controlador Java de la Secretaría de Estado de Administraciones Públicas
- * para el DNI electrónico.
+ * Controlador Java de la Secretaria de Estado de Administraciones Publicas
+ * para el DNI electronico.
  *
- * El Controlador Java para el DNI electrónico es un proveedor de seguridad de JCA/JCE 
- * que permite el acceso y uso del DNI electrónico en aplicaciones Java de terceros 
- * para la realización de procesos de autenticación, firma electrónica y validación 
+ * El Controlador Java para el DNI electronico es un proveedor de seguridad de JCA/JCE 
+ * que permite el acceso y uso del DNI electronico en aplicaciones Java de terceros 
+ * para la realizacion de procesos de autenticacion, firma electronica y validacion 
  * de firma. Para ello, se implementan las funcionalidades KeyStore y Signature para 
- * el acceso a los certificados y claves del DNI electrónico, así como la realización 
- * de operaciones criptográficas de firma con el DNI electrónico. El Controlador ha 
- * sido diseñado para su funcionamiento independiente del sistema operativo final.
+ * el acceso a los certificados y claves del DNI electronico, asi como la realizacion 
+ * de operaciones criptograficas de firma con el DNI electronico. El Controlador ha 
+ * sido disenado para su funcionamiento independiente del sistema operativo final.
  * 
- * Copyright (C) 2012 Dirección General de Modernización Administrativa, Procedimientos 
- * e Impulso de la Administración Electrónica
+ * Copyright (C) 2012 Direccion General de Modernizacion Administrativa, Procedimientos 
+ * e Impulso de la Administracion Electronica
  * 
  * Este programa es software libre y utiliza un licenciamiento dual (LGPL 2.1+
- * o EUPL 1.1+), lo cual significa que los usuarios podrán elegir bajo cual de las
- * licencias desean utilizar el código fuente. Su elección deberá reflejarse 
- * en las aplicaciones que integren o distribuyan el Controlador, ya que determinará
+ * o EUPL 1.1+), lo cual significa que los usuarios podran elegir bajo cual de las
+ * licencias desean utilizar el codigo fuente. Su eleccion debera reflejarse 
+ * en las aplicaciones que integren o distribuyan el Controlador, ya que determinara
  * su compatibilidad con otros componentes.
  *
- * El Controlador puede ser redistribuido y/o modificado bajo los términos de la 
+ * El Controlador puede ser redistribuido y/o modificado bajo los terminos de la 
  * Lesser GNU General Public License publicada por la Free Software Foundation, 
- * tanto en la versión 2.1 de la Licencia, o en una versión posterior.
+ * tanto en la version 2.1 de la Licencia, o en una version posterior.
  * 
- * El Controlador puede ser redistribuido y/o modificado bajo los términos de la 
- * European Union Public License publicada por la Comisión Europea, 
- * tanto en la versión 1.1 de la Licencia, o en una versión posterior.
+ * El Controlador puede ser redistribuido y/o modificado bajo los terminos de la 
+ * European Union Public License publicada por la Comision Europea, 
+ * tanto en la version 1.1 de la Licencia, o en una version posterior.
  * 
- * Debería recibir una copia de la GNU Lesser General Public License, si aplica, junto
- * con este programa. Si no, consúltelo en <http://www.gnu.org/licenses/>.
+ * Deberia recibir una copia de la GNU Lesser General Public License, si aplica, junto
+ * con este programa. Si no, consultelo en <http://www.gnu.org/licenses/>.
  * 
- * Debería recibir una copia de la European Union Public License, si aplica, junto
- * con este programa. Si no, consúltelo en <http://joinup.ec.europa.eu/software/page/eupl>.
+ * Deberia recibir una copia de la European Union Public License, si aplica, junto
+ * con este programa. Si no, consultelo en <http://joinup.ec.europa.eu/software/page/eupl>.
  *
- * Este programa es distribuido con la esperanza de que sea útil, pero
- * SIN NINGUNA GARANTÍA; incluso sin la garantía implícita de comercialización
- * o idoneidad para un propósito particular.
+ * Este programa es distribuido con la esperanza de que sea util, pero
+ * SIN NINGUNA GARANTIA; incluso sin la garantia implicita de comercializacion
+ * o idoneidad para un proposito particular.
  */
 package es.gob.jmulticard.ui.passwordcallback;
 
