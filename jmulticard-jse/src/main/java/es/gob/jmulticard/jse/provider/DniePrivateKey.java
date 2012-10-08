f19b237374b7af60953f283963ed96c79212f62a
Javier Morais Romero <javier.morais@inteco.es>
2012-10-08 08:36:49 +0000

    Correcciones de una incidencia en CommandApdu que puede provocar que el módulo no se compile en Java 1.4.
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/trunk/project@3194 dcdbb401-323c-49cf-88a0-803402807c85

--- jmulticard-jse/src/main/java/es/gob/jmulticard/jse/provider/DniePrivateKey.java
+++ jmulticard-jse/src/main/java/es/gob/jmulticard/jse/provider/DniePrivateKey.java
@@ -2,29 +2,29 @@
  * Controlador Java de la Secretaria de Estado de Administraciones Publicas
  * para el DNI electronico.
  *
- * El Controlador Java para el DNI electronico es un proveedor de seguridad de JCA/JCE 
- * que permite el acceso y uso del DNI electronico en aplicaciones Java de terceros 
- * para la realizacion de procesos de autenticacion, firma electronica y validacion 
- * de firma. Para ello, se implementan las funcionalidades KeyStore y Signature para 
- * el acceso a los certificados y claves del DNI electronico, asi como la realizacion 
- * de operaciones criptograficas de firma con el DNI electronico. El Controlador ha 
+ * El Controlador Java para el DNI electronico es un proveedor de seguridad de JCA/JCE
+ * que permite el acceso y uso del DNI electronico en aplicaciones Java de terceros
+ * para la realizacion de procesos de autenticacion, firma electronica y validacion
+ * de firma. Para ello, se implementan las funcionalidades KeyStore y Signature para
+ * el acceso a los certificados y claves del DNI electronico, asi como la realizacion
+ * de operaciones criptograficas de firma con el DNI electronico. El Controlador ha
  * sido disenado para su funcionamiento independiente del sistema operativo final.
  * 
- * Copyright (C) 2012 Direccion General de Modernizacion Administrativa, Procedimientos 
+ * Copyright (C) 2012 Direccion General de Modernizacion Administrativa, Procedimientos
  * e Impulso de la Administracion Electronica
  * 
  * Este programa es software libre y utiliza un licenciamiento dual (LGPL 2.1+
  * o EUPL 1.1+), lo cual significa que los usuarios podran elegir bajo cual de las
- * licencias desean utilizar el codigo fuente. Su eleccion debera reflejarse 
+ * licencias desean utilizar el codigo fuente. Su eleccion debera reflejarse
  * en las aplicaciones que integren o distribuyan el Controlador, ya que determinara
  * su compatibilidad con otros componentes.
  *
- * El Controlador puede ser redistribuido y/o modificado bajo los terminos de la 
- * Lesser GNU General Public License publicada por la Free Software Foundation, 
+ * El Controlador puede ser redistribuido y/o modificado bajo los terminos de la
+ * Lesser GNU General Public License publicada por la Free Software Foundation,
  * tanto en la version 2.1 de la Licencia, o en una version posterior.
  * 
- * El Controlador puede ser redistribuido y/o modificado bajo los terminos de la 
- * European Union Public License publicada por la Comision Europea, 
+ * El Controlador puede ser redistribuido y/o modificado bajo los terminos de la
+ * European Union Public License publicada por la Comision Europea,
  * tanto en la version 1.1 de la Licencia, o en una version posterior.
  * 
  * Deberia recibir una copia de la GNU Lesser General Public License, si aplica, junto
@@ -50,89 +50,106 @@ import es.gob.jmulticard.card.Location;
 import es.gob.jmulticard.card.dnie.Dnie;
 import es.gob.jmulticard.card.dnie.DniePrivateKeyReference;
 
-/** Clave privada de un DNIe. La clase no contiene la clave privada en si, sino una referencia a ella
- * y una referencia al propio DNIe, con el canal seguro establecido.
- * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s */
+/**
+ * Clave privada de un DNIe. La clase no contiene la clave privada en si, sino
+ * una referencia a ella y una referencia al propio DNIe, con el canal seguro
+ * establecido.
+ * 
+ * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s
+ */
 public final class DniePrivateKey implements RSAPrivateKey {
 
-    private static final long serialVersionUID = 4403051294889801855L;
+	private static final long serialVersionUID = 4403051294889801855L;
 
-    private final Dnie dnie;
+	private final Dnie dnie;
 
-    /** Identificador de la clave. */
-    private final String id;
+	/** Identificador de la clave. */
+	private final String id;
 
-    /** Ruta de la clave. */
-    private final Location path;
+	/** Ruta de la clave. */
+	private final Location path;
 
-    /** Etiqueta de la clave. */
-    private final String name;
+	/** Etiqueta de la clave. */
+	private final String name;
 
-    /** {@inheritDoc} */
-    @Override
-    public String toString() {
-        return this.name;
-    }
+	/**
+	 * Crea una clave privada de DNIe.
+	 * 
+	 * @param keyReference
+	 *            Referencia a la clave privada del DNIe.
+	 */
+	DniePrivateKey(final DniePrivateKeyReference keyReference) {
+		this.dnie = keyReference.getDnieCard();
+		this.id = keyReference.getIdentifier();
+		this.path = keyReference.getKeyPath();
+		this.name = keyReference.getLabel();
+	}
 
-    /** Crea una clave privada de DNIe.
-     * @param keyReference Referencia a la clave privada del DNIe. */
-    DniePrivateKey(final DniePrivateKeyReference keyReference) {
-    	this.dnie = keyReference.getDnieCard();
-        this.id = keyReference.getIdentifier();
-        this.path = keyReference.getKeyPath();
-        this.name = keyReference.getLabel();
-    }
+	/** {@inheritDoc} */
+	@Override
+	public String getAlgorithm() {
+		return "RSA"; //$NON-NLS-1$
+	}
 
-    /** Obtiene la tarjeta capaz de operar con esta clave.
-     * @return Tarjeta capaz de operar con esta clave */
-    CryptoCard getCryptoCard() {
-        return this.dnie;
-    }
+	/**
+	 * Obtiene la tarjeta capaz de operar con esta clave.
+	 * 
+	 * @return Tarjeta capaz de operar con esta clave
+	 */
+	CryptoCard getCryptoCard() {
+		return this.dnie;
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    public String getAlgorithm() {
-        return "RSA"; //$NON-NLS-1$
-    }
+	/** {@inheritDoc} */
+	@Override
+	public byte[] getEncoded() {
+		return null;
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    public byte[] getEncoded() {
-        return null;
-    }
+	/** {@inheritDoc} */
+	@Override
+	public String getFormat() {
+		return null;
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    public String getFormat() {
-        return null;
-    }
+	/**
+	 * Recupera el identificador de la clave.
+	 * 
+	 * @return Identificador de la clave
+	 */
+	String getId() {
+		return this.id;
+	}
 
-    /** M&eacute;todo no soportado. */
-    @Override
-    public BigInteger getModulus() {
-        throw new UnsupportedOperationException();
-    }
+	/** M&eacute;todo no soportado. */
+	@Override
+	public BigInteger getModulus() {
+		throw new UnsupportedOperationException();
+	}
 
-    /** M&eacute;todo no soportado. */
-    @Override
-    public BigInteger getPrivateExponent() {
-        throw new UnsupportedOperationException();
-    }
+	/**
+	 * Recupera la ruta de la clave.
+	 * 
+	 * @return Ruta de la clave
+	 */
+	Location getPath() {
+		return this.path;
+	}
 
-    /** Recupera el identificador de la clave.
-     * @return Identificador de la clave */
-    String getId() {
-        return this.id;
-    }
+	/** M&eacute;todo no soportado. */
+	@Override
+	public BigInteger getPrivateExponent() {
+		throw new UnsupportedOperationException();
+	}
 
-    /** Recupera la ruta de la clave.
-     * @return Ruta de la clave */
-    Location getPath() {
-        return this.path;
-    }
+	/** {@inheritDoc} */
+	@Override
+	public String toString() {
+		return this.name;
+	}
 
-    @SuppressWarnings("static-method")
-    private void writeObject(@SuppressWarnings("unused") final ObjectOutputStream out) throws IOException {
-        throw new NotSerializableException();
-    }
+	@SuppressWarnings("static-method")
+	private void writeObject(final ObjectOutputStream out) throws IOException {
+		throw new NotSerializableException();
+	}
 }
--- jmulticard/src/main/java/es/gob/jmulticard/apdu/CommandApdu.java
+++ jmulticard/src/main/java/es/gob/jmulticard/apdu/CommandApdu.java
@@ -2,29 +2,29 @@
  * Controlador Java de la Secretaria de Estado de Administraciones Publicas
  * para el DNI electronico.
  *
- * El Controlador Java para el DNI electronico es un proveedor de seguridad de JCA/JCE 
- * que permite el acceso y uso del DNI electronico en aplicaciones Java de terceros 
- * para la realizacion de procesos de autenticacion, firma electronica y validacion 
- * de firma. Para ello, se implementan las funcionalidades KeyStore y Signature para 
- * el acceso a los certificados y claves del DNI electronico, asi como la realizacion 
- * de operaciones criptograficas de firma con el DNI electronico. El Controlador ha 
+ * El Controlador Java para el DNI electronico es un proveedor de seguridad de JCA/JCE
+ * que permite el acceso y uso del DNI electronico en aplicaciones Java de terceros
+ * para la realizacion de procesos de autenticacion, firma electronica y validacion
+ * de firma. Para ello, se implementan las funcionalidades KeyStore y Signature para
+ * el acceso a los certificados y claves del DNI electronico, asi como la realizacion
+ * de operaciones criptograficas de firma con el DNI electronico. El Controlador ha
  * sido disenado para su funcionamiento independiente del sistema operativo final.
  * 
- * Copyright (C) 2012 Direccion General de Modernizacion Administrativa, Procedimientos 
+ * Copyright (C) 2012 Direccion General de Modernizacion Administrativa, Procedimientos
  * e Impulso de la Administracion Electronica
  * 
  * Este programa es software libre y utiliza un licenciamiento dual (LGPL 2.1+
  * o EUPL 1.1+), lo cual significa que los usuarios podran elegir bajo cual de las
- * licencias desean utilizar el codigo fuente. Su eleccion debera reflejarse 
+ * licencias desean utilizar el codigo fuente. Su eleccion debera reflejarse
  * en las aplicaciones que integren o distribuyan el Controlador, ya que determinara
  * su compatibilidad con otros componentes.
  *
- * El Controlador puede ser redistribuido y/o modificado bajo los terminos de la 
- * Lesser GNU General Public License publicada por la Free Software Foundation, 
+ * El Controlador puede ser redistribuido y/o modificado bajo los terminos de la
+ * Lesser GNU General Public License publicada por la Free Software Foundation,
  * tanto en la version 2.1 de la Licencia, o en una version posterior.
  * 
- * El Controlador puede ser redistribuido y/o modificado bajo los terminos de la 
- * European Union Public License publicada por la Comision Europea, 
+ * El Controlador puede ser redistribuido y/o modificado bajo los terminos de la
+ * European Union Public License publicada por la Comision Europea,
  * tanto en la version 1.1 de la Licencia, o en una version posterior.
  * 
  * Deberia recibir una copia de la GNU Lesser General Public License, si aplica, junto
@@ -41,123 +41,147 @@ package es.gob.jmulticard.apdu;
 
 import java.io.ByteArrayOutputStream;
 
-/** Comando APDU para comunicaci&oacute;n con tarjeta inteligente.
- * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s */
+/**
+ * Comando APDU para comunicaci&oacute;n con tarjeta inteligente.
+ * 
+ * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s
+ */
 public abstract class CommandApdu extends Apdu {
 
-    private final byte cla;
-    private final byte ins;
-    private final byte p1;
-    private final byte p2;
-    private Integer le;
-    private final byte[] body;
+	private final byte cla;
+	private final byte ins;
+	private final byte p1;
+	private final byte p2;
+	private Integer le;
+	private final byte[] body;
 
-    protected CommandApdu(final byte cla,
-                          final byte ins,
-                          final byte param1,
-                          final byte param2,
-                          final byte[] data,
-                          final Integer ne) {
-        super();
-        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	protected CommandApdu(final byte cla, final byte ins, final byte param1,
+			final byte param2, final byte[] data, final Integer ne) {
+		super();
+		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
-        this.cla = cla;
-        baos.write(cla);
+		this.cla = cla;
+		baos.write(cla);
 
-        this.ins = ins;
-        baos.write(ins);
+		this.ins = ins;
+		baos.write(ins);
 
-        this.p1 = param1;
-        baos.write(param1);
+		this.p1 = param1;
+		baos.write(param1);
 
-        this.p2 = param2;
-        baos.write(param2);
+		this.p2 = param2;
+		baos.write(param2);
 
-        if (data == null) {
-            this.body = null;
-        }
-        else {
-            this.body = new byte[data.length];
-            System.arraycopy(data, 0, this.body, 0, data.length);
+		if (data == null) {
+			this.body = null;
+		} else {
+			this.body = new byte[data.length];
+			System.arraycopy(data, 0, this.body, 0, data.length);
 
-            baos.write(Integer.valueOf(String.valueOf(this.body.length)).byteValue());
+			baos.write(Integer.valueOf(String.valueOf(this.body.length))
+					.byteValue());
 
-            if (this.body.length > 0) {
-                try {
-                    baos.write(this.body);
-                }
-                catch (final Exception e) {
-                    throw new IllegalArgumentException("No se pueden tratar los datos de la APDU: " + e, e); //$NON-NLS-1$
-                }
-            }
-        }
-        this.le = ne;
-        if (ne != null) {
-            baos.write(ne.byteValue());
-        }
-        setBytes(baos.toByteArray());
-    }
+			if (this.body.length > 0) {
+				try {
+					baos.write(this.body);
+				} catch (final Exception e) {
+					throw new IllegalArgumentException(
+							"No se pueden tratar los datos de la APDU: " + e); //$NON-NLS-1$
+				}
+			}
+		}
+		this.le = ne;
+		if (ne != null) {
+			baos.write(ne.byteValue());
+		}
+		setBytes(baos.toByteArray());
+	}
 
-    /** Devuelve el primer par&aacute;metro (P1) de la APDU.
-     * @return Primer par&aacute;metro (P1) de la APDU */
-    public byte getP1() {
-        return this.p1;
-    }
+	/**
+	 * Devuelve la clase (CLA) de APDU.
+	 * 
+	 * @return Clase (CLA) de APDU
+	 */
+	public byte getCla() {
+		return this.cla;
+	}
 
-    /** Devuelve el segundo par&aacute;metro (P2) de la APDU.
-     * @return Segundo par&aacute;metro (P2) de la APDU */
-    public byte getP2() {
-        return this.p2;
-    }
+	/**
+	 * Obtiene el cuerpo de la APDU.
+	 * 
+	 * @return Cuerpo de la APDU, o <code>null</code> si no est&aacute;
+	 *         establecido
+	 */
+	public byte[] getData() {
+		if (this.body == null) {
+			return null;
+		}
+		final byte[] out = new byte[this.body.length];
+		System.arraycopy(this.body, 0, out, 0, this.body.length);
+		return out;
+	}
 
-    /** Devuelve la clase (CLA) de APDU.
-     * @return Clase (CLA) de APDU */
-    public byte getCla() {
-        return this.cla;
-    }
+	/**
+	 * Devuelve el octeto identificador de la instrucci&oacute;n (INS) que esta
+	 * APDU representa.
+	 * 
+	 * @return Identificador de instrucci&oacute;n
+	 */
+	public byte getIns() {
+		return this.ins;
+	}
 
-    /** Devuelve el octeto identificador de la instrucci&oacute;n (INS) que esta APDU representa.
-     * @return Identificador de instrucci&oacute;n */
-    public byte getIns() {
-        return this.ins;
-    }
+	/**
+	 * Obtiene el n&uacute;mero m&aacute;ximo de octetos esperados en la APDU de
+	 * respuesta.
+	 * 
+	 * @return N&uacute;mero m&aacute;ximo de octetos esperados en la APDU de
+	 *         respuesta, o <code>null</code> si no est&aacute; establecido
+	 */
+	public Integer getLe() {
+		return this.le;
+	}
 
-    /** Obtiene el cuerpo de la APDU.
-     * @return Cuerpo de la APDU, o <code>null</code> si no est&aacute; establecido */
-    public byte[] getData() {
-    	if (this.body == null) {
-    		return null;
-    	}
-        final byte[] out = new byte[this.body.length];
-        System.arraycopy(this.body, 0, out, 0, this.body.length);
-        return out;
-    }
+	/**
+	 * Devuelve el primer par&aacute;metro (P1) de la APDU.
+	 * 
+	 * @return Primer par&aacute;metro (P1) de la APDU
+	 */
+	public byte getP1() {
+		return this.p1;
+	}
 
-    /** Obtiene el n&uacute;mero m&aacute;ximo de octetos esperados en la APDU de respuesta.
-     * @return N&uacute;mero m&aacute;ximo de octetos esperados en la APDU de respuesta, o <code>null</code> si no est&aacute; establecido */
-    public Integer getLe() {
-        return this.le;
-    }
+	/**
+	 * Devuelve el segundo par&aacute;metro (P2) de la APDU.
+	 * 
+	 * @return Segundo par&aacute;metro (P2) de la APDU
+	 */
+	public byte getP2() {
+		return this.p2;
+	}
 
-    /** Establece el n&uacute;mero de octetos esperados en la APDU de respuesta.
-     * @param le N&uacute;mero esperado de octetos.
-      */
-    public void setLe(final int le) {
-        this.le = Integer.valueOf(String.valueOf(le));
-        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        baos.write(this.cla);
-        baos.write(this.ins);
-        baos.write(this.p1);
-        baos.write(this.p2);
-        if (this.body != null && this.body.length > 0) {
-            try {
-                baos.write(this.body);
-            }
-            catch (final Exception e) {
-                throw new IllegalArgumentException("No se pueden tratar los datos de la APDU: " + e, e); //$NON-NLS-1$
-            }
-        }
-        baos.write(le);
-        setBytes(baos.toByteArray());
-    }
+	/**
+	 * Establece el n&uacute;mero de octetos esperados en la APDU de respuesta.
+	 * 
+	 * @param le
+	 *            N&uacute;mero esperado de octetos.
+	 */
+	public void setLe(final int le) {
+		this.le = Integer.valueOf(String.valueOf(le));
+		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		baos.write(this.cla);
+		baos.write(this.ins);
+		baos.write(this.p1);
+		baos.write(this.p2);
+		if (this.body != null && this.body.length > 0) {
+			try {
+				baos.write(this.body);
+			} catch (final Exception e) {
+				throw new IllegalArgumentException(
+						"No se pueden tratar los datos de la APDU: " + e); //$NON-NLS-1$
+			}
+		}
+		baos.write(le);
+		setBytes(baos.toByteArray());
+	}
 }
