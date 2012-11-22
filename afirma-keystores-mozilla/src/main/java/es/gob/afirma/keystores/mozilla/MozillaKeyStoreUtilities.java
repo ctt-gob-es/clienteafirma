8727ccc4e92723c320b65d56a6b5174b2e94706d
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2012-11-22 15:42:47 +0000

    Corrección menor
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/afirma/trunk@3347 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-keystores-mozilla/src/main/java/es/gob/afirma/keystores/mozilla/MozillaKeyStoreUtilities.java
+++ afirma-keystores-mozilla/src/main/java/es/gob/afirma/keystores/mozilla/MozillaKeyStoreUtilities.java
@@ -17,7 +17,6 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.security.Provider;
 import java.security.Security;
 import java.util.ArrayList;
