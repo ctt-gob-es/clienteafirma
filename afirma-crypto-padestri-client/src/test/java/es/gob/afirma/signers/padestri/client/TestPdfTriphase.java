7392a3f805ae0323120005eea423cab990c33b72
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2012-11-27 10:09:06 +0000

    Correcciones menores
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/afirma/trunk@3387 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-crypto-padestri-client/src/test/java/es/gob/afirma/signers/padestri/client/TestPdfTriphase.java
+++ afirma-crypto-padestri-client/src/test/java/es/gob/afirma/signers/padestri/client/TestPdfTriphase.java
@@ -14,6 +14,7 @@ import es.gob.afirma.core.misc.AOUtil;
 import es.gob.afirma.core.misc.Base64;
 import es.gob.afirma.core.signers.AOSigner;
 
+/** Pruebas de firma trif&aacute;sica. */
 public class TestPdfTriphase {
 
 	/** Nombre de la propiedad de URL del servidor de firma trif&aacute;sica. */
@@ -32,8 +33,6 @@ public class TestPdfTriphase {
     private static final String CERT_PASS = "12341234"; //$NON-NLS-1$
     private static final String CERT_ALIAS = "anf usuario activo"; //$NON-NLS-1$
 
-    private static final String TEST_PDF_FILE = "TEST_PDF.pdf"; //$NON-NLS-1$
-    private static final String TEST_IMAGE_FILE = "splash.png"; //$NON-NLS-1$
     private static final String TEST_XADES_FILE = "activity_petition_details_xml.xsig"; //$NON-NLS-1$
 
     private PrivateKeyEntry pke;
@@ -41,7 +40,7 @@ public class TestPdfTriphase {
     private Properties serverConfig;
 
 	@Before
-	public void loadKeystore() throws Exception {
+	private void loadKeystore() throws Exception {
 
 		// Cargamos la referencia a la clave privada
         final KeyStore ks = KeyStore.getInstance("PKCS12"); //$NON-NLS-1$
@@ -54,6 +53,8 @@ public class TestPdfTriphase {
         this.serverConfig.setProperty(PROPERTY_NAME_DOC_ID, PROPERTY_VALUE_DOC_ID);
 	}
 
+	/** Prueba de firma trif&aacute;sica normal.
+	 * @throws Exception */
 	@Test
 	public void firma() throws Exception {
 		final AOSigner signer = new AOPDFTriPhaseSigner();
@@ -66,42 +67,26 @@ public class TestPdfTriphase {
         Assert.assertEquals("No se recibió un OK desde servidor", "OK", new String(result)); //$NON-NLS-1$ //$NON-NLS-2$
 	}
 
+	/** Prueba de firma trif&aacute;sica con adjunto en el PDF.
+	 * @throws Exception */
 	@Test
 	public void firmaConAdjunto() throws Exception {
 		final AOSigner signer = new AOPDFTriPhaseSigner();
-//		final AOSigner signer = new AOPDFSigner();
 
 		final Properties config = new Properties(this.serverConfig);
 		for (final String key : this.serverConfig.keySet().toArray(new String[this.serverConfig.size()])) {
 			config.setProperty(key, this.serverConfig.getProperty(key));
 		}
 
-//		config.setProperty(PROPERTY_NAME_ATTACH, loadFileOnBase64(TEST_IMAGE_FILE));
-//		config.setProperty(PROPERTY_NAME_ATTACH_FILENAME, "splash.png"); //$NON-NLS-1$
-//		config.setProperty(PROPERTY_NAME_ATTACH_DESCRIPTION, "Imagen adjunta de prueba"); //$NON-NLS-1$
-
-//		config.setProperty(PROPERTY_NAME_ATTACH, Base64.encode("Hola Mundo!!".getBytes()));
-//		config.setProperty(PROPERTY_NAME_ATTACH_FILENAME, "hola.txt"); //$NON-NLS-1$
-//		config.setProperty(PROPERTY_NAME_ATTACH_DESCRIPTION, "Adjunto de prueba"); //$NON-NLS-1$
-
 		config.setProperty(PROPERTY_NAME_ATTACH, loadFileOnBase64(TEST_XADES_FILE));
 		config.setProperty(PROPERTY_NAME_ATTACH_FILENAME, "firma_metadatos.xml"); //$NON-NLS-1$
 		config.setProperty(PROPERTY_NAME_ATTACH_DESCRIPTION, "Metadatos ENI firmados"); //$NON-NLS-1$
 
         final byte[] result = signer.sign(null, "SHA512withRSA", this.pke, config); //$NON-NLS-1$
-//		final byte[] result = signer.sign(
-//				AOUtil.getDataFromInputStream(ClassLoader.getSystemResourceAsStream(TEST_PDF_FILE)),
-//				"SHA512withRSA", this.pke, config); //$NON-NLS-1$
 
         Assert.assertNotNull("Error durante el proceso de firma, resultado nulo", result); //$NON-NLS-1$
         Assert.assertEquals("No se recibió un OK desde servidor", "OK", new String(result)); //$NON-NLS-1$ //$NON-NLS-2$
 
-//        final File tempFile = File.createTempFile("Prueba", ".pdf");
-//        final FileOutputStream fos = new FileOutputStream(tempFile);
-//        fos.write(result);
-//        fos.close();
-//
-//        System.out.println("Guardamos los datos en el fichero: " + tempFile.getAbsolutePath());
 	}
 
 	private static String loadFileOnBase64(final String filename) throws Exception {
