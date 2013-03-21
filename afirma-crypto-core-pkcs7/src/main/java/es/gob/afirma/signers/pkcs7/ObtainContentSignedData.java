fcab193d2784f6c8571bbe4efc025bf7b3699a87
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2013-03-21 11:39:39 +0000

    Se agrega un método para la obtención del hash de los datos firmados con un algoritmo determinado
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/afirma/trunk@3765 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-crypto-core-pkcs7/src/main/java/es/gob/afirma/signers/pkcs7/ObtainContentSignedData.java
+++ afirma-crypto-core-pkcs7/src/main/java/es/gob/afirma/signers/pkcs7/ObtainContentSignedData.java
@@ -16,62 +16,127 @@ import java.util.logging.Logger;
 
 import org.bouncycastle.asn1.ASN1InputStream;
 import org.bouncycastle.asn1.ASN1Sequence;
+import org.bouncycastle.asn1.ASN1Set;
 import org.bouncycastle.asn1.ASN1TaggedObject;
 import org.bouncycastle.asn1.DERObjectIdentifier;
 import org.bouncycastle.asn1.DEROctetString;
+import org.bouncycastle.asn1.DERSet;
+import org.bouncycastle.asn1.cms.CMSAttributes;
 import org.bouncycastle.asn1.cms.ContentInfo;
 import org.bouncycastle.asn1.cms.SignedData;
+import org.bouncycastle.asn1.cms.SignerInfo;
 import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
 
 /** Clase que obtiene el contenido de un fichero en formato SignedData. de CMS o
  * CADES. */
 public final class ObtainContentSignedData {
 
-    private ObtainContentSignedData() {
-        // No permitimos la instanciacion
-    }
+	private ObtainContentSignedData() {
+		// No permitimos la instanciacion
+	}
 
-    private static final Logger LOGGER = Logger.getLogger("es.gob.afirma"); //$NON-NLS-1$
+	private static final Logger LOGGER = Logger.getLogger("es.gob.afirma"); //$NON-NLS-1$
 
-    /** M&eacute;todo que obtiene el contenido firmado de un tipo Signed Data
-     * tanto en CADES como en CMS. Si la firma no contiene los datos, devuelve <code>null</code>.
-     * @param data
-     *        datos que contienen la firma.
-     * @return el contenido firmado.
-     * @throws IOException Si no se pueden leer los datos */
-    public static byte[] obtainData(final byte[] data) throws IOException {
-        byte[] contenido = null;
+	/** M&eacute;todo que obtiene el contenido firmado de un tipo Signed Data
+	 * tanto en CADES como en CMS. Si la firma no contiene los datos, devuelve <code>null</code>.
+	 * @param data
+	 *        datos que contienen la firma.
+	 * @return el contenido firmado.
+	 * @throws IOException Si no se pueden leer los datos */
+	public static byte[] obtainData(final byte[] data) throws IOException {
+		byte[] contenido = null;
 
-        // LEEMOS EL FICHERO QUE NOS INTRODUCEN
-        final ASN1InputStream is = new ASN1InputStream(data);
-        final ASN1Sequence dsq  = (ASN1Sequence) is.readObject();
-        is.close();
+		// LEEMOS EL FICHERO QUE NOS INTRODUCEN
+		final ASN1InputStream is = new ASN1InputStream(data);
+		final ASN1Sequence dsq  = (ASN1Sequence) is.readObject();
+		is.close();
 
-        final Enumeration<?> e = dsq.getObjects();
-        // Elementos que contienen los elementos OID Data
-        final DERObjectIdentifier doi = (DERObjectIdentifier) e.nextElement();
-        // Contenido a obtener informacion
-        final ASN1TaggedObject doj = (ASN1TaggedObject) e.nextElement();
+		final Enumeration<?> e = dsq.getObjects();
+		// Elementos que contienen los elementos OID Data
+		final DERObjectIdentifier doi = (DERObjectIdentifier) e.nextElement();
+		// Contenido a obtener informacion
+		final ASN1TaggedObject doj = (ASN1TaggedObject) e.nextElement();
 
-        // buscamos si es signedData
-        if (doi.equals(PKCSObjectIdentifiers.signedData)) {
-            // obtenemos el signed Data
-            final SignedData sd = SignedData.getInstance(doj.getObject());
-            final ContentInfo ci = sd.getEncapContentInfo();
-            // obtenemos el contenido si lo tiene.
-            if (ci.getContent() != null) {
-                final DEROctetString os = (DEROctetString) ci.getContent();
-                contenido = os.getOctets();
-            }
-            else {
-                LOGGER.warning("No existe contenido en esta firma."); //$NON-NLS-1$
-            }
-        }
-        else {
-            LOGGER.warning("No se puede obtener el contenido de esta firma."); //$NON-NLS-1$
-        }
+		// buscamos si es signedData
+		if (doi.equals(PKCSObjectIdentifiers.signedData)) {
+			// obtenemos el signed Data
+			final SignedData sd = SignedData.getInstance(doj.getObject());
+			final ContentInfo ci = sd.getEncapContentInfo();
+			// obtenemos el contenido si lo tiene.
+			if (ci.getContent() != null) {
+				contenido = ((DEROctetString) ci.getContent()).getOctets();
+			}
+			else {
+				LOGGER.warning("No existe contenido en esta firma."); //$NON-NLS-1$
+			}
+		}
+		else {
+			LOGGER.warning("No se puede obtener el contenido de esta firma."); //$NON-NLS-1$
+		}
 
-        return contenido;
-    }
+		return contenido;
+	}
+
+	/** M&eacute;todo que obtiene la huella digital de los datos firmados en una firma CMS/CAdES.
+	 * La huella se obtenida estar&aacute; generada con el algoritmo de huella indicado, si este
+	 * algoritmo es el que se utiliz&oacute; en alguna de las operaci&oacute;nes de firma con la
+	 * que se gener&oacute; esta firma. Si no se utiliz&oacute, este algoritmo, se devuelve
+	 * {@code null}.
+	 * @param data
+	 *        datos que contienen la firma.
+	 * @return La huella digital de los datos firmados generada con el algoritmo indicado, o
+	 * {@code null} si esta no se encuentra en la firma.
+	 * @throws IOException Si no se pueden leer los datos */
+	public static byte[] obtainMessageDigest(final byte[] signature, final String digestAlgorithm) throws IOException {
+
+		// LEEMOS EL FICHERO QUE NOS INTRODUCEN
+		final ASN1InputStream is = new ASN1InputStream(signature);
+		final ASN1Sequence dsq  = (ASN1Sequence) is.readObject();
+		is.close();
+
+		final Enumeration<?> e = dsq.getObjects();
+
+		// Elementos que contienen los elementos OID Data
+		final DERObjectIdentifier doi = (DERObjectIdentifier) e.nextElement();
+
+		// Comprobamos que sea una firma
+		if (!doi.equals(PKCSObjectIdentifiers.signedData)) {
+			LOGGER.warning("No se puede obtener el contenido de esta firma."); //$NON-NLS-1$
+			return null;
+		}
+
+		// Contenido a obtener informacion
+		final ASN1TaggedObject doj = (ASN1TaggedObject) e.nextElement();
+		final SignedData sd = SignedData.getInstance(doj.getObject());
+		final ASN1Set signerInfosSd = sd.getSignerInfos();
+
+		byte[] messageDigest = null;
+
+		for (int i = 0; i < signerInfosSd.size(); i++) {
+			final SignerInfo si = new SignerInfo((ASN1Sequence) signerInfosSd.getObjectAt(i));
+			final AlgorithmIdentifier algHash = si.getDigestAlgorithm();
+			if (algHash.getAlgorithm().toString().equals(AOAlgorithmID.getOID(digestAlgorithm))) {
+				final ASN1Set signedAttrib = si.getAuthenticatedAttributes();
+				for (int s = 0; s < signedAttrib.size(); s++) {
+					final ASN1Sequence elemento = (ASN1Sequence) signedAttrib.getObjectAt(s);
+					final DERObjectIdentifier oids = (DERObjectIdentifier) elemento.getObjectAt(0);
+					if (CMSAttributes.messageDigest.getId().equals(oids.toString())) {
+						final DERSet derSetHash = (DERSet) elemento.getObjectAt(1);
+						final DEROctetString derHash = (DEROctetString) derSetHash.getObjectAt(0);
+						messageDigest = derHash.getOctets();
+						break;
+					}
+				}
+				if (messageDigest != null) {
+					break;
+				}
+			}
+		}
+		if (messageDigest == null) {
+			LOGGER.warning("No se ha encontrado en la firma una huella digital generada con el algoritmo: " + digestAlgorithm); //$NON-NLS-1$
+		}
+		return messageDigest;
+	}
 
 }
