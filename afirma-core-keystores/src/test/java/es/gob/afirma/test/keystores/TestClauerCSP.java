2796b7cf5880329a5adeef8e5ce1b5bc73d2b0a1
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2013-04-18 17:10:46 +0000

    Se agrega una clase para pruebas con dispositivos Clauer a través de su CSP
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/afirma/trunk@3838 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-core-keystores/src/test/java/es/gob/afirma/test/keystores/TestClauerCSP.java
+++ afirma-core-keystores/src/test/java/es/gob/afirma/test/keystores/TestClauerCSP.java
@@ -1,25 +1,19 @@
 package es.gob.afirma.test.keystores;
 
+import java.security.KeyStore;
 import java.security.KeyStore.PrivateKeyEntry;
 import java.security.Signature;
-
-import javax.security.auth.callback.PasswordCallback;
 
 import org.junit.Ignore;
 import org.junit.Test;
 
 import es.gob.afirma.core.misc.Base64;
-import es.gob.afirma.keystores.main.callbacks.CachePasswordCallback;
-import es.gob.afirma.keystores.main.common.AOKeyStore;
-import es.gob.afirma.keystores.main.common.AOKeyStoreManager;
-import es.gob.afirma.keystores.main.common.AOKeyStoreManagerFactory;
 
 /** Prueba simple de firma con PKCS#11.
  * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s */
 public final class TestClauerCSP {
 
-	private static final char[] PIN = "12341234".toCharArray(); //$NON-NLS-1$
-	private static final String ALIAS = "CLAUER_PERSONA FISICA DE LA PEÇA DE PROVES - 111118550"; //$NON-NLS-1$
+	private static final String ALIAS = "CLAUER_PERSONA FISICA DE LA PEÇA DE PROVES"; //$NON-NLS-1$
 
 	/** Prueba de firma con CSP.
 	 * @throws Exception */
@@ -28,28 +22,17 @@ public final class TestClauerCSP {
 	@Ignore
 	public void testCapi() throws Exception {
 
-		final PasswordCallback pc = new CachePasswordCallback(PIN);
+		final KeyStore ks = KeyStore.getInstance("WINDOWS-MY"); //$NON-NLS-1$
+		ks.load(null, null);
 
-		final AOKeyStoreManager ksm = AOKeyStoreManagerFactory.getAOKeyStoreManager(
-    		AOKeyStore.WINDOWS,
-    		null,
-    		"Windows", //$NON-NLS-1$
-    		pc,
-    		null
-		);
+		final PrivateKeyEntry pke = (PrivateKeyEntry) ks.getEntry(ALIAS, new KeyStore.PasswordProtection(new char[0]));
 
-		final PrivateKeyEntry pke = ksm.getKeyEntry(ALIAS, pc);
+		final Signature signature = Signature.getInstance("SHA1withRSA"); //$NON-NLS-1$
+		signature.initSign(pke.getPrivateKey());
+		signature.update("Hola Mundo!!".getBytes()); //$NON-NLS-1$
+		final byte[] mySignature = signature.sign();
 
-		try {
-			final Signature signature = Signature.getInstance("SHA1withRSA"); //$NON-NLS-1$
-			signature.initSign(pke.getPrivateKey());
-			signature.update("Hola Mundo!!".getBytes()); //$NON-NLS-1$
-			final byte[] mySignature = signature.sign();
-			System.out.println(Base64.encode(mySignature));
-		} catch (final Exception e) {
-			System.err.println("Ocurrio un error durante el proceso de firma: " + e); //$NON-NLS-1$
-			e.printStackTrace();
-		}
+		System.out.println(Base64.encode(mySignature));
 	}
 
 	/** M&eacute;todo de entrada.
