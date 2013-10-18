8b5aed3b241b0a6c873ffb2e1129604cc52ca863
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2013-10-18 07:20:03 +0000

    Se mejora la función de comprobación de si el certificado de firma del JAR es de confianza para el usuario y su inserción en el almacén
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/project/afirma/trunk@4233 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-core-keystores/src/main/java/es/gob/afirma/keystores/misc/JarSignatureCertExtractor.java
+++ afirma-core-keystores/src/main/java/es/gob/afirma/keystores/misc/JarSignatureCertExtractor.java
@@ -25,7 +25,6 @@ import java.util.zip.ZipInputStream;
 
 import sun.security.pkcs.PKCS7;
 import es.gob.afirma.core.misc.AOUtil;
-import es.gob.afirma.core.misc.Platform;
 import es.gob.afirma.core.ui.AOUIFactory;
 
 /** Clase de utilidad para obtener los certificados de la firma del JAR
@@ -34,22 +33,24 @@ import es.gob.afirma.core.ui.AOUIFactory;
  * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s */
 public final class JarSignatureCertExtractor {
 
+	private static final Logger LOGGER = Logger.getLogger("es.gob.afirma"); //$NON-NLS-1$
+	
 	private static final int BUFFER_SIZE = 1024;
-
+	
 	private static final String SIGNATURE_DIR_PATH = "META-INF/"; //$NON-NLS-1$
 	private static final String SIGNATURE_EXT = ".RSA"; //$NON-NLS-1$
-
+	
 	private static final String USER_HOME = "$USER_HOME"; //$NON-NLS-1$
+	
+	private static final String DEFAULT_PASSWORD = ""; //$NON-NLS-1$
 	private static final String[] CACERTS_DEFAULT_PASSWORDS = {
-		"", //$NON-NLS-1$
+		DEFAULT_PASSWORD,
 		"changeit", //$NON-NLS-1$
 		"changeme", //$NON-NLS-1$
 	};
 
-	private static final Logger LOGGER = Logger.getLogger("es.gob.afirma"); //$NON-NLS-1$
-
 	private static String keystorePassword = null;
-
+	
 	private JarSignatureCertExtractor() {
 		// No permitimos la instanciacion
 	}
@@ -67,7 +68,7 @@ public final class JarSignatureCertExtractor {
 		if (src == null) {
 			throw new IOException("No se ha podido acceder a los recursos del JAR"); //$NON-NLS-1$
 		}
-
+		
 		int n = 0;
 		ZipEntry e;
 		ByteArrayOutputStream baos = null;
@@ -85,8 +86,13 @@ public final class JarSignatureCertExtractor {
 		}
 		return baos == null ? null : baos.toByteArray();
 	}
-
-	private static File getJavaCaKeyStoreFile() {
+	
+	/**
+	 * Recupera el fichero con el almac&eacute;n de CAs de confianza del usuario.
+	 * Si no lo localiza ni lo puede crear, devuelve {@code null}.
+	 * @return Fichero con el almac&eacute;n de CAs de confianza del usuario.
+	 */
+	private static File getUsersJavaCaKeyStoreFile() {
 
 		String keystoreFilename = System.getProperty(
 			"deployment.user.security.trusted.cacerts" //$NON-NLS-1$
@@ -100,33 +106,32 @@ public final class JarSignatureCertExtractor {
 			);
 		}
 
-		File ret = keystoreFilename != null ? new File(keystoreFilename) : null;
-
-		// Si no existe el alamcen indicado por la variable de entorno o esta variable
-		// no estaba establecida buscamos en la ruta por defecto
-		if (ret == null || !ret.exists()) {
-			final String defaultPath = Platform.getJavaHome() +
-				File.separator +
-				"lib" + //$NON-NLS-1$
-				File.separator +
-				"security" + //$NON-NLS-1$
-				File.separator +
-				"cacerts"; //$NON-NLS-1$
-
-			ret = new File(defaultPath);
-		}
-
-		if (!ret.exists()) {
-			throw new IllegalStateException(
-					"No se ha encontrado el almacen de certificados raiz de Java" //$NON-NLS-1$
-				);
-		}
-
-		return ret;
+		final File ret = keystoreFilename != null ? new File(keystoreFilename) : null;
+		
+		// Devolvemos el fichero con el truststore si existe su directorio padre, ya que entonces
+		// o existe o lo podemos crear en ese directorio
+		return (ret != null && ret.getParentFile().exists()) ? ret : null; 
 	}
 
+	/**
+	 * Recupera el fichero con el almac&eacute;n de CAs de confianza del sistema.
+	 * Si no lo localiza, devuelve {@code null}.
+	 * @return Fichero con el almac&eacute;n de CAs de confianza del sistema.
+	 */
+	private static File getSystemsJavaCaKeyStoreFile() {
 
+		final String keystoreFilename = System.getProperty(
+			"deployment.system.security.cacerts" //$NON-NLS-1$
+		);
 
+		if (keystoreFilename == null) {
+			return null;
+		}
+		
+		final File ret = new File(keystoreFilename);
+		return ret.exists() ? ret : null;
+	}
+	
 	private static KeyStore getJavaCaKeyStore(final File storeFile) throws KeyStoreException,
 	                                                                        NoSuchAlgorithmException,
 	                                                                        CertificateException,
@@ -148,18 +153,23 @@ public final class JarSignatureCertExtractor {
 		}
 		fis.close();
 
-
-
+		
+		
 		return ks;
 	}
 
-	private static void chechCertChain(final X509Certificate[] chain,
+	private static void checkCertChain(final X509Certificate[] chain,
 			                           final KeyStore trustStore) throws CertPathValidatorException,
 			                                                             KeyStoreException,
 			                                                             InvalidAlgorithmParameterException,
 			                                                             CertificateException,
 			                                                             NoSuchAlgorithmException {
-
+		
+		// Si no hay certificados en el almacen, no estara entre los certificados de confianza
+		if (trustStore.size() == 0) {
+			throw new CertPathValidatorException("No hay certificados en el almacen de confianza"); //$NON-NLS-1$
+		}
+		
 		// Miramos si el certificado mas elevado de la cadena esta en el trustrore,
 		// en cuyo caso no hacemos nada
 		final X509Certificate chainEdge = chain[chain.length - 1];
@@ -194,44 +204,65 @@ public final class JarSignatureCertExtractor {
 	                                                                              CertificateException,
 	                                                                              IOException,
 	                                                                              InvalidAlgorithmParameterException {
-		// Primero obtenemos el almacen CA de Java, asi si no se encuentra o la
-		// contrasena no es la por defecto o se continua
-		final File cacertFile;
-		try {
-			cacertFile = getJavaCaKeyStoreFile();
-		} catch (final Exception e) {
-			LOGGER.warning("No se ha podido encontrar el truststore por defecto de Java: " + e); //$NON-NLS-1$
-			return;
-		}
-		if (!cacertFile.canWrite() || !cacertFile.canRead()) {
-			LOGGER.warning("No se tienen permisos para leer/editar el truststore por defecto de Java"); //$NON-NLS-1$
-			return;
-		}
 
-		final KeyStore cacerts = getJavaCaKeyStore(cacertFile);
-
-		// Obtenemos los certificados con los que se ha firmado este JAR
+		// Primero, obtenemos los certificados con los que se ha firmado la aplicacion
 		final X509Certificate[] certs = getJarSignatureCertChain();
 		if (certs == null || certs.length < 1) {
+			LOGGER.warning("La aplicacion no esta firmada"); //$NON-NLS-1$
 			return;
 		}
-
+		
+		// A continuacion, comprobamos si esos certificados son de confianza para el sistema
+		final File systemsCaCertFile = getSystemsJavaCaKeyStoreFile();
+		if (systemsCaCertFile != null) {
+			try {
+				checkCertChain(certs, getJavaCaKeyStore(systemsCaCertFile));
+				// Si no salta excepcion, salimos, porque ha validado y no hay que hacer nada
+				LOGGER.info("Los certificados de firma del JAR son de confianza en Java"); //$NON-NLS-1$
+				return;
+			}
+			catch (final Exception e) {
+				LOGGER.info("Error en la validacion de los certificados contra el almacen de Java"); //$NON-NLS-1$
+				// Si falla continuamos con el almacen de confianza del usuario
+			}
+		}
+		
+		// Si no son de confianza para el sistema, comprobamos si lo son para el usuario
+		// Cargamos el fichero del almacen
+		final File usersCaCertFile = getUsersJavaCaKeyStoreFile();
+		if (usersCaCertFile == null) {
+			LOGGER.warning("No se puede localizar el almacen de confianza del usuario, se suspende la validacion"); //$NON-NLS-1$
+			return;
+		}
+		
+		// Si existe el fichero, lo cargamos, si no existe pero se puede crear, lo creamos  
+		final KeyStore usersTruststore;
+		if (!usersCaCertFile.exists()) {
+			keystorePassword = DEFAULT_PASSWORD;
+			usersTruststore = KeyStore.getInstance(KeyStore.getDefaultType());
+			usersTruststore.load(null, keystorePassword.toCharArray());
+			LOGGER.info("Creamos el truststore ya que no existia previamente"); //$NON-NLS-1$
+		}
+		else {
+			usersTruststore = getJavaCaKeyStore(usersCaCertFile);
+		}
+		
 		// Comprobamos si el extremo de la cadena es de confianza o no
 		try {
-			chechCertChain(certs, cacerts);
+			checkCertChain(certs, usersTruststore);
 			// Si no salta excepcion, salimos, porque ha validado y no hay que hacer
 			// nada
-			LOGGER.info("Los certificados de firma del JAR ya son de confianza en Java"); //$NON-NLS-1$
+			LOGGER.info("Los certificados de firma del JAR ya son de confianza para el usuario"); //$NON-NLS-1$
 			return;
 		}
 		catch (final CertPathValidatorException e) {
-			LOGGER.warning("Debemos agregar el certificado al truststore para que sea de confianza: " + e); //$NON-NLS-1$
+			LOGGER.warning("Debemos agregar el certificado al truststore del usuario para que sea de confianza: " + e); //$NON-NLS-1$
 			// Se ignora, porque si falla la validacion es que debemos continuar
 			// normalmente con el proceso, ya que significa que no se valida la
-			// cadena y hay que hacer insertar la raiz
+			// cadena y hay que insertar la raiz
 		}
 
-		// Creamos una lista de que se va a insertar para preguntarle al usuario
+		// Creamos la lista de certificados que se van a insertar para preguntarle al usuario
 		final StringBuilder sb = new StringBuilder("<br>"); //$NON-NLS-1$
 		for (final X509Certificate cert : certs) {
 			sb.append("&nbsp;- "); //$NON-NLS-1$
@@ -258,17 +289,16 @@ public final class JarSignatureCertExtractor {
 		}
 
 		for (final X509Certificate cert : certs) {
-			cacerts.setCertificateEntry(
+			usersTruststore.setCertificateEntry(
 				AOUtil.getCN(cert) + cert.getSerialNumber(),
 				cert
 			);
 		}
 
-		final FileOutputStream fos = new FileOutputStream(cacertFile);
-		cacerts.store(fos, keystorePassword.toCharArray());
+		final FileOutputStream fos = new FileOutputStream(usersCaCertFile);
+		usersTruststore.store(fos, keystorePassword.toCharArray());
 		fos.close();
 
 		LOGGER.info("Se han insertado correctamente certificados en el cacerts del usuario"); //$NON-NLS-1$
-
 	}
 }
