3e36c3bb4c06cbe2fc89da939b8418172ea57d06
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2013-11-18 10:04:06 +0000

    Se implementa la serialización XML de PdfSign, falta la deserialización
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/project/afirma/trunk@4313 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-crypto-pdf/src/main/java/es/gob/afirma/signers/pades/PdfSignResult.java
+++ afirma-crypto-pdf/src/main/java/es/gob/afirma/signers/pades/PdfSignResult.java
@@ -1,14 +1,30 @@
 package es.gob.afirma.signers.pades;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.Calendar;
 import java.util.Properties;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import es.gob.afirma.core.misc.Base64;
 
 
 /** Resultado de una pre-firma (como primera parte de un firma trif&aacute;sica) o una firma completa PAdES. */
 /** <i>JavaBean</i> que encapsula los resultados de la pre-firma o firma completa PDF. */
-public final class PdfSignResult {
+public final class PdfSignResult implements Serializable {
 
-    private final String fileID;
+	private static final long serialVersionUID = 2L;
+
+	private final String fileID;
     private final byte[] sign;
     private final Calendar signTime;
     private final Properties extraParams;
@@ -48,6 +64,64 @@ public final class PdfSignResult {
      * @return Momento en el que se realiz&oacute; la firma */
     public Calendar getSignTime() {
     	return this.signTime;
+    }
+
+    private static String properties2Base64(final Properties p) throws IOException {
+    	if (p == null) {
+    		return ""; //$NON-NLS-1$
+    	}
+    	final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    	p.store(baos, "PAdES"); //$NON-NLS-1$
+    	return Base64.encode(baos.toByteArray());
+    }
+
+    private static Properties base642Properties(final String base64) throws IOException {
+    	final Properties p = new Properties();
+    	if (base64 == null) {
+    		return p;
+    	}
+    	p.load(
+			new ByteArrayInputStream(
+				Base64.decode(base64)
+			)
+		);
+    	return p;
+    }
+
+    private void writeObject(final ObjectOutputStream out) throws IOException {
+    	final StringBuilder sb = new StringBuilder()
+    		.append("<signResult>\n") //$NON-NLS-1$
+    		.append(" <extraParams>\n") //$NON-NLS-1$
+    		.append(properties2Base64(getExtraParams())).append('\n')
+    		.append(" </extraParams>\n") //$NON-NLS-1$
+    		.append(" <pdfId>\n") //$NON-NLS-1$
+    		.append(getFileID()).append('\n')
+    		.append(" </pdfIf>") //$NON-NLS-1$
+    		.append(" <sign>\n") //$NON-NLS-1$
+    		.append(Base64.encode(getSign())).append('\n')
+    		.append(" </sign>\n") //$NON-NLS-1$
+    		.append(" <signTime>") //$NON-NLS-1$
+    		.append(Long.toString(getSignTime().getTimeInMillis())).append('\n')
+    		.append(" </signTime>") //$NON-NLS-1$
+    		.append("</signResult>") //$NON-NLS-1$
+		;
+    	out.write(sb.toString().getBytes());
+    }
+
+    @SuppressWarnings({ "static-method", "unused" })
+	private void readObject(final ObjectInputStream in) throws IOException, ClassNotFoundException {
+    	final Document xmlSign;
+    	try {
+    		xmlSign = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(in);
+		}
+    	catch (final SAXException e) {
+			throw new IOException(e);
+		}
+    	catch (final ParserConfigurationException e) {
+    		throw new IOException(e);
+		}
+    	//TODO: Implementar la deserializacion XML
+    	throw new UnsupportedOperationException();
     }
 
 }
