c4b46fa9c35df0f764daf182c470fa164d2d8989
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2014-01-12 20:26:53 +0000

    Se cambian clases deprecadas
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/project/afirma/trunk@4445 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-crypto-cades-multi/src/test/java/es/gob/afirma/signers/multi/cades/TestCosign.java
+++ afirma-crypto-cades-multi/src/test/java/es/gob/afirma/signers/multi/cades/TestCosign.java
@@ -10,9 +10,8 @@ import java.security.KeyStore.PrivateKeyEntry;
 import java.security.MessageDigest;
 import java.util.Properties;
 
-import junit.framework.Assert;
-
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
