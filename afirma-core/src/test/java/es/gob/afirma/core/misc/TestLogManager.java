5e0f4823e51b9a559ff3acec19ee7b4255716f56
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2014-04-10 19:56:25 +0000

    Prueba simple del gestor de registro
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/project/afirma/trunk@4655 dcdbb401-323c-49cf-88a0-803402807c85

---
+++ afirma-core/src/test/java/es/gob/afirma/core/misc/TestLogManager.java
@@ -0,0 +1,26 @@
+package es.gob.afirma.core.misc;
+
+import java.util.logging.Logger;
+
+import org.junit.Test;
+
+import es.gob.afirma.core.LogManager;
+import es.gob.afirma.core.LogManager.App;
+
+/** Prueba del gestor de registro.
+ * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s */
+public final class TestLogManager {
+
+	/** Prueba de la obtenci&oacute;n del registro. */
+	@SuppressWarnings("static-method")
+	@Test
+	public void testLogRetrieve() throws Exception {
+		LogManager.install(App.OTHER);
+		final Logger logger = Logger.getLogger("es.gob.afirma"); //$NON-NLS-1$
+		logger.info("Info"); //$NON-NLS-1$
+		logger.warning("Warning"); //$NON-NLS-1$
+		logger.severe("Severe"); //$NON-NLS-1$
+		System.out.println(LogManager.getLogFile());
+	}
+
+}
