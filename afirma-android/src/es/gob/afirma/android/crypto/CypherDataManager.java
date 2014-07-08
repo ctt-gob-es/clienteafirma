8e7dcb04478ff43fb7d406c0395fd8fdfa5583fe
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2014-07-08 16:07:58 +0000

    Cambio de nombres de clases (por corrección ortográfica)
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/cliente-afirma/trunk@4919 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-android/src/es/gob/afirma/android/crypto/CypherDataManager.java
+++ afirma-android/src/es/gob/afirma/android/crypto/CypherDataManager.java
@@ -1,4 +1,4 @@
-package es.gob.afirma.standalone.crypto;
+package es.gob.afirma.android.crypto;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -8,7 +8,7 @@ import java.util.Arrays;
 import es.gob.afirma.core.misc.Base64;
 
 /** Gestor para el cifrado sim&eacute;trico de datos (para el servidor intermedio). */
-public final class CipherDataManager {
+public final class CypherDataManager {
 
 	/** Car&aacute;cter utilizado para separar el padding agregado a los datos para cifrarlos y los propios datos
 	 * cifrados en base64. */
@@ -44,7 +44,7 @@ public final class CipherDataManager {
 
 	/** Descifra una cadena de datos. Esta cadena viene precedida por el n&uacute;mero de caracteres de padding que
 	 * se agregaron y separado por un punto (.) de la cadena base 64 con los datos cifrados.
-	 * @param dataB64 Cadena de datos con la forma: PADDING.CIPHERDATAB64.
+	 * @param data Cadena de datos con la forma: PADDING.CIPHERDATAB64.
 	 * @param cipherKey Clave de cifrado.
 	 * @return Datos descifrados.
 	 * @throws InvalidKeyException Cuando la clave no es v&aacute;lida.
@@ -59,7 +59,7 @@ public final class CipherDataManager {
 			padding = Integer.parseInt(data.substring(0, dotPos));
 		}
 
-		final byte[] decipheredData = DesCipher.decipher(
+		final byte[] decipheredData = DesCypher.decypher(
 				Base64.decode(data.substring(dotPos + 1).replace('+', '-').replace('/', '_'), true),
 				cipherKey);
 
@@ -76,7 +76,14 @@ public final class CipherDataManager {
 	 * @throws GeneralSecurityException Cuando falla el proceso de cifrado.
 	 * @throws IOException */
 	public static String cipherData(final byte[] data, final byte[] cipherKey) throws InvalidKeyException, GeneralSecurityException, IOException {
-		return Integer.toString((DesCipher.getPaddingLength() - data.length % DesCipher.getPaddingLength()) % DesCipher.getPaddingLength()) +
-				PADDING_CHAR_SEPARATOR + Base64.encode(DesCipher.cipher(data, cipherKey), true);
+		return
+			Integer.toString(
+				(DesCypher.getBlockSize() - data.length % DesCypher.getBlockSize()) % DesCypher.getBlockSize()
+			) +
+			PADDING_CHAR_SEPARATOR +
+			Base64.encode(
+				DesCypher.cypher(data, cipherKey),
+				true
+			);
 	}
 }
