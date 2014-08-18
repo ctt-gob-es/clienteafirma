f7ac6a6f6fa67fae3d1d3ff800e63215a9b384ba
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2014-08-18 16:42:01 +0000

    Se incorpora el WSDL al propio proyecto.
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/cliente-afirma/trunk@4997 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-signfolder-proxy/src/main/java/es/gob/afirma/signfolder/client/ObjectFactory.java
+++ afirma-signfolder-proxy/src/main/java/es/gob/afirma/signfolder/client/ObjectFactory.java
@@ -27,11 +27,6 @@ import javax.xml.namespace.QName;
 @XmlRegistry
 public class ObjectFactory {
 
-    private final static QName _MobileDocumentSignatureType_QNAME = new QName("", "signatureType");
-    private final static QName _MobileDocumentData_QNAME = new QName("", "data");
-    private final static QName _MobileDocumentSignAlgorithm_QNAME = new QName("", "signAlgorithm");
-    private final static QName _MobileDocumentSize_QNAME = new QName("", "size");
-    private final static QName _MobileDocumentSignatureParameters_QNAME = new QName("", "signatureParameters");
     private final static QName _MobileRequestImportanceLevel_QNAME = new QName("", "importanceLevel");
     private final static QName _MobileRequestRef_QNAME = new QName("", "ref");
     private final static QName _MobileRequestApplication_QNAME = new QName("", "application");
@@ -43,76 +38,17 @@ public class ObjectFactory {
     private final static QName _MobileRequestIdentifier_QNAME = new QName("", "identifier");
     private final static QName _MobileSignLineMobileSignerList_QNAME = new QName("", "mobileSignerList");
     private final static QName _MobileSignLineType_QNAME = new QName("", "type");
+    private final static QName _MobileDocumentSignatureType_QNAME = new QName("", "signatureType");
+    private final static QName _MobileDocumentData_QNAME = new QName("", "data");
+    private final static QName _MobileDocumentSignAlgorithm_QNAME = new QName("", "signAlgorithm");
+    private final static QName _MobileDocumentSize_QNAME = new QName("", "size");
+    private final static QName _MobileDocumentSignatureParameters_QNAME = new QName("", "signatureParameters");
 
     /**
      * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: es.gob.afirma.signfolder.client
      * 
      */
     public ObjectFactory() {
-    }
-
-    /**
-     * Create an instance of {@link ReportPreviewResponse }
-     * 
-     */
-    public ReportPreviewResponse createReportPreviewResponse() {
-        return new ReportPreviewResponse();
-    }
-
-    /**
-     * Create an instance of {@link ApproveRequest }
-     * 
-     */
-    public ApproveRequest createApproveRequest() {
-        return new ApproveRequest();
-    }
-
-    /**
-     * Create an instance of {@link MobileDocument }
-     * 
-     */
-    public MobileDocument createMobileDocument() {
-        return new MobileDocument();
-    }
-
-    /**
-     * Create an instance of {@link MobileStringList }
-     * 
-     */
-    public MobileStringList createMobileStringList() {
-        return new MobileStringList();
-    }
-
-    /**
-     * Create an instance of {@link MobileDocSignInfoList }
-     * 
-     */
-    public MobileDocSignInfoList createMobileDocSignInfoList() {
-        return new MobileDocSignInfoList();
-    }
-
-    /**
-     * Create an instance of {@link GetDocumentsToSignResponse }
-     * 
-     */
-    public GetDocumentsToSignResponse createGetDocumentsToSignResponse() {
-        return new GetDocumentsToSignResponse();
-    }
-
-    /**
-     * Create an instance of {@link MobileRequestList }
-     * 
-     */
-    public MobileRequestList createMobileRequestList() {
-        return new MobileRequestList();
-    }
-
-    /**
-     * Create an instance of {@link RejectRequestResponse }
-     * 
-     */
-    public RejectRequestResponse createRejectRequestResponse() {
-        return new RejectRequestResponse();
     }
 
     /**
@@ -124,131 +60,11 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link MobileRequestFilter }
+     * Create an instance of {@link ReportPreview }
      * 
      */
-    public MobileRequestFilter createMobileRequestFilter() {
-        return new MobileRequestFilter();
-    }
-
-    /**
-     * Create an instance of {@link MobileSignLine }
-     * 
-     */
-    public MobileSignLine createMobileSignLine() {
-        return new MobileSignLine();
-    }
-
-    /**
-     * Create an instance of {@link QueryRequestResponse }
-     * 
-     */
-    public QueryRequestResponse createQueryRequestResponse() {
-        return new QueryRequestResponse();
-    }
-
-    /**
-     * Create an instance of {@link MobileApplication }
-     * 
-     */
-    public MobileApplication createMobileApplication() {
-        return new MobileApplication();
-    }
-
-    /**
-     * Create an instance of {@link DocumentPreview }
-     * 
-     */
-    public DocumentPreview createDocumentPreview() {
-        return new DocumentPreview();
-    }
-
-    /**
-     * Create an instance of {@link ApproveRequestResponse }
-     * 
-     */
-    public ApproveRequestResponse createApproveRequestResponse() {
-        return new ApproveRequestResponse();
-    }
-
-    /**
-     * Create an instance of {@link QueryApplicationsMobile }
-     * 
-     */
-    public QueryApplicationsMobile createQueryApplicationsMobile() {
-        return new QueryApplicationsMobile();
-    }
-
-    /**
-     * Create an instance of {@link SaveSignResponse }
-     * 
-     */
-    public SaveSignResponse createSaveSignResponse() {
-        return new SaveSignResponse();
-    }
-
-    /**
-     * Create an instance of {@link GetDocumentsToSign }
-     * 
-     */
-    public GetDocumentsToSign createGetDocumentsToSign() {
-        return new GetDocumentsToSign();
-    }
-
-    /**
-     * Create an instance of {@link SaveSign }
-     * 
-     */
-    public SaveSign createSaveSign() {
-        return new SaveSign();
-    }
-
-    /**
-     * Create an instance of {@link MobileDocumentList }
-     * 
-     */
-    public MobileDocumentList createMobileDocumentList() {
-        return new MobileDocumentList();
-    }
-
-    /**
-     * Create an instance of {@link SignPreview }
-     * 
-     */
-    public SignPreview createSignPreview() {
-        return new SignPreview();
-    }
-
-    /**
-     * Create an instance of {@link MobileRequest }
-     * 
-     */
-    public MobileRequest createMobileRequest() {
-        return new MobileRequest();
-    }
-
-    /**
-     * Create an instance of {@link MobileDocSignInfo }
-     * 
-     */
-    public MobileDocSignInfo createMobileDocSignInfo() {
-        return new MobileDocSignInfo();
-    }
-
-    /**
-     * Create an instance of {@link QueryRequestList }
-     * 
-     */
-    public QueryRequestList createQueryRequestList() {
-        return new QueryRequestList();
-    }
-
-    /**
-     * Create an instance of {@link QueryRequestListResponse }
-     * 
-     */
-    public QueryRequestListResponse createQueryRequestListResponse() {
-        return new QueryRequestListResponse();
+    public ReportPreview createReportPreview() {
+        return new ReportPreview();
     }
 
     /**
@@ -260,51 +76,43 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link DocumentPreviewResponse }
+     * Create an instance of {@link MobileRequestList }
      * 
      */
-    public DocumentPreviewResponse createDocumentPreviewResponse() {
-        return new DocumentPreviewResponse();
+    public MobileRequestList createMobileRequestList() {
+        return new MobileRequestList();
     }
 
     /**
-     * Create an instance of {@link RejectRequest }
+     * Create an instance of {@link SignPreview }
      * 
      */
-    public RejectRequest createRejectRequest() {
-        return new RejectRequest();
+    public SignPreview createSignPreview() {
+        return new SignPreview();
     }
 
     /**
-     * Create an instance of {@link MobileApplicationList }
+     * Create an instance of {@link SaveSign }
      * 
      */
-    public MobileApplicationList createMobileApplicationList() {
-        return new MobileApplicationList();
+    public SaveSign createSaveSign() {
+        return new SaveSign();
     }
 
     /**
-     * Create an instance of {@link SignPreviewResponse }
+     * Create an instance of {@link MobileRequestFilter }
      * 
      */
-    public SignPreviewResponse createSignPreviewResponse() {
-        return new SignPreviewResponse();
+    public MobileRequestFilter createMobileRequestFilter() {
+        return new MobileRequestFilter();
     }
 
     /**
-     * Create an instance of {@link ReportPreview }
+     * Create an instance of {@link DocumentPreview }
      * 
      */
-    public ReportPreview createReportPreview() {
-        return new ReportPreview();
-    }
-
-    /**
-     * Create an instance of {@link MobileSignLineList }
-     * 
-     */
-    public MobileSignLineList createMobileSignLineList() {
-        return new MobileSignLineList();
+    public DocumentPreview createDocumentPreview() {
+        return new DocumentPreview();
     }
 
     /**
@@ -324,49 +132,195 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link MobileSignFormat }{@code >}}
+     * Create an instance of {@link MobileSignLine }
      * 
      */
-    @XmlElementDecl(namespace = "", name = "signatureType", scope = MobileDocument.class)
-    public JAXBElement<MobileSignFormat> createMobileDocumentSignatureType(MobileSignFormat value) {
-        return new JAXBElement<MobileSignFormat>(_MobileDocumentSignatureType_QNAME, MobileSignFormat.class, MobileDocument.class, value);
+    public MobileSignLine createMobileSignLine() {
+        return new MobileSignLine();
     }
 
     /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link DataHandler }{@code >}}
+     * Create an instance of {@link GetDocumentsToSign }
      * 
      */
-    @XmlElementDecl(namespace = "", name = "data", scope = MobileDocument.class)
-    @XmlMimeType("application/octet-stream")
-    public JAXBElement<DataHandler> createMobileDocumentData(DataHandler value) {
-        return new JAXBElement<DataHandler>(_MobileDocumentData_QNAME, DataHandler.class, MobileDocument.class, value);
+    public GetDocumentsToSign createGetDocumentsToSign() {
+        return new GetDocumentsToSign();
     }
 
     /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
+     * Create an instance of {@link ApproveRequest }
      * 
      */
-    @XmlElementDecl(namespace = "", name = "signAlgorithm", scope = MobileDocument.class)
-    public JAXBElement<String> createMobileDocumentSignAlgorithm(String value) {
-        return new JAXBElement<String>(_MobileDocumentSignAlgorithm_QNAME, String.class, MobileDocument.class, value);
+    public ApproveRequest createApproveRequest() {
+        return new ApproveRequest();
     }
 
     /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link Integer }{@code >}}
+     * Create an instance of {@link SaveSignResponse }
      * 
      */
-    @XmlElementDecl(namespace = "", name = "size", scope = MobileDocument.class)
-    public JAXBElement<Integer> createMobileDocumentSize(Integer value) {
-        return new JAXBElement<Integer>(_MobileDocumentSize_QNAME, Integer.class, MobileDocument.class, value);
+    public SaveSignResponse createSaveSignResponse() {
+        return new SaveSignResponse();
     }
 
     /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
+     * Create an instance of {@link ReportPreviewResponse }
      * 
      */
-    @XmlElementDecl(namespace = "", name = "signatureParameters", scope = MobileDocument.class)
-    public JAXBElement<String> createMobileDocumentSignatureParameters(String value) {
-        return new JAXBElement<String>(_MobileDocumentSignatureParameters_QNAME, String.class, MobileDocument.class, value);
+    public ReportPreviewResponse createReportPreviewResponse() {
+        return new ReportPreviewResponse();
+    }
+
+    /**
+     * Create an instance of {@link MobileSignLineList }
+     * 
+     */
+    public MobileSignLineList createMobileSignLineList() {
+        return new MobileSignLineList();
+    }
+
+    /**
+     * Create an instance of {@link MobileApplicationList }
+     * 
+     */
+    public MobileApplicationList createMobileApplicationList() {
+        return new MobileApplicationList();
+    }
+
+    /**
+     * Create an instance of {@link SignPreviewResponse }
+     * 
+     */
+    public SignPreviewResponse createSignPreviewResponse() {
+        return new SignPreviewResponse();
+    }
+
+    /**
+     * Create an instance of {@link MobileRequest }
+     * 
+     */
+    public MobileRequest createMobileRequest() {
+        return new MobileRequest();
+    }
+
+    /**
+     * Create an instance of {@link MobileDocument }
+     * 
+     */
+    public MobileDocument createMobileDocument() {
+        return new MobileDocument();
+    }
+
+    /**
+     * Create an instance of {@link RejectRequestResponse }
+     * 
+     */
+    public RejectRequestResponse createRejectRequestResponse() {
+        return new RejectRequestResponse();
+    }
+
+    /**
+     * Create an instance of {@link MobileStringList }
+     * 
+     */
+    public MobileStringList createMobileStringList() {
+        return new MobileStringList();
+    }
+
+    /**
+     * Create an instance of {@link MobileApplication }
+     * 
+     */
+    public MobileApplication createMobileApplication() {
+        return new MobileApplication();
+    }
+
+    /**
+     * Create an instance of {@link QueryRequestList }
+     * 
+     */
+    public QueryRequestList createQueryRequestList() {
+        return new QueryRequestList();
+    }
+
+    /**
+     * Create an instance of {@link ApproveRequestResponse }
+     * 
+     */
+    public ApproveRequestResponse createApproveRequestResponse() {
+        return new ApproveRequestResponse();
+    }
+
+    /**
+     * Create an instance of {@link GetDocumentsToSignResponse }
+     * 
+     */
+    public GetDocumentsToSignResponse createGetDocumentsToSignResponse() {
+        return new GetDocumentsToSignResponse();
+    }
+
+    /**
+     * Create an instance of {@link QueryRequestResponse }
+     * 
+     */
+    public QueryRequestResponse createQueryRequestResponse() {
+        return new QueryRequestResponse();
+    }
+
+    /**
+     * Create an instance of {@link QueryRequestListResponse }
+     * 
+     */
+    public QueryRequestListResponse createQueryRequestListResponse() {
+        return new QueryRequestListResponse();
+    }
+
+    /**
+     * Create an instance of {@link MobileDocumentList }
+     * 
+     */
+    public MobileDocumentList createMobileDocumentList() {
+        return new MobileDocumentList();
+    }
+
+    /**
+     * Create an instance of {@link MobileDocSignInfo }
+     * 
+     */
+    public MobileDocSignInfo createMobileDocSignInfo() {
+        return new MobileDocSignInfo();
+    }
+
+    /**
+     * Create an instance of {@link DocumentPreviewResponse }
+     * 
+     */
+    public DocumentPreviewResponse createDocumentPreviewResponse() {
+        return new DocumentPreviewResponse();
+    }
+
+    /**
+     * Create an instance of {@link MobileDocSignInfoList }
+     * 
+     */
+    public MobileDocSignInfoList createMobileDocSignInfoList() {
+        return new MobileDocSignInfoList();
+    }
+
+    /**
+     * Create an instance of {@link RejectRequest }
+     * 
+     */
+    public RejectRequest createRejectRequest() {
+        return new RejectRequest();
+    }
+
+    /**
+     * Create an instance of {@link QueryApplicationsMobile }
+     * 
+     */
+    public QueryApplicationsMobile createQueryApplicationsMobile() {
+        return new QueryApplicationsMobile();
     }
 
     /**
@@ -466,6 +420,52 @@ public class ObjectFactory {
     @XmlElementDecl(namespace = "", name = "type", scope = MobileSignLine.class)
     public JAXBElement<String> createMobileSignLineType(String value) {
         return new JAXBElement<String>(_MobileSignLineType_QNAME, String.class, MobileSignLine.class, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link MobileSignFormat }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "", name = "signatureType", scope = MobileDocument.class)
+    public JAXBElement<MobileSignFormat> createMobileDocumentSignatureType(MobileSignFormat value) {
+        return new JAXBElement<MobileSignFormat>(_MobileDocumentSignatureType_QNAME, MobileSignFormat.class, MobileDocument.class, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link DataHandler }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "", name = "data", scope = MobileDocument.class)
+    @XmlMimeType("application/octet-stream")
+    public JAXBElement<DataHandler> createMobileDocumentData(DataHandler value) {
+        return new JAXBElement<DataHandler>(_MobileDocumentData_QNAME, DataHandler.class, MobileDocument.class, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "", name = "signAlgorithm", scope = MobileDocument.class)
+    public JAXBElement<String> createMobileDocumentSignAlgorithm(String value) {
+        return new JAXBElement<String>(_MobileDocumentSignAlgorithm_QNAME, String.class, MobileDocument.class, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Integer }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "", name = "size", scope = MobileDocument.class)
+    public JAXBElement<Integer> createMobileDocumentSize(Integer value) {
+        return new JAXBElement<Integer>(_MobileDocumentSize_QNAME, Integer.class, MobileDocument.class, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "", name = "signatureParameters", scope = MobileDocument.class)
+    public JAXBElement<String> createMobileDocumentSignatureParameters(String value) {
+        return new JAXBElement<String>(_MobileDocumentSignatureParameters_QNAME, String.class, MobileDocument.class, value);
     }
 
 }
