ffd7504f32b97a19ac87bd6639106283b8107589
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2014-08-27 07:39:26 +0000

    Se mueve el tests de cofirma CAdES de una firma CMS al proyecto de multifirmas CAdES.
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/cliente-afirma/trunk@5035 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-crypto-cades-multi/src/test/java/es/gob/afirma/signers/multi/cades/TestCompatibilidad.java
+++ afirma-crypto-cades-multi/src/test/java/es/gob/afirma/signers/multi/cades/TestCompatibilidad.java
@@ -1,4 +1,4 @@
-package es.gob.afirma.test.cades;
+package es.gob.afirma.signers.multi.cades;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,7 +14,7 @@ import es.gob.afirma.signers.cades.AOCAdESSigner;
 
 public class TestCompatibilidad {
 
-	private static final String CMS_SIGN_FILENAME = "cms_implicit.csig";
+	private static final String CMS_SIGN_FILENAME = "cms_implicit.csig"; //$NON-NLS-1$
 	
 	private static final String CERT_PATH = "ANF_PF_Activo.pfx"; //$NON-NLS-1$
 	private static final String CERT_PASS = "12341234"; //$NON-NLS-1$
@@ -24,17 +24,17 @@ public class TestCompatibilidad {
 	@Test
 	public void testCofirmaCadesSobreCms() throws Exception {
 		
-		byte[] signature = loadFile(CMS_SIGN_FILENAME);
+		final byte[] cmsSignature = loadFile(CMS_SIGN_FILENAME);
 			
 		final KeyStore ks = KeyStore.getInstance("PKCS12"); //$NON-NLS-1$
 		ks.load(ClassLoader.getSystemResourceAsStream(CERT_PATH), CERT_PASS.toCharArray());
 		final PrivateKeyEntry pke = (PrivateKeyEntry) ks.getEntry(CERT_ALIAS, new KeyStore.PasswordProtection(CERT_PASS.toCharArray()));
 		
-		Properties extraParams = new Properties();
-		extraParams.setProperty("mode", AOSignConstants.SIGN_MODE_IMPLICIT);
+		final Properties extraParams = new Properties();
+		extraParams.setProperty("mode", AOSignConstants.SIGN_MODE_IMPLICIT); //$NON-NLS-1$
 		
 		new AOCAdESSigner().cosign(
-				signature,
+				cmsSignature,
 				AOSignConstants.SIGN_ALGORITHM_SHA1WITHRSA,
 				pke.getPrivateKey(),
 				pke.getCertificateChain(),
@@ -43,7 +43,7 @@ public class TestCompatibilidad {
 	
 	private static byte[] loadFile(String filename) throws IOException {
 		
-		final InputStream is = TestCompatibilidad.class.getClassLoader().getResourceAsStream(CMS_SIGN_FILENAME);
+		final InputStream is = TestCompatibilidad.class.getClassLoader().getResourceAsStream(filename);
 		byte[] data = AOUtil.getDataFromInputStream(is);
 		is.close();
 		
