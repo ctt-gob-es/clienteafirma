d372f0c87dcb1fd61f6988a6780058ce62f4e3a1
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2014-08-30 19:29:53 +0000

    Mejora del JavaDoc
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/cliente-afirma/trunk@5046 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-crypto-cades-multi/src/test/java/es/gob/afirma/signers/multi/cades/TestCompatibilidad.java
+++ afirma-crypto-cades-multi/src/test/java/es/gob/afirma/signers/multi/cades/TestCompatibilidad.java
@@ -12,27 +12,31 @@ import es.gob.afirma.core.misc.AOUtil;
 import es.gob.afirma.core.signers.AOSignConstants;
 import es.gob.afirma.signers.cades.AOCAdESSigner;
 
+/** Pruebas de compatibilidad CAdES-CMS. */
 public class TestCompatibilidad {
 
 	private static final String CMS_SIGN_FILENAME = "cms_implicit.csig"; //$NON-NLS-1$
-	
+
 	private static final String CERT_PATH = "ANF_PF_Activo.pfx"; //$NON-NLS-1$
 	private static final String CERT_PASS = "12341234"; //$NON-NLS-1$
 	private static final String CERT_ALIAS = "anf usuario activo"; //$NON-NLS-1$
 
-	
+
+	/** Prueba de cofirma CAdES sobre firma CMS.
+	 * @throws Exception en caso de cualquier error. */
+	@SuppressWarnings("static-method")
 	@Test
 	public void testCofirmaCadesSobreCms() throws Exception {
-		
+
 		final byte[] cmsSignature = loadFile(CMS_SIGN_FILENAME);
-			
+
 		final KeyStore ks = KeyStore.getInstance("PKCS12"); //$NON-NLS-1$
 		ks.load(ClassLoader.getSystemResourceAsStream(CERT_PATH), CERT_PASS.toCharArray());
 		final PrivateKeyEntry pke = (PrivateKeyEntry) ks.getEntry(CERT_ALIAS, new KeyStore.PasswordProtection(CERT_PASS.toCharArray()));
-		
+
 		final Properties extraParams = new Properties();
 		extraParams.setProperty("mode", AOSignConstants.SIGN_MODE_IMPLICIT); //$NON-NLS-1$
-		
+
 		new AOCAdESSigner().cosign(
 				cmsSignature,
 				AOSignConstants.SIGN_ALGORITHM_SHA1WITHRSA,
@@ -40,13 +44,13 @@ public class TestCompatibilidad {
 				pke.getCertificateChain(),
 				extraParams);
 	}
-	
-	private static byte[] loadFile(String filename) throws IOException {
-		
+
+	private static byte[] loadFile(final String filename) throws IOException {
+
 		final InputStream is = TestCompatibilidad.class.getClassLoader().getResourceAsStream(filename);
-		byte[] data = AOUtil.getDataFromInputStream(is);
+		final byte[] data = AOUtil.getDataFromInputStream(is);
 		is.close();
-		
+
 		return data;
 	}
 }
