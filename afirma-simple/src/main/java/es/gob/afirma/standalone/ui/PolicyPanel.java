4bd2e3627dd250b541d6766a8e341a893bcd35ec
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2014-09-02 21:16:25 +0000

    Mejora del JavaDoc
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/cliente-afirma/trunk@5070 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-simple/src/main/java/es/gob/afirma/standalone/ui/PolicyPanel.java
+++ afirma-simple/src/main/java/es/gob/afirma/standalone/ui/PolicyPanel.java
@@ -35,19 +35,19 @@ public class PolicyPanel extends JPanel implements ItemListener {
 		"SHA-384", //$NON-NLS-1$
 		"SHA-256" //$NON-NLS-1$
 	};
-	
+
 	private JComboBox policiesCombo;
 
 	private JTextField identifierField;
-	
+
 	private JTextField hashField;
-	
+
 	private JComboBox hashAlgorithmField;
-	
+
 	private JTextField qualifierField;
 
 	private final List<PolicyItem> policies;
-	
+
 	private AdESPolicy currentPolicy;
 
 	/**
@@ -56,25 +56,25 @@ public class PolicyPanel extends JPanel implements ItemListener {
 	 * @param policies Listado de pol&iacute;ticas prefijadas.
 	 * @param currentPolicy Pol&iacute;tica actualmente configurada.
 	 */
-	public PolicyPanel(final String signFormat, final List<PolicyItem> policies, AdESPolicy currentPolicy) {
+	public PolicyPanel(final String signFormat, final List<PolicyItem> policies, final AdESPolicy currentPolicy) {
 
 		// La politica actual sera la personalizada siempre que no sea una de las prefijadas
 		AdESPolicy customPolicy = currentPolicy;
 		if (currentPolicy != null) {
-			for (PolicyItem item : policies) {
+			for (final PolicyItem item : policies) {
 				if (item.equals(customPolicy)) {
 					customPolicy = null;
 				}
 			}
 		}
-		
+
 		// Las politicas seran:
 		// 0: Ninguna politica
 		// De 1 a N: Politicas prefijadas
 		// Ultima: Politica personalizada
 		this.policies = new ArrayList<PolicyPanel.PolicyItem>();
 		this.policies.add(new PolicyItem(SimpleAfirmaMessages.getString("PreferencesPanel.24"), null)); //$NON-NLS-1$
-		for (PolicyItem item : policies) {
+		for (final PolicyItem item : policies) {
 			this.policies.add(item);
 		}
 		this.policies.add(new PolicyItem(SimpleAfirmaMessages.getString("PreferencesPanel.26"), customPolicy)); //$NON-NLS-1$
@@ -100,7 +100,7 @@ public class PolicyPanel extends JPanel implements ItemListener {
 		this.policiesCombo.setModel(new DefaultComboBoxModel(
 				this.policies.toArray(new PolicyItem[this.policies.size()])
 				));
-		
+
 		add(this.policiesCombo, c);
 		this.policiesCombo.getAccessibleContext().setAccessibleDescription(SimpleAfirmaMessages.getString("PreferencesPanel.47")); //$NON-NLS-1$
 		this.policiesCombo.addItemListener(this);
@@ -162,40 +162,38 @@ public class PolicyPanel extends JPanel implements ItemListener {
 	}
 
 	/**
-	 * Obtiene el &iacute;ndice de la politica personalizada en el listado de pol&iacute;ticas. 
+	 * Obtiene el &iacute;ndice de la politica personalizada en el listado de pol&iacute;ticas.
 	 * @return &Iacute;ndice.
 	 */
 	private int getCustomPolicyIndex() {
 		return this.policiesCombo.getItemCount() - 1;
 	}
-	
+
 	/**
 	 * Establece el listener que atiende a los cambios realizados en el panel de
 	 * configuraci&oacute;n de pol&iacute;ticas.
 	 * @param modListener Listener al que se notifican los eventos de cambio.
 	 */
 	public void setModificationListener(final ModificationListener modListener) {
-		
+
 		this.policiesCombo.addItemListener(modListener);
 		this.identifierField.addKeyListener(modListener);
 		this.hashField.addKeyListener(modListener);
 		this.hashAlgorithmField.addItemListener(modListener);
 		this.qualifierField.addKeyListener(modListener);
 	}
-	
-	/**
-	 * Establece el listener que atiende a los cambios realizados en el panel de
+
+	/** Establece el listener que atiende a los cambios realizados en el panel de
 	 * configuraci&oacute;n de pol&iacute;ticas.
-	 * @param modListener Listener al que se notifican los eventos de cambio.
-	 */
+	 * @param keyListener Listener al que se notifican los eventos de cambio. */
 	public void setKeyListener(final KeyListener keyListener) {
-		
+
 		this.identifierField.addKeyListener(keyListener);
 		this.hashField.addKeyListener(keyListener);
 		this.hashAlgorithmField.addKeyListener(keyListener);
 		this.qualifierField.addKeyListener(keyListener);
 	}
-	
+
 	/** {@inheritDoc} */
 	@Override
 	public void itemStateChanged(final ItemEvent ie) {
@@ -203,7 +201,7 @@ public class PolicyPanel extends JPanel implements ItemListener {
 		if (ie.getStateChange() == ItemEvent.DESELECTED){
 			return;
 		}
-		
+
 		// Cambiamos las selecciones necesarias
 		final boolean enabled = this.policiesCombo.getSelectedIndex() > POLICY_INDEX_NONE;
 		final boolean editable = this.policiesCombo.getSelectedIndex() == getCustomPolicyIndex();
@@ -218,11 +216,11 @@ public class PolicyPanel extends JPanel implements ItemListener {
 
 		// El combo de algoritmos se podra seleccionar cuando este activado
 		this.hashAlgorithmField.setEnabled(editable);
-		
+
 		// Mostramos la configuracion de politica que corresponde
 		loadPolicy(((PolicyItem) this.policiesCombo.getSelectedItem()).getPolicy());
 	}
-	
+
 	/**
 	 * Carga los datos de una politica en el panel.
 	 * @param policy Politica a cargar.
@@ -240,16 +238,16 @@ public class PolicyPanel extends JPanel implements ItemListener {
 			this.qualifierField.setText(""); //$NON-NLS-1$
 		}
 	}
-	
+
 	/**
 	 * Politica de firma definida por los valores que deben establecerse para la misma y un nombre
 	 * que la define de cara al usuario.
 	 */
 	public static class PolicyItem {
-		
+
 		private final String name;
 		private AdESPolicy policy;
-		
+
 		/**
 		 * Construye el elemento con nombre y configuraci&oacute;n de pol&iacute;tica.
 		 * @param name Nombre de la configuraci&oacute;n.
@@ -259,7 +257,7 @@ public class PolicyPanel extends JPanel implements ItemListener {
 			this.name = name;
 			this.policy = policy;
 		}
-		
+
 		/**
 		 * Recupera la configuraci&oacute;n de pol&iacute;tica.
 		 * @return Configuraci&oacute;n.
@@ -267,7 +265,7 @@ public class PolicyPanel extends JPanel implements ItemListener {
 		public AdESPolicy getPolicy() {
 			return this.policy;
 		}
-		
+
 		/**
 		 * Establece la configuraci&oacute;n de pol&iacute;tica.
 		 * @param policy Configuraci&oacute;n.
@@ -275,10 +273,10 @@ public class PolicyPanel extends JPanel implements ItemListener {
 		public void setPolicy(final AdESPolicy policy) {
 			this.policy = policy;
 		}
-		
+
 		@Override
-		public boolean equals(Object obj) {
-			
+		public boolean equals(final Object obj) {
+
 			if (obj instanceof AdESPolicy) {
 				return this.policy != null && this.policy.equals(obj);
 			}
@@ -290,7 +288,7 @@ public class PolicyPanel extends JPanel implements ItemListener {
 			}
 			return false;
 		}
-		
+
 		@Override
 		public String toString() {
 			return this.name;
@@ -301,12 +299,12 @@ public class PolicyPanel extends JPanel implements ItemListener {
 			return super.hashCode();
 		}
 	}
-	
+
 	/**
-	 * Guarda la configuraci&oacute;n actual dentro el contexto. NO GUARDA EN LAS PREFERENCIAS. 
+	 * Guarda la configuraci&oacute;n actual dentro el contexto. NO GUARDA EN LAS PREFERENCIAS.
 	 */
 	public void saveCurrentPolicy() {
-		
+
 		final AdESPolicy policy = getCurrentPolicy();
 		if (this.policiesCombo.getSelectedIndex() == getCustomPolicyIndex()) {
 			((PolicyItem) this.policiesCombo.getSelectedItem()).setPolicy(policy);
@@ -316,7 +314,7 @@ public class PolicyPanel extends JPanel implements ItemListener {
 		}
 		this.currentPolicy = policy;
 	}
-	
+
 	/**
 	 * Crea un objeto de configuracion con los valores actuales.
 	 * @return Configuraci&oacute;n de pol&iacute;tica actualmente establecida o
@@ -325,11 +323,11 @@ public class PolicyPanel extends JPanel implements ItemListener {
 	 * es v&aacute;lida para una pol&iacute;tica.
 	 */
 	public AdESPolicy getCurrentPolicy() {
-		
+
 		if (this.policiesCombo.getSelectedIndex() <= POLICY_INDEX_NONE) {
 			return null;
 		}
-		
+
 		return new AdESPolicy(
 				this.identifierField.getText(),
 				this.hashField.getText(),
