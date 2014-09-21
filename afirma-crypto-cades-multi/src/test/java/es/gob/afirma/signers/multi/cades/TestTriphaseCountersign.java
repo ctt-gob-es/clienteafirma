a6f592142f76a61fc719e5e0b824369292e7eabb
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2014-09-21 10:46:33 +0000

    Actualización a JUnit 4
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/cliente-afirma/trunk@5152 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-crypto-cades-multi/src/test/java/es/gob/afirma/signers/multi/cades/TestTriphaseCountersign.java
+++ afirma-crypto-cades-multi/src/test/java/es/gob/afirma/signers/multi/cades/TestTriphaseCountersign.java
@@ -8,9 +8,8 @@ import java.security.KeyStore.PrivateKeyEntry;
 import java.util.Date;
 import java.util.Properties;
 
-import junit.framework.Assert;
-
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
