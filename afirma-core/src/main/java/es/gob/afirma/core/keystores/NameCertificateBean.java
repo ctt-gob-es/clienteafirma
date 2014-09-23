f785d80c807e099ebbb3a498e63d768680272d3f
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2014-09-23 11:25:35 +0000

    Mejora del JavaDoc
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/cliente-afirma/trunk@5171 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-core/src/main/java/es/gob/afirma/core/keystores/NameCertificateBean.java
+++ afirma-core/src/main/java/es/gob/afirma/core/keystores/NameCertificateBean.java
@@ -2,10 +2,8 @@ package es.gob.afirma.core.keystores;
 
 import java.security.cert.X509Certificate;
 
-/**
- * Tupla Nombre-Certificado con la que mantener la asociaci&oacute;n entre un nombre
- * identificador y su certificado asociado.
- */
+/** Tupla Nombre-Certificado con la que mantener la asociaci&oacute;n entre un nombre
+ * identificador y su certificado asociado. */
 public class NameCertificateBean {
 
 	private final String alias;
@@ -14,46 +12,36 @@ public class NameCertificateBean {
 
 	private final X509Certificate[] certificateChain;
 
-	/**
-	 * Construye la tupla Nombre-Certificado.
+	/** Construye la tupla Nombre-Certificado.
 	 * @param alias Alias del certificado.
 	 * @param name Nombre identificador del certificado.
-	 * @param cert Certificado.
-	 */
+	 * @param certChain Cadena de certificados del firmante. */
 	public NameCertificateBean(final String alias, final String name, final X509Certificate[] certChain) {
 		this.alias = alias;
 		this.name = name;
 		this.certificateChain = certChain == null ? null : certChain.clone();
 	}
 
-	/**
-	 * Devuelve el alias del certificado.
-	 * @return Alias.
-	 */
+	/** Devuelve el alias del certificado.
+	 * @return Alias. */
 	public String getAlias() {
 		return this.alias;
 	}
 
-	/**
-	 * Devuelve el nombre indentificador del certificado.
-	 * @return Nombre.
-	 */
+	/** Devuelve el nombre indentificador del certificado.
+	 * @return Nombre. */
 	public String getName() {
 		return this.name;
 	}
 
-	/**
-	 * Devuelve el certificado.
-	 * @return Certificado.
-	 */
+	/** Devuelve el certificado.
+	 * @return Certificado. */
 	public X509Certificate getCertificate() {
 		return this.certificateChain[0];
 	}
 
-	/**
-	 * Devuelve la cadena de certificaci&oacute;n del certificado.
-	 * @return Cadena de certificaci&oacute;n.
-	 */
+	/** Devuelve la cadena de certificaci&oacute;n del certificado.
+	 * @return Cadena de certificaci&oacute;n. */
 	public X509Certificate[] getCertificateChain() {
 		return this.certificateChain;
 	}
