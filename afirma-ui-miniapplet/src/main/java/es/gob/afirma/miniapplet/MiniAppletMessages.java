f8a8becc9737a462582dc00a6e64d1594b0f94df
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2014-10-06 08:36:32 +0000

    Mejora de flexibilidad y en los comentarios
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/cliente-afirma/trunk@5233 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-ui-miniapplet/src/main/java/es/gob/afirma/miniapplet/MiniAppletMessages.java
+++ afirma-ui-miniapplet/src/main/java/es/gob/afirma/miniapplet/MiniAppletMessages.java
@@ -53,15 +53,18 @@ final class MiniAppletMessages {
     }
 
     /** Recupera el texto identificado con la clave proporcionada y sustituye las
-     * subcadenas de tipo "%i" por el texto en la posici&oacute;n 'i' del array
-     * proporcionado. Introducir m&aacute;s de 10 cadenas distintas para sustituir
+     * subcadenas de tipo "%i" por el texto correspondiente segun posicion de los
+     * parametros indicados despues de la clave. Las posiciones empiezan a contar
+     * por 0. As&iacute;, el texto "%0" se sustituira por el segundo par&aacute;ametro
+     * de la funci&oacute;n, el texto "%1" por el siguiente,...<br>
+     * Introducir m&aacute;s de 10 cadenas distintas para sustituir
      * conllevar&aacute; errores en el resultado.
      * @param key
      *        Clave del texto.
      * @param params
      *        Par&aacute;metros que se desean insertar.
      * @return Recuerso textual con las subcadenas sustituidas. */
-    static String getString(final String key, final String[] params) {
+    static String getString(final String key, final String... params) {
 
         String text;
         try {
@@ -71,7 +74,7 @@ final class MiniAppletMessages {
             return '!' + key + '!';
         }
 
-        if (params != null) {
+        if (params != null && params.length > 0) {
             for (int i = 0; i < params.length; i++) {
             	if (params[i] != null) {
             		text = text.replace("%" + i, params[i]); //$NON-NLS-1$
