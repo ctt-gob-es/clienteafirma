5b4dfda12af3edef00a1b4b252765e3412389490
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2014-10-17 07:13:02 +0000

    Nuevas clase de gestión de KeyUsage
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/cliente-afirma/trunk@5278 dcdbb401-323c-49cf-88a0-803402807c85

---
+++ afirma-core/src/main/java/es/gob/afirma/core/keystores/KeyUsage.java
@@ -0,0 +1,209 @@
+package es.gob.afirma.core.keystores;
+
+import java.security.cert.X509Certificate;
+
+/** Identificador de uso de certificados correspondiente a la subestructura
+ * X.509 ASN.1:
+ * <pre>
+ *  KeyUsage ::= BIT STRING {
+ *   digitalSignature        (0),
+ *   nonRepudiation          (1),
+ *   keyEncipherment         (2),
+ *   dataEncipherment        (3),
+ *   keyAgreement            (4),
+ *   keyCertSign             (5),
+ *   cRLSign                 (6),
+ *   encipherOnly            (7),
+ *   decipherOnly            (8)
+ *  }
+ * </pre>
+ * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s. */
+public final class KeyUsage {
+
+	private static final int KEYUSAGE_NBITS = 9;
+
+	private final Boolean[] usage;
+
+	/** Uso para firma electr&oacute;nica. */
+	public static final KeyUsage SIGN = new KeyUsage(
+		new Boolean[] {
+            null, // digitalSignature
+            Boolean.TRUE, // nonRepudiation
+            null, // keyEncipherment
+            null, // dataEncipherment
+            null, // keyAgreement
+            null, // keyCertSign
+            null, // cRLSign
+            null, // encipherOnly
+            null  // decipherOnly
+		}
+	);
+
+	/** Uso para autenticaci&oacute;n. */
+	public static final KeyUsage AUTH = new KeyUsage(
+		new Boolean[] {
+			Boolean.TRUE, // digitalSignature
+            null, // nonRepudiation
+            null, // keyEncipherment
+            null, // dataEncipherment
+            null, // keyAgreement
+            null, // keyCertSign
+            null, // cRLSign
+            null, // encipherOnly
+            null  // decipherOnly
+		}
+	);
+
+	/** Uso para autenticaci&oacute;n. */
+	public static final KeyUsage CYPH = new KeyUsage(
+		new Boolean[] {
+			null, // digitalSignature
+            null, // nonRepudiation
+            null, // keyEncipherment
+            Boolean.TRUE, // dataEncipherment
+            null, // keyAgreement
+            null, // keyCertSign
+            null, // cRLSign
+            null, // encipherOnly
+            null  // decipherOnly
+		}
+	);
+
+	@Override
+	public boolean equals(final Object e) {
+		if (!(e instanceof KeyUsage)) {
+			return false;
+		}
+		final KeyUsage ku = (KeyUsage) e;
+		final Boolean[] other = ku.getUsage();
+		final Boolean[] me = getUsage();
+		if (other.length != me.length) {
+			return false;
+		}
+		for (int i=0;i<me.length;i++) {
+			if (other[i] != me[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/** Indica si el objeto actual permite tambi&eacute;n los usos del objeto proporcionado.
+	 * @param ku Uso de certificado a comprobar si tambi&eacute;n est&aacute;n soportados por esta instancia.
+	 * @return <code>true</code> si el objeto actual permite los usos del objeto proporcionado,
+	 *         <code>false</code> en caso contrario. */
+	public boolean includes(final KeyUsage ku) {
+		if (ku == null) {
+			return false;
+		}
+		final Boolean[] other = ku.getUsage();
+		final Boolean[] me = getUsage();
+		if (other.length != me.length) {
+			return false;
+		}
+		for (int i=0;i<me.length;i++) {
+			if (other[i] != null && other[i] != me[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int ret = 0;
+		for (final Boolean b : this.usage) {
+			ret = ret + b.hashCode();
+		}
+		return ret;
+	}
+
+    /** Construye una identificador de uso de certificados.
+     * @param keyUsage M&aacute;scara de bits para filtro por <i>KeyUsage</i><br>
+     *        Cada certificado puede permitir simult&aacute;neamente cualquiera de
+     *        estos 8 usos:<br>
+     *        <ol>
+     *        <li><b>digitalSignature</b></li>
+     *        <li><b>nonRepudiation</b></li>
+     *        <li><b>keyEncipherment</b></li>
+     *        <li><b>dataEncipherment</b></li>
+     *        <li><b>keyAgreement</b></li>
+     *        <li><b>keyCertSign</b></li>
+     *        <li><b>cRLSign</b></li>
+     *        <li><b>encipherOnly</b></li>
+     *        <li><b>decipherOnly</b></li>
+     *        </ol>
+     *        Cada uno de los elementos del array designan en orden a uno de estos 8
+     *        usos. El valor de cada elemento puede ser:
+     *        <ul>
+     *        <li>{@code null}: No se comprueba el valor de este uso.</li>
+     *        <li>{@code false}: El certificado no debe tener permitido ese uso.</li>
+     *        <li>{@code true}: El certificado debe tener permitido ese uso.</li>
+     *        </ul> */
+	public KeyUsage(final Boolean[] keyUsage) {
+		if (keyUsage == null || keyUsage.length != KEYUSAGE_NBITS) {
+			throw new IllegalArgumentException(
+				"El uso debe proporcionarse como un Boolean[] no nulo de exactamente 9 posiciones" //$NON-NLS-1$
+			);
+		}
+		this.usage = keyUsage.clone();
+	}
+
+	/** Construye una identificador de uso de certificados a partir de un certificado X.509.
+	 * @param cert Certificado de origen. */
+	public KeyUsage(final X509Certificate cert) {
+		if (cert == null) {
+			throw new IllegalArgumentException(
+				"El certificado de origen no puede ser nulo" //$NON-NLS-1$
+			);
+		}
+		final boolean[] ke = cert.getKeyUsage();
+		if (ke.length != KEYUSAGE_NBITS) {
+			throw new IllegalArgumentException(
+				"El certificado de origen tiene un KeyUsage con un numero de posiciones no soportado: " + ke.length //$NON-NLS-1$
+			);
+		}
+		this.usage = new Boolean[KEYUSAGE_NBITS];
+		for (int i=0; i<KEYUSAGE_NBITS; i++) {
+			this.usage[i] = Boolean.valueOf(ke[i]);
+		}
+	}
+
+	Boolean[] getUsage() {
+		return this.usage.clone();
+	}
+
+	@Override
+	public String toString() {
+		if (includes(SIGN) && includes(AUTH) && includes(CYPH)) {
+			return "Firma, autenticaci\u00F3n y cifrado"; //$NON-NLS-1$
+		}
+		if (includes(SIGN) && includes(AUTH)) {
+			return "Firma y autenticaci\u00F3n"; //$NON-NLS-1$
+		}
+		if (includes(AUTH)) {
+			return "Autenticaci\u00F3n"; //$NON-NLS-1$
+		}
+		if (includes(SIGN)) {
+			return "Firma"; //$NON-NLS-1$
+		}
+		if (includes(CYPH)) {
+			return "Cifrado"; //$NON-NLS-1$
+		}
+		final StringBuilder sb = new StringBuilder("{"); //$NON-NLS-1$
+		for (final Boolean b : this.usage) {
+			if (b == null) {
+				sb.append("-,"); //$NON-NLS-1$
+			}
+			else if (b.booleanValue()) {
+				sb.append("1,"); //$NON-NLS-1$
+			}
+			else {
+				sb.append("0,"); //$NON-NLS-1$
+			}
+		}
+		sb.append("}"); //$NON-NLS-1$
+		return sb.toString().replace(",}", "}"); //$NON-NLS-1$ //$NON-NLS-2$
+	}
+
+}
---
+++ afirma-core/src/test/java/es/gob/afirma/core/misc/TestKeyUsage.java
@@ -0,0 +1,36 @@
+package es.gob.afirma.core.misc;
+
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import org.junit.Test;
+
+import es.gob.afirma.core.keystores.KeyUsage;
+
+/** Pruebas de <i>KeyUsage</i> de certificados.
+ * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s */
+public final class TestKeyUsage {
+
+	private static final String[] TEST_FILES = new String[] {
+		"DNIE01.cer", //$NON-NLS-1$
+		"DNIE02.cer", //$NON-NLS-1$
+		"MDEF01.cer", //$NON-NLS-1$
+		"MDEF02.cer", //$NON-NLS-1$
+		"MDEF03.cer", //$NON-NLS-1$
+		"CERES.cer" //$NON-NLS-1$
+	};
+
+	/** Pruebas de impresi&oacute;n en consola del <i>KeyUsage</i>.
+	 * @throws Exception en cualquier error. */
+	@SuppressWarnings("static-method")
+	@Test
+	public void testCertKeyUsage() throws Exception {
+		final CertificateFactory cf = CertificateFactory.getInstance("X.509"); //$NON-NLS-1$
+		for (final String certName : TEST_FILES) {
+			final X509Certificate c = (X509Certificate) cf.generateCertificate(ClassLoader.getSystemResourceAsStream(certName));
+			final KeyUsage ku = new KeyUsage(c);
+			System.out.println(ku.toString());
+		}
+	}
+
+}
