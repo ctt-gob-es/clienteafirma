6d94e8b5d42d08d380ec6c486d10b4d8396265be
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2014-11-25 15:59:36 +0000

    Nuevos parámetros en el XML de tarea de firma manuscrita
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/cliente-afirma/trunk@5353 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-crypto-handwritten/src/test/java/es/gob/afirma/crypto/handwritten/TestSignTask.java
+++ afirma-crypto-handwritten/src/test/java/es/gob/afirma/crypto/handwritten/TestSignTask.java
@@ -1,22 +1,9 @@
 package es.gob.afirma.crypto.handwritten;
 
-import java.net.URI;
-import java.net.URL;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Marshaller;
-
 import org.junit.Test;
 
 import es.gob.afirma.core.misc.AOUtil;
 import es.gob.afirma.core.misc.Base64;
-import es.gob.afirma.crypto.handwritten.SerializableTsaParams.SerializableTsaRequestExtension;
 
 /** Pruebas de tareas de firma.
  * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s */
@@ -44,60 +31,6 @@ public class TestSignTask {
 		System.out.println(
 			SignTask.getInstance(xml)
 		);
-	}
-
-	/** Prueba una serializaci&oacute;n.
-	 * @throws Exception En cualquier error. */
-	@SuppressWarnings("static-method")
-	@Test
-	public void testSerial() throws Exception {
-		final SerializableTsaParams tsaParams = new SerializableTsaParams(
-				true,
-				"4.3.2.1", //$NON-NLS-1$
-				new URI("http://kaka.ka"), //$NON-NLS-1$
-				"user", //$NON-NLS-1$
-				"password", //$NON-NLS-1$
-				new SerializableTsaRequestExtension[] { new SerializableTsaRequestExtension("1.2.3.4", false, new byte[] { (byte) 0xff, (byte) 0xfa }) }, //$NON-NLS-1$
-				"SHA-512", //$NON-NLS-1$
-				new byte[] { 0x00, 0x01, 0x02, 0x03 },
-				"p12password" //$NON-NLS-1$
-			);
-
-			final SingleBioSignData sbd = new SingleBioSignData(
-				new SignerInfoBean("Astrid", "Idoate", "Gil", "12345678Z"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-				"<html><body><h1>HOLA</h1></body></html>", //$NON-NLS-1$
-				null,
-				new Rectangle(10, 10, 100, 100),
-				new Rectangle(50, 30, 200, 75),
-				1
-			);
-			final List<SingleBioSignData> signs = new ArrayList<SingleBioSignData>(1);
-			signs.add(sbd);
-
-			final Map<String, String> p = new ConcurrentHashMap<String, String>();
-			p.put("clave", "valor"); //$NON-NLS-1$ //$NON-NLS-2$
-
-			final SignTask st = new SignTask(
-				tsaParams,
-				new URL("http://www.google.com/"), //$NON-NLS-1$
-				new URL("http://www.ibm.es"), //$NON-NLS-1$
-				"data", //$NON-NLS-1$
-				Base64.encode(
-					((X509Certificate) CertificateFactory.getInstance("X.509").generateCertificate( //$NON-NLS-1$
-						TestBioSigner.class.getResourceAsStream("/democert.cer") //$NON-NLS-1$
-					)).getEncoded()
-				),
-				signs,
-				true,
-				p
-			);
-
-			final Marshaller m = JAXBContext.newInstance(SignTask.class).createMarshaller();
-		    m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-		    m.marshal(st, System.out);
-
-		    System.out.println();
-		    System.out.println(st.toString());
 	}
 
 }
