565961d0ec181e2bc78dcfc1f35870ea5b9b5e46
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2014-12-17 14:30:18 +0000

    Mejora de la documentación
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/cliente-afirma/trunk@5431 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-simple/src/test/java/es/gob/afirma/test/simple/MailHyperLinkTest.java
+++ afirma-simple/src/test/java/es/gob/afirma/test/simple/MailHyperLinkTest.java
@@ -9,13 +9,11 @@ import org.junit.Test;
 /** Test de apertura de aplicaci&oacute;n por defecto de correo electr&oacute;nico. */
 public final class MailHyperLinkTest {
 
-	/**
-	 * Abre el cliente de correo para enviar un mail.
-	 * @throws Exception Cuando ocurra un error.
-	 */
+	/** Abre el cliente de correo para enviar un mail.
+	 * @throws Exception Cuando ocurra un error. */
 	@SuppressWarnings("static-method")
 	@Test
-	@Ignore
+	@Ignore // Necesita GUI
 	public void testDesktopMailTo() throws Exception {
 		Desktop.getDesktop().mail(new URI("mailto:tomas.garciameras@atos.net")); //$NON-NLS-1$
 	}
