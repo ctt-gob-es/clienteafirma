ea712a6ce1b636a822f7e81a2e66fb5c61b77502
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2014-12-21 23:05:54 +0000

    Corrección en la prueba de adición de CommitmentTypeIndication en XAdES y mejora de la documentación
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/cliente-afirma/trunk@5443 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-crypto-xades/src/test/java/es/gob/afirma/test/xades/TestCommitmentTypeIndications.java
+++ afirma-crypto-xades/src/test/java/es/gob/afirma/test/xades/TestCommitmentTypeIndications.java
@@ -26,7 +26,7 @@ public final class TestCommitmentTypeIndications {
     static {
 	    p3.setProperty("format", AOSignConstants.SIGN_FORMAT_XADES_ENVELOPED); //$NON-NLS-1$
 	    p3.setProperty("commitmentTypeIndications", "1"); //$NON-NLS-1$ //$NON-NLS-2$
-	    p3.setProperty("commitmentTypeIndication0Identifier", "1.2.840.113549.1.9.16.6.1"); //$NON-NLS-1$ //$NON-NLS-2$
+	    p3.setProperty("commitmentTypeIndication0Identifier", "2"); //$NON-NLS-1$ //$NON-NLS-2$
 	    p3.setProperty("commitmentTypeIndication0Description", "Prueba"); //$NON-NLS-1$ //$NON-NLS-2$
 	    p3.setProperty("commitmentTypeIndication0DocumentationReferences", "http://es.atos.net/es-es/home.html|http://atos.net/en-us/home.html"); //$NON-NLS-1$ //$NON-NLS-2$
 	    p3.setProperty("commitmentTypeIndication0CommitmentTypeQualifiers", "Q1|Q2|Q3"); //$NON-NLS-1$ //$NON-NLS-2$
