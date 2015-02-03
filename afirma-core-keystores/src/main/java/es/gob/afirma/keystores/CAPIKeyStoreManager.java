d4029cc0749958bed7713264ae422a13e8224bda
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2015-02-03 12:24:00 +0000

    Mejora del JavaDoc
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/cliente-afirma/trunk@5498 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-core-keystores/src/main/java/es/gob/afirma/keystores/CAPIKeyStoreManager.java
+++ afirma-core-keystores/src/main/java/es/gob/afirma/keystores/CAPIKeyStoreManager.java
@@ -28,21 +28,17 @@ public final class CAPIKeyStoreManager extends AOKeyStoreManager {
 	private static KeyStore capiKsMy = null;
 
     /** Obtiene la clave privada de un certificado.
-     * @param alias
-     *        Alias del certificado
-     * @param pssCallback
-     *        <i>CallBback</i> para obtener la contrase&ntilde;a del
-     *        certificado que contiene la clave
-     * @return Clave privada del certificado correspondiente al alias
-     * @throws KeyStoreException
-     * 		   Cuando ocurren errores en el tratamiento del almac&eacute;n de claves
-     * @throws NoSuchAlgorithmException
-     * 		   Cuando no se puede identificar el algoritmo para la recuperaci&oacute;n de la clave.
-     * @throws UnrecoverableEntryException
-     * 		   Si la contrase&ntilde;a proporcionada no es v&aacute;lida para obtener la clave privada
-     * @throws es.gob.afirma.core.AOCancelledOperationException
-     * 		   Cuando el usuario cancela el proceso antes de que finalice
-     */
+     * @param alias Alias del certificado.
+     * @param pssCallback <i>CallBback</i> para obtener la contrase&ntilde;a del
+     *                    certificado que contiene la clave.
+     * @return Clave privada del certificado correspondiente al alias.
+     * @throws KeyStoreException Cuando ocurren errores en el tratamiento del almac&eacute;n de claves.
+     * @throws NoSuchAlgorithmException Cuando no se puede identificar el algoritmo para la
+     *                                  recuperaci&oacute;n de la clave.
+     * @throws UnrecoverableEntryException Si la contrase&ntilde;a proporcionada no es v&aacute;lida
+     *                                     para obtener la clave privada
+     * @throws es.gob.afirma.core.AOCancelledOperationException Cuando el usuario cancela el proceso
+     *                                                          antes de que finalice. */
     @Override
 	public KeyStore.PrivateKeyEntry getKeyEntry(final String alias,
     		                                    final PasswordCallback pssCallback) throws KeyStoreException,
