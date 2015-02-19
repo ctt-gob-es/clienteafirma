8dfbfaf8eb48aeb664eaf760c6caeb1566c02702
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2015-02-19 16:41:08 +0000

    Esqueleto de la clase original com.sun.net.ssl.HostNameVerifier, solo para habilitar la compilaci&oacute;n en Android.
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/cliente-afirma/trunk@5542 dcdbb401-323c-49cf-88a0-803402807c85

---
+++ afirma-android/afirma-ui-android/src/com/sun/net/ssl/package-info.java
@@ -0,0 +1,4 @@
+/** Clases <code>com.sun</code> no existentes en Android, vac&iacute;as en implementac&iacute;n,
+ * solo para habilitar la compilaci&oacute;n.
+ * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s */
+package com.sun.net.ssl;
