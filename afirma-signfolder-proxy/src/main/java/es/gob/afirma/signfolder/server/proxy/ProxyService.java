6be28b4ec61b5303fc3d41e21035dc009f58a9ff
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2015-03-11 16:06:38 +0000

    Se eliminan los métodos doGet y doPost del servlet.
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/cliente-afirma/trunk@5574 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-signfolder-proxy/src/main/java/es/gob/afirma/signfolder/server/proxy/ProxyService.java
+++ afirma-signfolder-proxy/src/main/java/es/gob/afirma/signfolder/server/proxy/ProxyService.java
@@ -19,7 +19,6 @@ import java.util.Vector;
 import java.util.logging.Logger;
 
 import javax.activation.DataHandler;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -304,18 +303,6 @@ public final class ProxyService extends HttpServlet {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		t.printStackTrace(new PrintWriter(baos));
 		return new String(baos.toByteArray());
-	}
-
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		super.doGet(req, resp);
-	}
-
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		super.doPost(req, resp);
 	}
 
 	/**
