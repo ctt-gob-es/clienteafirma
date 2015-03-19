82f8538d4d52becc751b2274d02af2afcaea10e2
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2015-03-19 18:47:21 +0000

    Constantes para los valores numéricos
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/cliente-afirma/trunk@5626 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-crypto-certvalidation/src/main/java/es/gob/afirma/cert/certvalidation/ValidationResult.java
+++ afirma-crypto-certvalidation/src/main/java/es/gob/afirma/cert/certvalidation/ValidationResult.java
@@ -22,9 +22,18 @@ public enum ValidationResult {
 	/** Error interno o del servidor. */
 	SERVER_ERROR(7);
 
+	private static final int CODE_VALID = 0;
+	private static final int CODE_CORRUPT = 1;
+	private static final int CODE_CA_NOT_SUPPORTED = 2;
+	private static final int CODE_NOT_YET_VALID = 3;
+	private static final int CODE_EXPIRED = 4;
+	private static final int CODE_REVOKED = 5;
+	private static final int CODE_UNKNOWN = 6;
+	private static final int CODE_SERVER_ERROR = 7;
+
 	private final int resultCode;
 	private ValidationResult(final int code) {
-		if (code < 0 || code > 7) {
+		if (code < CODE_VALID || code > CODE_SERVER_ERROR) {
 			throw new IllegalArgumentException(
 				"El codigo de resultado debe estar comprendido entre 0 y 7: " + code //$NON-NLS-1$
 			);
@@ -47,27 +56,27 @@ public enum ValidationResult {
 	 * @return <code>true</code> si corresponde a un certificado X.509v3 v&aacute;lido, <code>false</code>
 	 *         en caso contrario */
 	public boolean isValid() {
-		return this.resultCode == 0;
+		return this.resultCode == CODE_VALID;
 	}
 
 	@Override
 	public String toString() {
 		switch(this.resultCode) {
-			case 0:
+			case CODE_VALID:
 				return "Valido"; //$NON-NLS-1$
-			case 1:
+			case CODE_CORRUPT:
 				return "No compatible X.509 o corrupto"; //$NON-NLS-1$
-			case 2:
+			case CODE_CA_NOT_SUPPORTED:
 				return "No es de una CA soportada"; //$NON-NLS-1$
-			case 3:
+			case CODE_NOT_YET_VALID:
 				return "Aun no valido"; //$NON-NLS-1$
-			case 4:
+			case CODE_EXPIRED:
 				return "Caducado"; //$NON-NLS-1$
-			case 5:
+			case CODE_REVOKED:
 				return "Revocado"; //$NON-NLS-1$
-			case 6:
+			case CODE_UNKNOWN:
 				return "Desconocido"; //$NON-NLS-1$
-			case 7:
+			case CODE_SERVER_ERROR:
 				return "Error interno o del servidor"; //$NON-NLS-1$
 			default:
 				throw new IllegalStateException(
