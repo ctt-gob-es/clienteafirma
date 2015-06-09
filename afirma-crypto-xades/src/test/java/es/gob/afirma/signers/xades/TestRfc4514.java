38d45bdb8a0f0e3f57b4218f0720267d1def6514
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2015-06-09 19:53:50 +0000

    Pruebas de codificación de nombres X.500 acordes a la RFC 4514.
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/cliente-afirma/trunk@5967 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-crypto-xades/src/test/java/es/gob/afirma/signers/xades/TestRfc4514.java
+++ afirma-crypto-xades/src/test/java/es/gob/afirma/signers/xades/TestRfc4514.java
@@ -6,10 +6,12 @@ import org.junit.Test;
  * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s. */
 public final class TestRfc4514 {
 
+	/** Prueba de adecuaci&oacute;n de la codificaci&oacute;n de nombres X.500 acordes a la RFC 4514. */
+	@SuppressWarnings("static-method")
 	@Test
 	public void testEscapeHelper() {
 		System.out.println(
-			EscapeHelper.escapeLdapName("O=Atos, OU=Consultoria, CN=Tomas")
+			EscapeHelper.escapeLdapName("O=Atos, OU=Consultoria, CN=\"Tomas, el grande\"") //$NON-NLS-1$
 		);
 	}
 
