32c410e853d58679ab46a63d8b719f7d268dd433
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2015-06-22 10:31:02 +0000

    Se agrega un tests para la prueba de selección mediante XPath de nodos con namespace para generar sobre ellos una firma enveloped.
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/cliente-afirma/trunk@6036 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-crypto-xades/src/test/java/es/gob/afirma/signers/xades/TestEnvelopedOnCusomNode.java
+++ afirma-crypto-xades/src/test/java/es/gob/afirma/signers/xades/TestEnvelopedOnCusomNode.java
@@ -21,6 +21,15 @@ public final class TestEnvelopedOnCusomNode {
     private static final String CERT_PASS = "12341234"; //$NON-NLS-1$
     private static final String CERT_ALIAS = "fisico activo prueba"; //$NON-NLS-1$
 
+    private static final Properties p4 = new Properties();
+    static {
+	    p4.setProperty("format", AOSignConstants.SIGN_FORMAT_XADES_ENVELOPED); //$NON-NLS-1$
+	    p4.setProperty("mode", AOSignConstants.SIGN_MODE_IMPLICIT); //$NON-NLS-1$
+	    p4.setProperty("includeOnlySignningCertificate", "true"); //$NON-NLS-1$ //$NON-NLS-2$
+	    p4.setProperty("insertEnvelopedSignatureOnNodeByXPath", "namespace:Facturae"); //$NON-NLS-1$ //$NON-NLS-2$
+	    p4.setProperty("useManifest", "true"); //$NON-NLS-1$ //$NON-NLS-2$
+    }
+
     private static final Properties p3 = new Properties();
     static {
 	    p3.setProperty("format", AOSignConstants.SIGN_FORMAT_XADES_ENVELOPED); //$NON-NLS-1$
@@ -102,6 +111,38 @@ public final class TestEnvelopedOnCusomNode {
 		);
 
         final File f = File.createTempFile("xpathnodenveloped-", ".xml"); //$NON-NLS-1$ //$NON-NLS-2$
+        final java.io.FileOutputStream fos = new java.io.FileOutputStream(f);
+        fos.write(result);
+        fos.flush(); fos.close();
+        System.out.println("Temporal para comprobacion manual: " + f.getAbsolutePath()); //$NON-NLS-1$
+
+	}
+
+	/** Prueba de XAdES Enveloped con inserci&oacute;n de firma en nodo espec&iacute;fico sobre un XML con un namespace.
+	 * @throws Exception En cualquier error. */
+	@SuppressWarnings("static-method")
+	@Test
+	public void TestInsertSignatureOnCustomNodeWithNamespace() throws Exception {
+
+        Logger.getLogger("es.gob.afirma").setLevel(Level.WARNING); //$NON-NLS-1$
+        final PrivateKeyEntry pke;
+        final KeyStore ks = KeyStore.getInstance("PKCS12"); //$NON-NLS-1$
+
+        ks.load(ClassLoader.getSystemResourceAsStream(CERT_PATH), CERT_PASS.toCharArray());
+        pke = (PrivateKeyEntry) ks.getEntry(CERT_ALIAS, new KeyStore.PasswordProtection(CERT_PASS.toCharArray()));
+        final AOSigner signer = new AOXAdESSigner();
+
+        final byte[] data = AOUtil.getDataFromInputStream(ClassLoader.getSystemResourceAsStream("xpathnodenvelopedwithns.xml")); //$NON-NLS-1$
+
+        final byte[] result = signer.sign(
+    		data,
+    		"SHA512withRSA", //$NON-NLS-1$
+    		pke.getPrivateKey(),
+    		pke.getCertificateChain(),
+    		p4
+		);
+
+        final File f = File.createTempFile("xpathnodenvelopedwithns-", ".xml"); //$NON-NLS-1$ //$NON-NLS-2$
         final java.io.FileOutputStream fos = new java.io.FileOutputStream(f);
         fos.write(result);
         fos.flush(); fos.close();
