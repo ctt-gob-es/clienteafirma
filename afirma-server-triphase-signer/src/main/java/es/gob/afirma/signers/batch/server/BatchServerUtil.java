17bef8e1c6fff0bc6c22582094556799bb3bc759
Tomas GARCIA-MERAS CAPOTE <tomas.garciameras@atos.net>
2015-09-29 11:23:48 +0000

    Ahora el proceso por lotes se puede configurar para uso concurrente o en serie
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/cliente-afirma/trunk@6598 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-server-triphase-signer/src/main/java/es/gob/afirma/signers/batch/server/BatchServerUtil.java
+++ afirma-server-triphase-signer/src/main/java/es/gob/afirma/signers/batch/server/BatchServerUtil.java
@@ -7,14 +7,31 @@ import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+import java.util.logging.Logger;
 
 import es.gob.afirma.core.misc.AOUtil;
 import es.gob.afirma.core.misc.Base64;
 import es.gob.afirma.core.signers.TriphaseData;
 import es.gob.afirma.signers.batch.SignBatch;
+import es.gob.afirma.signers.batch.SignBatchConcurrent;
 import es.gob.afirma.signers.batch.SignBatchSerial;
 
 final class BatchServerUtil {
+
+	private static final boolean CONCURRENT;
+	static {
+		final Properties p = new Properties();
+		try {
+			p.load(BatchServerUtil.class.getResourceAsStream("/config.properties")); //$NON-NLS-1$
+		}
+		catch(final Exception e) {
+			Logger.getLogger("es.gob.afirma").severe( //$NON-NLS-1$
+				"No se ha podido cargar la configuracion del proceso por lotes, se usara el modo no concurrente: " + e //$NON-NLS-1$
+			);
+		}
+		CONCURRENT = Boolean.parseBoolean(p.getProperty("concurrentmode", "false")); //$NON-NLS-1$ //$NON-NLS-2$
+	}
 
 	private BatchServerUtil() {
 		// No instanciable
@@ -32,11 +49,13 @@ final class BatchServerUtil {
 				"La definicion de lote no puede ser nula" //$NON-NLS-1$
 			);
 		}
-		return new SignBatchSerial(
-			AOUtil.isBase64(xmlAsUrlSafeBase64.getBytes()) ?
-				Base64.decode(unDoUrlSafe(xmlAsUrlSafeBase64)) :
-					xmlAsUrlSafeBase64.getBytes()
-		);
+		final byte[] xml = AOUtil.isBase64(xmlAsUrlSafeBase64.getBytes()) ?
+			Base64.decode(unDoUrlSafe(xmlAsUrlSafeBase64)) :
+				xmlAsUrlSafeBase64.getBytes();
+		if (CONCURRENT) {
+			return new SignBatchConcurrent(xml);
+		}
+		return new SignBatchSerial(xml);
 	}
 
 	static X509Certificate[] getCertificates(final String certListUrlSafeBase64) throws CertificateException,
