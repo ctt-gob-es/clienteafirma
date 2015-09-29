f9f02ee0025d3a25a8593ae8f658bbf283c78ccc
Carlos Gamuci Millan <carlos.gamuci@atos.net>
2015-09-29 11:13:38 +0000

    Se eliminan paquetes ajenos al proyecto.
    git-svn-id: http://svn-ctt.administracionelectronica.gob.es/svn/clienteafirma/projects/cliente-afirma/trunk@6597 dcdbb401-323c-49cf-88a0-803402807c85

--- afirma-crypto-batch-client/src/main/java/es/fnmtrcm/ceres/batch/filewrapper/package-info.java
+++
@@ -1,4 +0,0 @@
-/** Adaptador del firmador por lotes del Cliente Afirma para funcionar en base a
- * directorios y ficheros.
- * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s. */
-package es.fnmtrcm.ceres.batch.filewrapper;
