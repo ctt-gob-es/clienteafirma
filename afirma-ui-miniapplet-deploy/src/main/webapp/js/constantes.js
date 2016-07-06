86ee2d3a97ddeec5f76d97584953ebc7b32ba1bf
Carlos Gamuci <carlos.gamuci@gmail.com>
2016-07-06 14:43:56 +0200

    Actualización de los ficheros de prueba.

---
+++ afirma-ui-miniapplet-deploy/src/main/webapp/js/constantes.js
@@ -0,0 +1,27 @@
+
+var Constants = {
+		
+	// IMPORTANTE: PARA PRUEBAS, USAR SIEMPRE UNA IP O NOMBRE DE DOMINIO, NUNCA 'LOCALHOST' O '127.0.0.1'
+	// SI NO SE HACE ASI, AUTOFIRMA BLOQUEARA LA FIRMA POR SEGURIDAD
+	
+		/* PRODUCCION
+		
+	URL_BASE_APPLE : "https://valide.redsara.es/firmaMovil",
+
+	URL_BASE_SERVICES : "https://valide.redsara.es/firmaMovil"
+	*/
+
+		/* PREPRODUCCION
+		
+	URL_BASE_APPLE : "http://prevalide.redsara.es/firmaMovil",
+
+	URL_BASE_SERVICES : "http://prevalide.redsara.es/firmaMovil"
+	*/
+
+		/* DESARROLLO */
+		
+		URL_BASE_APPLET : "http://10.1.46.156:8080/afirma-ui-miniapplet-deploy",
+
+		URL_BASE_SERVICES : "http://10.1.46.156:8080"
+	
+};
