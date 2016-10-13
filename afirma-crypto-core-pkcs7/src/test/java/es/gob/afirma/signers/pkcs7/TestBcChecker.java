5fef8e87d8d54631d95e072f7b6097f041966819
albfernandez <infjaf@gmail.com>
2016-10-13 21:43:05 +0200

    Elimina el test de una clase que no se usa

--- afirma-crypto-core-pkcs7/src/test/java/es/gob/afirma/signers/pkcs7/TestBcChecker.java
+++ afirma-crypto-core-pkcs7/src/test/java/es/gob/afirma/signers/pkcs7/TestBcChecker.java
@@ -1,5 +1,6 @@
 package es.gob.afirma.signers.pkcs7;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 /** Pruebas de la comprobaci&oacute;n de la versi&oacute;n de BouncyCastle.
@@ -9,6 +10,7 @@ public final class TestBcChecker {
 	/** Prueba la comprobaci&oacute;n de la versi&oacute;n de BouncyCastle. */
 	@SuppressWarnings("static-method")
 	@Test
+	@Ignore // Ya no se usa BouncyCastle, además BCChecker solo se usa en este test
 	public void testBcCheck() {
 		new BCChecker().checkBouncyCastle();
 	}
