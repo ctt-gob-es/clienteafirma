c22de5ca2dc1573a629a2f0e98a55c5d8c8e7c06
ClawGrip <clawgrip@hotmail.com>
2016-10-28 18:48:05 +0200

    Limpieza de la clase

--- afirma-simple/src/main/java/es/gob/afirma/standalone/HDPIManager.java
+++ afirma-simple/src/main/java/es/gob/afirma/standalone/HDPIManager.java
@@ -7,61 +7,65 @@ import java.util.logging.Logger;
 
 /** Obtiene el modelo del dispositivo y verifica si tiene HDPI.
  * @author Sergio Mart&iacute;nez Rico */
-public class HDPIManager {
+final class HDPIManager {
 
 	private static final Logger LOGGER = Logger.getLogger("es.gob.afirma"); //$NON-NLS-1$
-	
-    static String executeWmicCommand() {
-    	Runtime rt = Runtime.getRuntime();
-    	String[] commands = {"wmic", "csproduct", "get", "name"}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-    	Process proc = null;
-    	String modelName = null;
+
+    private static String executeWmicCommand() {
+
+    	final Runtime rt = Runtime.getRuntime();
+    	final String[] commands = { "wmic", "csproduct", "get", "name" }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+
 		try {
-			proc = rt.exec(commands);
-			BufferedReader stdInput = new BufferedReader(new 
-					InputStreamReader(proc.getInputStream()));
+			final Process proc = rt.exec(commands);
+			final BufferedReader stdInput = new BufferedReader(
+				new InputStreamReader(proc.getInputStream())
+			);
 			String s = null;
-			// Obtenemos la segunda linea que ser el nombre de modelo 
+			// Obtenemos la segunda linea que ser el nombre de modelo
 			// (la primera es el nombre de la columna)
 			while ((s = stdInput.readLine()) != null) {
 			    if(!s.equals("Name") && s.length() > 0) { //$NON-NLS-1$
-			    	modelName = s.trim();
+			    	return s.trim();
 			    }
 			}
-		} catch (IOException e) {
-			LOGGER.warning(
-                       "Error en la ejecucion del comando "+ commands.toString() + ": " + e //$NON-NLS-1$ //$NON-NLS-2$
-                );
 		}
-		return modelName;
+		catch (final IOException e) {
+			LOGGER.warning(
+               "Error en la ejecucion del comando " + commands.toString() + ": " + e //$NON-NLS-1$ //$NON-NLS-2$
+            );
+		}
+		return null;
     }
 
 
-	/**
-	 * @return Devuelve true si se est&aacute; ejecutando sobre un dispositivo HDPI.
-	 */
+	/** Indica si el dispositivo actual tiene pantalla HIDPI.
+	 * @return <code>true</code> si se est&aacute; ejecutando sobre un dispositivo HIDPI,
+	 *         <code>false</code> en caso contrario. */
 	public static boolean isHDPIDevice() {
-		String modelName = executeWmicCommand();
-		// En caso de ser una surface con HDPI se utiliza el Look&Feel Metal. 
+		final String modelName = executeWmicCommand();
+		// En caso de ser una surface con HIDPI se utiliza el Look&Feel Metal.
     	// En caso contrario se utiliza Nimbus.
-    	boolean hdpiDevice = false;
-		BufferedReader br = new BufferedReader(new InputStreamReader(HDPIManager.class.getResourceAsStream("/lookandfeel/hdpi_devices"))); //$NON-NLS-1$
-
+		final BufferedReader br = new BufferedReader(
+			new InputStreamReader(
+				HDPIManager.class.getResourceAsStream("/lookandfeel/hdpi_devices") //$NON-NLS-1$
+			)
+		);
 	    try {
 	    	String line = br.readLine();
 	           while (line != null) {
 	       		if(line.equals(modelName)) {
-	           		hdpiDevice = true;
+	           		return true;
 	           	}
 	       		line = br.readLine();
 	       	}
 	    }
-	    catch(IOException e) { 
+	    catch(final IOException e) {
 	    	LOGGER.warning(
-	                      "No se ha podido abrir el fichero de dispositivos HDPI: " + e //$NON-NLS-1$
-	               );
-	         }
-    	return hdpiDevice;
+                  "No se ha podido abrir el fichero de dispositivos HDPI: " + e //$NON-NLS-1$
+			);
+	    }
+    	return false;
 	}
-    
+
 }
