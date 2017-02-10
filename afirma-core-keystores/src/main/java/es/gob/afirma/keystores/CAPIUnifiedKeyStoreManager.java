c9927d114e737504faff243ef02f0ac79ccd980c
Carlos Gamuci <carlos.gamuci@gmail.com>
2017-02-10 13:07:55 +0100

    Se crea un almacén con un compendio de tarjetas configuradas mediante sus PKCS#11 buscan en el sistema para suplir el acceso al almacén de Windows por encontrarnos en un perfil temporal de usuario.

--- afirma-core-keystores/src/main/java/es/gob/afirma/keystores/CAPIUnifiedKeyStoreManager.java
+++ afirma-core-keystores/src/main/java/es/gob/afirma/keystores/CAPIUnifiedKeyStoreManager.java
@@ -10,6 +10,7 @@
 
 package es.gob.afirma.keystores;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -22,13 +23,17 @@ import es.gob.afirma.core.AOCancelledOperationException;
  * que desde el CSP/MiniDriver de Windows de estas tarjetas. */
 public class CAPIUnifiedKeyStoreManager extends AggregatedKeyStoreManager {
 
+	private static final String SYSTEM_PROPERTY_USERNAME = "user.name"; //$NON-NLS-1$
+	private static final String ENVIRONMENT_PROPERTY_USERPROFILE = "USERPROFILE"; //$NON-NLS-1$
+	private static final String TEMPORARY_PROFILE_NAME = "TEMP"; //$NON-NLS-1$
+
 	private PasswordCallback passwordCallback = null;
 	private Object[] configParams = null;
 
 	/** Indica si el almacen se cargo previamente. */
 	private boolean initialized = false;
 
-	/** Crea un <i>AOKeyStoreManager</i> para acceso a almacenes de claves de Firefox. */
+	/** Crea un <i>AOKeyStoreManager</i> para acceso a almacenes de claves de Windows. */
 	public CAPIUnifiedKeyStoreManager() {
 		setKeyStoreType(AOKeyStore.WINDOWS);
 	}
@@ -49,20 +54,30 @@ public class CAPIUnifiedKeyStoreManager extends AggregatedKeyStoreManager {
 
 		final Object parentComponent = params != null && params.length > 0 ? params[0] : null;
 
-		// Primero anadimos el almacen principal de Windows
-		final AOKeyStoreManager capiKsm = new CAPIKeyStoreManager();
-		try {
-			capiKsm.init(type, store, pssCallBack, params, forceReset);
+		// Si NO nos encontramos en un perfil temporal, cargaremos el almacen de Windows y a continuacion
+		// los almacenes preferentes. En cambio, si estamos en un almacen temporal y, por tanto no tendremos
+		// certificados en el almacen, cargaremos primero los almacenes preferentes y, si no se encuentran,
+		// intentaremos cargar el resto de tarjetas inteligentes que soportemos.
+		AOKeyStoreManager capiKsm = null;
+		final boolean usingTemporaryProfile = isTemporaryProfile();
+		if (usingTemporaryProfile) {
+			LOGGER.info("Detectado perfil temporal. Se omitira la carga del almacen de Windows y se buscaran tarjetas conocidas insertadas"); //$NON-NLS-1$
 		}
-		catch(final Exception e) {
-			LOGGER.severe(
-					"No se ha podido cargar el almacen de Windows, se continuara con los almacenes preferentes: " + e //$NON-NLS-1$
-					);
+		else {
+			// Primero anadimos el almacen principal de Windows
+			capiKsm = new CAPIKeyStoreManager();
+			try {
+				capiKsm.init(type, store, pssCallBack, params, forceReset);
+			}
+			catch(final Exception e) {
+				LOGGER.severe(
+						"No se ha podido cargar el almacen de Windows, se continuara con los almacenes preferentes: " + e //$NON-NLS-1$
+						);
+			}
+			addKeyStoreManager(capiKsm);
 		}
-		addKeyStoreManager(capiKsm);
 
-		// Intentamos ahora agregar los almacenes externos preferentes ajenos a los
-		// dispositivos de seguridad configurados en Firefox haciendo uso del controlador Java
+		// Intentamos ahora agregar los almacenes externos preferentes (DNIe/CERES)
 		boolean preferredKsPresent = false;
 		if (forceReset || !this.initialized) {
 			try {
@@ -74,11 +89,28 @@ public class CAPIUnifiedKeyStoreManager extends AggregatedKeyStoreManager {
 			}
 		}
 
-		// Al comprobar si estaba disponible alguno de los almacenes preferentes (tarjetas)
-		// se habra perdido la conexion con cualquier otra tarjeta configurada en el almacen
-		// de Windows. Asi que, cuando no se encuentran los preferentes, se reinicia el almacen
-		// para recuperar la conexion con cualquier tarjeta conectada y que posiblemente desee usarse
-		if (!preferredKsPresent) {
+		// Si estamos en un perfil temporal, cargaremos las tarjetas que encontremos a
+		// partir de su PKCS#12
+		if (!preferredKsPresent && usingTemporaryProfile) {
+
+			final AOKeyStoreManager scKs = new SmartCardUnifiedKeyStoreManager();
+			try {
+				scKs.init(type, store, pssCallBack, params, forceReset);
+			}
+			catch(final Exception e) {
+				LOGGER.severe(
+						"No se ha podido cargar el almacen unificado de tarjetas: " + e //$NON-NLS-1$
+						);
+			}
+			addKeyStoreManager(scKs);
+		}
+
+		// Si se cargo el almacen de Windows, al comprobar si estaba disponible alguno de
+		// los almacenes preferentes (tarjetas) se habra perdido la conexion con cualquier
+		// otra tarjeta configurada en el almacen de Windows. Asi que, cuando no se
+		// encuentran los preferentes, se reinicia el almacen para recuperar la conexion
+		// con cualquier tarjeta conectada y que posiblemente desee usarse
+		if (capiKsm != null && !preferredKsPresent) {
 			try {
 				capiKsm.refresh();
 			} catch (final IOException e) {
@@ -92,5 +124,31 @@ public class CAPIUnifiedKeyStoreManager extends AggregatedKeyStoreManager {
 	@Override
 	public void refresh() throws IOException {
 		init(AOKeyStore.WINDOWS, null, this.passwordCallback, this.configParams, true);
+	}
+
+	/**
+	 * Comprueba si el perfil de Windows activo se trata de un perfil temporal.<br>
+	 * Una forma mas segura de comprobarlo seria insertar un KeyEntry en el almacen
+	 * de Windows. Si esta operacion fallase con KeyStoreException indicando que se
+	 * trata de un perfil temporal, se trataria de edste tipo de perfil, pero la
+	 * operaci&oacute;n seria demasiado problem&aacute;tica (se necesita una clave
+	 * a guardar, el almacen podr&iacute;a tener contrase&ntilde;a,...).
+	 * @return {@code true} si consideramos que estamos en un perfil temporal,
+	 * {@code false} en caso contrario o si no se ha podido comprobar.
+	 */
+	private static boolean isTemporaryProfile() {
+
+		final String userName = System.getProperty(SYSTEM_PROPERTY_USERNAME);
+		final String profileDirName = System.getenv(ENVIRONMENT_PROPERTY_USERPROFILE);
+		if (userName == null || profileDirName == null) {
+			LOGGER.warning("No se ha podido identificar el nombre de usuario o su directorio de perfil. No se comprobara si se trata de un perfil temporal"); //$NON-NLS-1$
+			return false;
+		}
+		final String profileName = new File(profileDirName).getName();
+
+		return !userName.equals(profileName) &&
+				(TEMPORARY_PROFILE_NAME.equals(profileName) ||
+						profileName.startsWith(TEMPORARY_PROFILE_NAME + '.'));
+
 	}
 }
---
+++ afirma-core-keystores/src/main/java/es/gob/afirma/keystores/SmartCardUnifiedKeyStoreManager.java
@@ -0,0 +1,213 @@
+/* Copyright (C) 2017 [Gobierno de Espana]
+ * This file is part of "Cliente @Firma".
+ * "Cliente @Firma" is free software; you can redistribute it and/or modify it under the terms of:
+ *   - the GNU General Public License as published by the Free Software Foundation;
+ *     either version 2 of the License, or (at your option) any later version.
+ *   - or The European Software License; either version 1.1 or (at your option) any later version.
+ * You may contact the copyright holder at: soporte.afirma5@mpt.es
+ */
+
+package es.gob.afirma.keystores;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.security.auth.callback.PasswordCallback;
+
+import es.gob.afirma.core.AOCancelledOperationException;
+import es.gob.afirma.core.misc.Platform;
+import es.gob.afirma.keystores.callbacks.UIPasswordCallback;
+
+/** Representa a un <i>AOKeyStoreManager</i> que presenta los certificados de las
+ * tarjetas detectadas en encontrado como parte de un &uacute;nico almacen de claves.
+ * El uso que se plantea para este almac&eacute;n es el de servir para sustituir al
+ * almac&eacute;n de Windows cuando se detecte que no se puede acceder al mismo. Este
+ * caso se da en cuentas con un perfil temporal.<br>
+ * Este almac&eacute;n har&aacute; uso de una serie de PKCS#11 conocidos para acceder
+ * a las tarjetas. Tambi&eacute;n se har&aacute; uso del controlador 100% Java de DNIe
+ * y se dar&aacute; prioridad al DNIe sobre el resto de tarjetas. Debido a que la carga
+ * de este controlador puede interferir con el uso de los PKCS#11, si se detecta un DNIe
+ * se ignorar&aacute;n el resto de tarjetas. */
+public class SmartCardUnifiedKeyStoreManager extends AggregatedKeyStoreManager {
+
+	private PasswordCallback passwordCallback = null;
+	private Object[] configParams = null;
+
+	/** Indica si una tarjeta preferente (DNIe) ya se carg&oacute;. */
+	private boolean preferredKsAdded = false;
+
+	/** Indica si el almac&eacute;n se carg&oacute; previamente. */
+	private boolean initialized = false;
+
+	/** Crea un <i>AOKeyStoreManager</i> para acceso a las tarjetas inteligentes
+	 * conocidas que se encuentren en el sistema. */
+	public SmartCardUnifiedKeyStoreManager() {
+		setKeyStoreType(AOKeyStore.KNOWN_SMARTCARDS);
+	}
+
+	/** Inicializa la clase gestora de almacenes de claves. */
+	@Override
+	public final void init(final AOKeyStore type,
+			               final InputStream store,
+			               final PasswordCallback pssCallBack,
+			               final Object[] params,
+			               final boolean forceReset) {
+
+		this.passwordCallback = pssCallBack;
+		this.configParams = params;
+
+		final Object parentComponent = params != null && params.length > 0 ? params[0] : null;
+		boolean excludePreferredKeyStores = false;
+
+		// Vaciamos el listado de almacenes agregados
+		removeAll();
+
+		// Intentamos ahora agregar los almacenes externos preferentes ajenos a los
+		// dispositivos de seguridad configurados en Firefox haciendo uso del controlador Java
+		if (forceReset || !this.initialized) {
+			try {
+				this.preferredKsAdded = KeyStoreUtilities.addPreferredKeyStoreManagers(this, parentComponent);
+			}
+			catch (final AOCancelledOperationException e) {
+				LOGGER.info("Se cancelo el uso del driver Java"); //$NON-NLS-1$
+				// En caso de haber detectado una tarjeta preferente pero haberse cancelado su uso,
+				// permitiremos utilizar el resto de modulos a excepcion de las preferentes
+				this.preferredKsAdded = false;
+				excludePreferredKeyStores = true;
+			}
+		}
+
+		// Si se pudo agregar algun almacen preferente entendemos que se desean usar y no cargamos
+		// el resto de tarjetas. Si no, iniciamos el proceso de todos los PKCS#11.
+		// DNIe nunca se cargara como almacen externo. En el caso de las tarjetas CERES, si no se
+		// pudo cargar a traves del controlador JAVA, se intentara cargar a traves del PKCS#11.
+		// Si se tienen registrados varios PKCS#11 para una misma tarjeta, en cuanto funcione
+		// la carga mediante uno de ellos, se ignora el resto
+		if (!this.preferredKsAdded) {
+
+			for (final KnownSmartCardsPkcs11 sc : KnownSmartCardsPkcs11.values()) {
+
+				LOGGER.warning("Intentamos cargar: " + sc.getDescription());
+
+				// Si no se quieren cargar los PKCS#11 de los almacenes preferidos y
+				// estamos ante uno de ellos, nos lo saltamos
+				if (excludePreferredKeyStores && sc.isPreferred()) {
+					continue;
+				}
+
+				for (final String pkcs11Library : sc.getPkcs11Names()) {
+
+					LOGGER.warning("Probamos con la biblioteca: " + pkcs11Library);
+
+					AOKeyStoreManager ksm;
+					try {
+						ksm = initPkcs11(sc.getDescription(), parentComponent, forceReset, pkcs11Library);
+					}
+					catch (final AOCancelledOperationException ex) {
+						LOGGER.warning("Se cancelo el acceso al almacen externo  '" + sc.getDescription() + "', se continuara con el siguiente: " + ex); //$NON-NLS-1$ //$NON-NLS-2$
+						continue;
+					}
+					catch (final Exception ex) {
+						// En ciertos sistemas Linux fallan las inicializaciones la primera vez por culpa de PC/SC, reintentamos
+						if (Platform.OS.LINUX.equals(Platform.getOS())) {
+							try {
+								ksm = initPkcs11(sc.getDescription(), parentComponent, forceReset, pkcs11Library);
+							}
+							catch (final AOCancelledOperationException exc) {
+								LOGGER.warning("Se cancelo el acceso al almacen externo  '" + sc.getDescription() + "', se continuara con el siguiente: " + exc); //$NON-NLS-1$ //$NON-NLS-2$
+								continue;
+							}
+							catch(final Exception e) {
+								continue;
+							}
+						}
+						continue;
+					}
+					addKeyStoreManager(ksm);
+
+					LOGGER.info("El almacen externo '" + sc.getDescription() + "' ha podido inicializarse, se anadiran sus entradas"); //$NON-NLS-1$ //$NON-NLS-2$
+					break;
+				}
+			}
+		}
+
+		this.initialized = true;
+	}
+
+	private static AOKeyStoreManager initPkcs11(
+			final String descr,
+			final Object parent,
+			final boolean forceReset,
+			final String libName) throws AOKeyStoreManagerException, IOException {
+
+		final AOKeyStoreManager ksm = new AOKeyStoreManager();
+		ksm.init(
+				AOKeyStore.PKCS11,
+				null,
+				new UIPasswordCallback(KeyStoreMessages.getString("SmartCardUnifiedKeyStoreManager.0", descr.toString()), parent), //$NON-NLS-1$
+				new String[] { libName, descr.toString() },
+				forceReset
+				);
+
+		return ksm;
+	}
+
+	@Override
+	public void refresh() throws IOException {
+		init(AOKeyStore.KNOWN_SMARTCARDS, null, this.passwordCallback, this.configParams, true);
+	}
+
+	/**
+	 * Bibliotecas PKCS#11 de las tarjetas inteligentes conocidas.
+	 */
+	private enum KnownSmartCardsPkcs11 {
+		CERES("FNMT-CERES", new String[] {  "%SYSTEMROOT%/System32/FNMT_P11_x64.dll",  //$NON-NLS-1$ //$NON-NLS-2$
+											"%SYSTEMROOT%/System32/FNMT_P11.dll", //$NON-NLS-1$
+											"%SYSTEMROOT%/SysWOW64/FNMT_P11.dll"}, true), //$NON-NLS-1$
+		CARDOS("CardOS", new String[] { "%SYSTEMROOT%/SysWOW64/siecap11.dll",  //$NON-NLS-1$ //$NON-NLS-2$
+										"%SYSTEMROOT%/System32/siecap11.dll" }, false), //$NON-NLS-1$
+		TUI("TUI", new String[] { "%PROGRAMFILES%/Gemalto/IDGo 800 PKCS#11/IDPrimePKCS11.dll", //$NON-NLS-1$ //$NON-NLS-2$
+								  "%PROGRAMFILES(x86)%/Gemalto/IDGo 800 PKCS#11/IDPrimePKCS11.dll" }, false), //$NON-NLS-1$
+		SAFESIGN("SafeSign", new String[] { "%SYSTEMROOT%/SysWOW64/aetpkss1.dll",  //$NON-NLS-1$ //$NON-NLS-2$
+											"%SYSTEMROOT%/System32/aetpkss1.dll" }, false); //$NON-NLS-1$
+
+		private static final String[] ENV_PROPERTIES = new String[] {
+				"%PROGRAMFILES%", "%PROGRAMFILES(x86)%", "%SYSTEMROOT%" }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+
+		private String description;
+		private String[] pkcs11Names;
+		private boolean preferredKs;
+
+		private KnownSmartCardsPkcs11(String description, String[] pkcs11Names, boolean preferredKs) {
+			this.description = description;
+			this.pkcs11Names = pkcs11Names;
+			this.preferredKs = preferredKs;
+		}
+
+		public String getDescription() {
+			return this.description;
+		}
+
+		public String[] getPkcs11Names() {
+			final String[] paths = new String[this.pkcs11Names.length];
+			for (int i = 0; i < this.pkcs11Names.length; i++) {
+				paths[i] = replaceEnvProperties(this.pkcs11Names[i]);
+			}
+			return paths;
+		}
+
+		private static String replaceEnvProperties(String absolutePath) {
+			String path = absolutePath;
+			for (final String envProperty : ENV_PROPERTIES) {
+				if (path.contains(envProperty)) {
+					path = path.replace(envProperty, System.getenv(envProperty.substring(1, envProperty.length() - 1)));
+				}
+			}
+			return path;
+		}
+
+		public boolean isPreferred() {
+			return this.preferredKs;
+		}
+	}
+}
