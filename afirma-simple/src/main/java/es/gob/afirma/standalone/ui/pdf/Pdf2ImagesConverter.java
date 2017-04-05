5bfc9e4cf411e79dcf305d2e1aa71ef1c1b51d92
José Javier Rodríguez Gómez <jjrodgom@users.noreply.github.com>
2017-04-05 09:04:47 +0200

    Cambios desde el trunk

--- afirma-simple/src/main/java/es/gob/afirma/standalone/ui/pdf/Pdf2ImagesConverter.java
+++ afirma-simple/src/main/java/es/gob/afirma/standalone/ui/pdf/Pdf2ImagesConverter.java
@@ -7,20 +7,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.rendering.ImageType;
+import org.apache.pdfbox.rendering.PDFRenderer;
 
 /** Conversor de PDF a conjunto de im&aacute;genes.
  * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s. */
 final class Pdf2ImagesConverter {
 
 	static List<BufferedImage> pdf2Images(final byte[] inPdf) throws IOException {
-		final PDDocument document = PDDocument.load(new ByteArrayInputStream(inPdf));
-		final List<?> pdfPages = document.getDocumentCatalog().getAllPages();
-		final List<BufferedImage> pagesAsImages = new ArrayList<>(pdfPages.size());
-		for (final Object page : pdfPages) {
-           pagesAsImages.add(((PDPage) page).convertToImage());
+		List<BufferedImage> pagesAsImages;
+		try (final PDDocument document = PDDocument.load(new ByteArrayInputStream(inPdf))) {
+			final PDFRenderer pdfRenderer = new PDFRenderer(document);
+			pagesAsImages = new ArrayList<>(document.getNumberOfPages());
+			for (int i = 0; i < document.getNumberOfPages(); i++) {
+				pagesAsImages.add(pdfRenderer.renderImageWithDPI(i, 150, ImageType.RGB));
+			}
         }
-		document.close();
         return pagesAsImages;
 	}
 
