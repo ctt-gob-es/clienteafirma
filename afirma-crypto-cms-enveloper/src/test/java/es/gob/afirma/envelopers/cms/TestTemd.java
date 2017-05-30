7af529b9883a2d1db3153ba6500ab04fb2c74858
ClawGrip <clawgrip@hotmail.com>
2017-05-30 13:20:01 +0200

    Pruebas de sobres con PKCS#11 TEMD

--- afirma-crypto-cms-enveloper/src/test/java/es/gob/afirma/envelopers/cms/TestTemd.java
+++ afirma-crypto-cms-enveloper/src/test/java/es/gob/afirma/envelopers/cms/TestTemd.java
@@ -1,12 +1,17 @@
 package es.gob.afirma.envelopers.cms;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.lang.reflect.Field;
 import java.security.KeyStore;
 import java.security.KeyStore.PrivateKeyEntry;
 import java.security.KeyStoreSpi;
+import java.security.Provider;
+import java.security.Security;
 import java.security.cert.X509Certificate;
 import java.util.Collection;
 import java.util.Enumeration;
+import java.util.logging.Logger;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -21,7 +26,87 @@ import es.gob.afirma.core.misc.AOUtil;
  * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s. */
 public final class TestTemd {
 
-	/** Prueba de creaci&oacute;n de sobre.
+    static final Logger LOGGER = Logger.getLogger("es.gob.afirma"); //$NON-NLS-1$
+
+	/** Prueba de creaci&oacute;n de sobre con PKCS#11 FNMT.
+	 * @throws Exception En cualquier error. */
+	@SuppressWarnings("static-method")
+	@Test
+	public void createEnvelopePkcs11Fnmt() throws Exception {
+
+		final byte[] content ="Hola mundo".getBytes(); //$NON-NLS-1$
+		final File f = new File(
+			TestTemd.class.getResource("/temd-pkcs11/x86_6_1_3/FNMT_P11.dll").toURI() //$NON-NLS-1$
+		);
+
+        final Provider p = new sun.security.pkcs11.SunPKCS11(
+    		new ByteArrayInputStream((
+				"name=pkcs11-win_dll\n" + //$NON-NLS-1$
+				"library=" + f.getAbsolutePath() + "\n" + //$NON-NLS-1$ //$NON-NLS-2$
+				"showInfo=false" //$NON-NLS-1$
+			).getBytes())
+		);
+		Security.addProvider(p);
+		final KeyStore ks = KeyStore.getInstance("PKCS11"); //$NON-NLS-1$
+		ks.load(null, "A111111a".toCharArray()); //$NON-NLS-1$
+
+		String selectedAlias = null;
+		X509Certificate dest = null;
+		final Enumeration<String> aliases = ks.aliases();
+		while (aliases.hasMoreElements()) {
+			final String alias = aliases.nextElement();
+			final X509Certificate tmpCer = (X509Certificate)ks.getCertificate(alias);
+			if (
+				tmpCer.getIssuerX500Principal().toString().contains("O=MDEF") && //$NON-NLS-1$
+				isCipherCert(tmpCer.getKeyUsage())
+			) {
+				selectedAlias = alias;
+				dest = tmpCer;
+			}
+		}
+
+		Assert.assertNotNull(dest);
+		Assert.assertNotNull(selectedAlias);
+
+		// Destinatario del sobre
+
+		final byte[] envelope = new AOCMSEnveloper().createCMSEnvelopedData(
+			content,
+			null,
+			new AOCipherConfig(
+				AOCipherAlgorithm.AES,
+				AOCipherBlockMode.ECB,
+				AOCipherPadding.PKCS5PADDING
+			),
+			new X509Certificate[] { dest },
+			Integer.valueOf(128) // <- CAMBIAR A 256 SI SE TIENE DESACTIVADA LA RESTRICCION EN EL JRE
+		);
+
+		final PrivateKeyEntry pke = (PrivateKeyEntry) ks.getEntry(
+			selectedAlias,
+			new KeyStore.PasswordProtection("A111111a".toCharArray()) //$NON-NLS-1$
+		);
+
+		// Comprobacion de que se esta intentando usar el certificado bueno
+		Assert.assertEquals(
+			"Prueba mal concebida, se esta intentando abrir el sobre con un certificado que no es destinatario", //$NON-NLS-1$
+			dest.getSerialNumber().toString(),
+			((X509Certificate)pke.getCertificate()).getSerialNumber().toString()
+		);
+
+		// Y comprobacion de que abre bien
+		final byte[] recoveredData = new AOCMSEnveloper().recoverData(envelope, pke);
+		Assert.assertEquals(
+			"El contenido desensobrado no coincide con el ensobrado", //$NON-NLS-1$
+			new String(content),
+			new String(recoveredData)
+		);
+
+		System.out.println(new String(recoveredData));
+
+	}
+
+	/** Prueba de creaci&oacute;n de sobre con CAPI.
 	 * @throws Exception En cualquier error. */
 	@SuppressWarnings("static-method")
 	@Test
