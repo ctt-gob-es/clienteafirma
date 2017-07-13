6110ec97c4c618d676093fc20017d6b3031239f1
ClawGrip <clawgrip@hotmail.com>
2017-07-13 22:48:21 +0200

    Mejoras de importancia en la configuración de proxies: * Correcciones en los proxies de tipo SOCKS (había bugs por los nombres de las proiedades). * Ahora, cuando se configura el proxy con variables de entorno, se pueden establecer listas de excepciones (*.nonProxyHosts). * Se implementa, para HTTPS y HTTP, el uso de proxies de sistema en Linux GNOME y Windows 7 y superiores.    - Este proxy se usa siempre y cuando no esté establecido ningún otro de cualquier otra forma. * Trazas de depuración ampliadas.

--- afirma-simple/src/main/java/es/gob/afirma/standalone/ProxyUtil.java
+++ afirma-simple/src/main/java/es/gob/afirma/standalone/ProxyUtil.java
@@ -1,7 +1,13 @@
 package es.gob.afirma.standalone;
 
 import java.net.Authenticator;
+import java.net.InetSocketAddress;
 import java.net.PasswordAuthentication;
+import java.net.Proxy;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 import java.util.logging.Logger;
 
 import es.gob.afirma.standalone.ui.preferences.PreferencesManager;
@@ -17,7 +23,53 @@ public final class ProxyUtil {
 		// No instanciable
 	}
 
-	/** Indica si hay un <i>Proxy</i> establecido en base a variables de entorno
+	private static boolean setDefaultHttpProxy() {
+		return setDefaultProxy("http://www.google.com", "http.proxyHost", "http.proxyPort"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+	}
+
+	private static boolean setDefaultHttpsProxy() {
+		return setDefaultProxy("https://www.google.com", "https.proxyHost", "https.proxyPort"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+	}
+
+	private static boolean setDefaultProxy(final String urlToCkech, final String hostProperty, final String portProperty) {
+		System.setProperty("java.net.useSystemProxies", "true"); //$NON-NLS-1$ //$NON-NLS-2$
+		final List<Proxy> l;
+		final URI uri;
+		try {
+			uri = new URI(urlToCkech);
+		    l = ProxySelector.getDefault().select(uri);
+		}
+		catch (final URISyntaxException e) {
+			LOGGER.warning(
+				"No se ha podido comprobar el proxy por defecto: " + e //$NON-NLS-1$
+			);
+		    return false;
+		}
+		if (l != null) {
+		    for (final Proxy proxy : l) {
+		    	LOGGER.info(
+	    			"Las conexiones para protocolo '" + uri.getScheme() + "' seran de tipo: " + proxy.type() //$NON-NLS-1$ //$NON-NLS-2$
+    			);
+		        final InetSocketAddress addr = (InetSocketAddress) proxy.address();
+
+		        if (addr == null) {
+		            continue;
+		        }
+
+				System.setProperty(hostProperty, addr.getHostName());
+				System.setProperty(portProperty, Integer.toString(addr.getPort()));
+
+		        LOGGER.info(
+					"Se usara el Proxy configurado en sistema para las conexiones de red HTTP: " + addr.getHostName() + ":" + addr.getPort() //$NON-NLS-1$ //$NON-NLS-2$
+				);
+
+				return true;
+		    }
+		}
+		return false;
+	}
+
+	/** Indica si hay un <i>Proxy</i> HTTP establecido en base a variables de entorno
 	 * del sistema operativo.
 	 * @return <code>true</code> si hay un <i>Proxy</i> establecido en base a
 	 * variables de entorno del sistema operativo, <code>false</code> en caso
@@ -25,60 +77,91 @@ public final class ProxyUtil {
 	public static boolean isSystemProxySet() {
     	final String systemProxyHost = System.getenv("http.proxyHost"); //$NON-NLS-1$
     	final String systemProxyPort = System.getenv("http.proxyPort"); //$NON-NLS-1$
-    	return systemProxyHost != null &&
-    		   systemProxyPort != null &&
+    	return systemProxyHost != null    &&
+    		   systemProxyPort != null    &&
     		   !systemProxyPort.isEmpty() &&
     		   systemProxyPort.matches("\\d+"); //$NON-NLS-1$
 	}
 
-	static void configureSystemProxy() {
+	/** Configura el <i>Proxy</i> de AutoFirma en base a las variables de entorno de sistema operativo.
+	 * @return <code>true</code> si hab&iacute;a alguna configuraci&oacute;n en variables de entorno y esta se
+	 *         estableci&oacute;, <code>false</code> en caso contrario. */
+	static boolean configureSystemProxy() {
 
-		final String systemHttpProxyHost  = System.getenv("http.proxyHost"); //$NON-NLS-1$
-    	final String systemHttpProxyPort  = System.getenv("http.proxyPort"); //$NON-NLS-1$
-    	final String systemHttpsProxyHost = System.getenv("https.proxyHost"); //$NON-NLS-1$
-    	final String systemHttpsProxyPort = System.getenv("https.proxyPort"); //$NON-NLS-1$
-    	final String systemFtpProxyHost   = System.getenv("ftp.proxyHost"); //$NON-NLS-1$
-    	final String systemFtpProxyPort   = System.getenv("ftp.proxyPort"); //$NON-NLS-1$
-    	final String systemSocksProxyHost = System.getenv("ftp.proxyHost"); //$NON-NLS-1$
-    	final String systemSocksProxyPort = System.getenv("ftp.proxyPort"); //$NON-NLS-1$
-    	final String systemProxyUsername  = System.getenv("java.proxyUser"); //$NON-NLS-1$
-    	final String systemProxyPassword  = System.getenv("java.proxyPassword"); //$NON-NLS-1$
+		final String systemHttpProxyHost      = System.getenv("http.proxyHost");      //$NON-NLS-1$
+    	final String systemHttpProxyPort      = System.getenv("http.proxyPort");      //$NON-NLS-1$
+    	final String systemHttpNonProxyHosts  = System.getenv("http.nonProxyHosts");  //$NON-NLS-1$
+    	final String systemHttpsProxyHost     = System.getenv("https.proxyHost");     //$NON-NLS-1$
+    	final String systemHttpsProxyPort     = System.getenv("https.proxyPort");     //$NON-NLS-1$
+    	final String systemHttpsNonProxyHosts = System.getenv("https.nonProxyHosts"); //$NON-NLS-1$
+    	final String systemFtpProxyHost       = System.getenv("ftp.proxyHost");       //$NON-NLS-1$
+    	final String systemFtpProxyPort       = System.getenv("ftp.proxyPort");       //$NON-NLS-1$
+    	final String systemFtpNonProxyHosts   = System.getenv("ftp.nonProxyHosts");   //$NON-NLS-1$
+    	final String systemSocksProxyHost     = System.getenv("socks.proxyHost");     //$NON-NLS-1$
+    	final String systemSocksProxyPort     = System.getenv("socks.proxyPort");     //$NON-NLS-1$
+    	final String systemSocksNonProxyHosts = System.getenv("socks.nonProxyHosts"); //$NON-NLS-1$
+    	final String systemProxyUsername      = System.getenv("java.proxyUser");      //$NON-NLS-1$
+    	final String systemProxyPassword      = System.getenv("java.proxyPassword");  //$NON-NLS-1$
 
-    	System.setProperty("http.proxyHost", systemHttpProxyHost); //$NON-NLS-1$
-		System.setProperty("http.proxyPort", systemHttpProxyPort); //$NON-NLS-1$
+    	boolean modified = false;
+
+    	if (systemHttpProxyHost != null && systemHttpProxyPort != null) {
+	    	System.setProperty("http.proxyHost", systemHttpProxyHost); //$NON-NLS-1$
+			System.setProperty("http.proxyPort", systemHttpProxyPort); //$NON-NLS-1$
+			if (systemHttpNonProxyHosts != null) {
+				System.setProperty("http.nonProxyHosts", systemHttpNonProxyHosts); //$NON-NLS-1$
+			}
+			LOGGER.info(
+				"Se usara el Proxy configurado en sistema para las conexiones de red HTTP: " + systemHttpProxyHost + ":" + systemHttpProxyPort //$NON-NLS-1$ //$NON-NLS-2$
+			);
+			modified = true;
+    	}
 
 		if (systemHttpsProxyHost != null && systemHttpsProxyPort != null) {
 			System.setProperty("https.proxyHost", systemHttpsProxyHost); //$NON-NLS-1$
 			System.setProperty("https.proxyPort", systemHttpsProxyPort); //$NON-NLS-1$
-		}
-		else {
-			System.setProperty("https.proxyHost", systemHttpProxyHost); //$NON-NLS-1$
-			System.setProperty("https.proxyPort", systemHttpProxyPort); //$NON-NLS-1$
+			if (systemHttpsNonProxyHosts != null) {
+				System.setProperty("https.nonProxyHosts", systemHttpsNonProxyHosts); //$NON-NLS-1$
+			}
+			LOGGER.info(
+				"Se usara el Proxy configurado en sistema para las conexiones de red HTTPS: " + systemHttpsProxyHost + ":" + systemHttpsProxyPort //$NON-NLS-1$ //$NON-NLS-2$
+			);
+			modified = true;
 		}
 
 		if (systemFtpProxyHost != null && systemFtpProxyPort != null) {
 			System.setProperty("ftp.proxyHost", systemFtpProxyHost); //$NON-NLS-1$
 			System.setProperty("ftp.proxyPort", systemFtpProxyPort); //$NON-NLS-1$
-		}
-		else {
-			System.setProperty("ftp.proxyHost", systemHttpProxyHost); //$NON-NLS-1$
-			System.setProperty("ftp.proxyPort", systemHttpProxyPort); //$NON-NLS-1$
+			if (systemFtpNonProxyHosts != null) {
+				System.setProperty("ftp.nonProxyHosts", systemFtpNonProxyHosts); //$NON-NLS-1$
+			}
+			LOGGER.info(
+				"Se usara el Proxy configurado en sistema para las conexiones de red FTP: " + systemFtpProxyHost + ":" + systemFtpProxyPort //$NON-NLS-1$ //$NON-NLS-2$
+			);
+			modified = true;
 		}
 
 		if (systemSocksProxyHost != null && systemSocksProxyPort != null) {
 			System.setProperty("socks.proxyHost", systemSocksProxyHost); //$NON-NLS-1$
 			System.setProperty("socks.proxyPort", systemSocksProxyPort); //$NON-NLS-1$
-		}
-		else {
-			System.setProperty("socks.proxyHost", systemHttpProxyHost); //$NON-NLS-1$
-			System.setProperty("socks.proxyPort", systemHttpProxyPort); //$NON-NLS-1$
+			if (systemSocksNonProxyHosts != null) {
+				System.setProperty("socks.nonProxyHosts", systemSocksNonProxyHosts); //$NON-NLS-1$
+			}
+			LOGGER.info(
+				"Se usara el Proxy configurado en sistema para las conexiones de red SOCKS: " + systemSocksProxyHost + ":" + systemSocksProxyPort //$NON-NLS-1$ //$NON-NLS-2$
+			);
+			modified = true;
 		}
 
-		setProxyAuthenticator(systemProxyUsername, systemProxyPassword);
+		// Solo ponemos el autenticador si se ha establecido alguna configuracion de Proxy
+		if (modified && systemProxyUsername != null) {
+			setProxyAuthenticator(systemProxyUsername, systemProxyPassword);
+			LOGGER.info(
+				"Se usaran las credeanciales de usuario configuradas en sistema para las conexiones de red con Proxy" //$NON-NLS-1$
+			);
+		}
 
-		LOGGER.info(
-			"Se usara el Proxy configurado en sistema para las conexiones de red: " + systemHttpProxyHost + ":" + systemHttpProxyPort //$NON-NLS-1$ //$NON-NLS-2$
-		);
+		return modified;
 	}
 
 	private static void setProxyAuthenticator(final String proxyUsername, final String proxyPassword) {
@@ -103,12 +186,12 @@ public final class ProxyUtil {
 
     	// Si hay un proxy configurado a nivel de sistema operativo se usa ese y se ignoran
     	// las opciones de AutoFirma
-    	if (isSystemProxySet()) {
-    		configureSystemProxy();
+    	if (configureSystemProxy()) {
+    		return;
     	}
     	// Si no hay configuracion de sistema operativo, se usa la configuracion del
     	// GUI de autofirma
-    	else if (PreferencesManager.getBoolean(PreferencesManager.PREFERENCE_GENERAL_PROXY_SELECTED, false)) {
+    	if (PreferencesManager.getBoolean(PreferencesManager.PREFERENCE_GENERAL_PROXY_SELECTED, false)) {
     		final String proxyHost = PreferencesManager.get(PreferencesManager.PREFERENCE_GENERAL_PROXY_HOST, null);
     		final String proxyPort = PreferencesManager.get(PreferencesManager.PREFERENCE_GENERAL_PROXY_PORT, null);
     		final String proxyUsername = PreferencesManager.get(PreferencesManager.PREFERENCE_GENERAL_PROXY_USERNAME, null);
@@ -122,6 +205,7 @@ public final class ProxyUtil {
 
     			System.setProperty("http.proxyHost", proxyHost); //$NON-NLS-1$
     			System.setProperty("http.proxyPort", proxyPort); //$NON-NLS-1$
+
     			System.setProperty("https.proxyHost", proxyHost); //$NON-NLS-1$
     			System.setProperty("https.proxyPort", proxyPort); //$NON-NLS-1$
 
@@ -161,8 +245,8 @@ public final class ProxyUtil {
 					"Se usara el Proxy por defecto de Java para las conexiones de red: " + javaProxyHost + ":" + javaProxyPort //$NON-NLS-1$ //$NON-NLS-2$
 				);
     		}
-    		else {
-    			LOGGER.info("No se usara Proxy para las conexiones de red"); //$NON-NLS-1$
+    		else if (!setDefaultHttpProxy() && !setDefaultHttpsProxy()) {
+				LOGGER.info("No se usara Proxy para las conexiones de red"); //$NON-NLS-1$
     		}
     	}
     }
@@ -171,12 +255,16 @@ public final class ProxyUtil {
 		LOGGER.info("No se usara Proxy para las conexiones de red"); //$NON-NLS-1$
 		System.clearProperty("http.proxyHost"); //$NON-NLS-1$
 		System.clearProperty("http.proxyPort"); //$NON-NLS-1$
+		System.clearProperty("http.nonProxyHosts"); //$NON-NLS-1$
 		System.clearProperty("https.proxyHost"); //$NON-NLS-1$
 		System.clearProperty("https.proxyPort"); //$NON-NLS-1$
+		System.clearProperty("https.nonProxyHosts"); //$NON-NLS-1$
 		System.clearProperty("ftp.proxHost"); //$NON-NLS-1$
 		System.clearProperty("ftp.proxyPort"); //$NON-NLS-1$
-		System.clearProperty("socksProxyHost"); //$NON-NLS-1$
-		System.clearProperty("socksProxyPort"); //$NON-NLS-1$
+		System.clearProperty("ftp.nonProxyHosts"); //$NON-NLS-1$
+		System.clearProperty("socks.proxyHost"); //$NON-NLS-1$
+		System.clearProperty("socks.proxyPort"); //$NON-NLS-1$
+		System.clearProperty("socks.nonProxyHosts"); //$NON-NLS-1$
 		Authenticator.setDefault(null);
     }
 
