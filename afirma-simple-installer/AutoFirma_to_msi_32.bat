68d57b2f03b506deaa85b00b066766cdea852b1c
Carlos Gamuci <carlos.gamuci@gmail.com>
2018-01-29 00:38:31 +0100

    Actualización del instalador
    - Actualización de los ficheros de instalación con los datos de AutoFirma 1.6.
    - Se crea la estructura de directorios para facilitar la creación de los instaladores.
    - Se incluyen los certificados de CA que por defecto se insertan en el almacén de confianza de las JRE empotradas.
    - Se incluyen ficheros .gitignore

--- afirma-simple-installer/AutoFirma_to_msi_32.bat
+++ afirma-simple-installer/AutoFirma_to_msi_32.bat
@@ -1,4 +1,6 @@
 candle AutoFirma_32_installer.wxs
 light -ext WixUtilExtension AutoFirma_32_installer.wixobj
+move AutoFirma_32_installer.msi AutoFirma32
+cd AutoFirma32
 rename AutoFirma_32_installer.msi AutoFirma_32_v1_6_0_installer.msi
 @pause
--- afirma-simple-installer/AutoFirma_to_msi_64.bat
+++ afirma-simple-installer/AutoFirma_to_msi_64.bat
@@ -1,4 +1,6 @@
 candle AutoFirma_64_installer.wxs
 light -ext WixUtilExtension AutoFirma_64_installer.wixobj
+move AutoFirma_64_installer.msi AutoFirma64
+cd AutoFirma64
 rename AutoFirma_64_installer.msi AutoFirma_64_v1_6_0_installer.msi
 @pause
