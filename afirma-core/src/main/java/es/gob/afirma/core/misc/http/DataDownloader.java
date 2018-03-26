ded31bd170ae3a9032957c1bbea24ced34e69c5a
ClawGrip <clawgrip@hotmail.com>
2018-03-26 21:25:35 +0200

    Se habilita la compresión GZIP de los datos (solo el contenido del parámetro de datos) pasados en la URL de invocaión. Esto permite, en algunos casos, evitar un mensaje de diálogo posterior (socket o servidor intermedio), haciendo más eficiente el proceso.

--- afirma-core/src/main/java/es/gob/afirma/core/misc/http/DataDownloader.java
+++ afirma-core/src/main/java/es/gob/afirma/core/misc/http/DataDownloader.java
@@ -10,12 +10,15 @@
 package es.gob.afirma.core.misc.http;
 
 import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.logging.Logger;
+import java.util.zip.GZIPInputStream;
 
 import es.gob.afirma.core.misc.AOUtil;
 import es.gob.afirma.core.misc.Base64;
@@ -24,8 +27,39 @@ import es.gob.afirma.core.misc.Base64;
  * @author Tom&aacute;s Garc&iacute;a-Mer&aacute;s. */
 public final class DataDownloader {
 
+	private static final int GUNZIP_BUFFER_SIZE = 1024;
+
+	private static final Logger LOGGER = Logger.getLogger("es.gob.afirma"); //$NON-NLS-1$
+
 	private DataDownloader() {
 		// No instanciable
+	}
+
+	/** Descarga datos del origen indicado.
+	 * @param ds Origen de los datos. Este puede ser, y se eval&uacute;a en este orden:
+	 *           <ol>
+	 *            <li>Una URL de tipo HTTP, HTTPS o FTP.</li>
+	 *            <li>Datos en formato Base64.</li>
+	 *            <li>Datos textuales (en este caso de devuelven ellos mismos como binario).</li>
+	 *           </ol>
+	 * @param gzipped Indica si los datos de entrada est&aacute;n comprimidos con GZIP y deben devolverse
+	 *                descomprimidos.
+	 * @return Datos obtenidos del origen indicado.
+	 * @throws IOException Si no se pueden obtener los datos. */
+	public static byte[] downloadData(final String ds, final boolean gzipped) throws IOException {
+		if (gzipped && Base64.isBase64(ds.getBytes())) {
+			LOGGER.info(
+				"Se ha indicado que los datos de entrada estan comrpimidos con GZIP" //$NON-NLS-1$
+			);
+			return downloadData(
+				Base64.encode(
+					gunzipBytes(
+						Base64.decode(ds.replace("_", "/").replace("-", "+")) //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+					)
+				)
+			);
+		}
+		return downloadData(ds);
 	}
 
 	/** Descarga datos del origen indicado.
@@ -76,17 +110,32 @@ public final class DataDownloader {
 
 		// Comprobamos que los datos se pueden tratar como base 64
 		if (Base64.isBase64(dataSource.getBytes())) {
-			Logger.getLogger("es.gob.afirma").info("El contenido a obtener es Base64"); //$NON-NLS-1$ //$NON-NLS-2$
+			LOGGER.info("El contenido a obtener es Base64"); //$NON-NLS-1$
 			try {
 				return Base64.decode(dataSource.replace("_", "/").replace("-", "+")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 			}
 			catch (final Exception e) {
-				Logger.getLogger("es.gob.afirma").warning("Los datos introducidos no se pueden tratar como base 64: " + e); //$NON-NLS-1$ //$NON-NLS-2$
+				LOGGER.warning(
+					"Los datos introducidos no se pueden tratar como Base64: " + e //$NON-NLS-1$
+				);
 			}
 		}
 
 		return dataSource.getBytes();
-
 	}
 
+	private static byte[] gunzipBytes(final byte[] compressedData) throws IOException {
+        try (
+    		final ByteArrayInputStream bis = new ByteArrayInputStream(compressedData);
+    		final ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            final GZIPInputStream gzipIS = new GZIPInputStream(bis)
+		) {
+            final byte[] buffer = new byte[GUNZIP_BUFFER_SIZE];
+            int len;
+            while ((len = gzipIS.read(buffer)) != -1) {
+                bos.write(buffer, 0, len);
+            }
+            return bos.toByteArray();
+        }
+	}
 }
