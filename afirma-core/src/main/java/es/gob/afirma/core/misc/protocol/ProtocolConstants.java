0afa0553bf2218b31e85d7b16db02282857b6887
ClawGrip <clawgrip@hotmail.com>
2018-09-12 19:41:14 +0200

    Cambios de visibilidad

--- afirma-core/src/main/java/es/gob/afirma/core/misc/protocol/ProtocolConstants.java
+++ afirma-core/src/main/java/es/gob/afirma/core/misc/protocol/ProtocolConstants.java
@@ -9,10 +9,11 @@
 
 package es.gob.afirma.core.misc.protocol;
 
-final class ProtocolConstants {
+/** Constantes de elementos de invocaci&oacute;n por protocolo. */
+public final class ProtocolConstants {
 
 	/** Par&aacute;metro que identifica la operaci&oacute;n a realizar. */
-	static final String OPERATION_PARAM = "op"; //$NON-NLS-1$
+	public static final String OPERATION_PARAM = "op"; //$NON-NLS-1$
 
 	private ProtocolConstants() {
 		// No instanciable
