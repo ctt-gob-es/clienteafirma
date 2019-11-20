0e6a41d244699da6a56869b4cc1786b46dbc185f
Carlos Gamuci <carlos.gamuci@gmail.com>
2019-11-20 15:35:05 +0100

    Firma de PDF/A-3B
    - Se agrega a la prueba unitaria de firmas PDF/A la firma de un documento PDF/A-3B.

--- afirma-crypto-pdf/src/test/java/es/gob/afirma/test/pades/TestPDFX.java
+++ afirma-crypto-pdf/src/test/java/es/gob/afirma/test/pades/TestPDFX.java
@@ -20,6 +20,7 @@ public final class TestPDFX {
 	private final static String TEST_FILE_A1B = "PDF-A1B.pdf"; //$NON-NLS-1$
 	private final static String TEST_FILE_A2B = "PDF-A2B.pdf"; //$NON-NLS-1$
 	private final static String TEST_FILE_A2B_PDFTOOLS = "PDF-A2B-PdfTools.pdf"; //$NON-NLS-1$
+	private final static String TEST_FILE_A3B = "PDF-A3B.pdf"; //$NON-NLS-1$
 
 	private final static String TEST_FILE_A1B_SIGNED = "PDFA1BSIGNED.pdf"; //$NON-NLS-1$
 	//private final static String TEST_FILE = "Monitorio_29-02-2016 tipoA.pdf"; //$NON-NLS-1$
@@ -170,7 +171,31 @@ public final class TestPDFX {
         	fos.write(resPdf);
         }
 
-        System.out.println("Fichero de firma de PDF/A-1A guardado en: " + outputFile.getAbsolutePath()); //$NON-NLS-1$
+        System.out.println("Fichero de firma de PDF/A-2B generado con PDFtools guardado en: " + outputFile.getAbsolutePath()); //$NON-NLS-1$
+
+        // Firma de PDF/A-3B
+
+        final byte[] testPdfA3B = AOUtil.getDataFromInputStream(
+        		ClassLoader.getSystemResourceAsStream(TEST_FILE_A3B)
+        		);
+
+        resPdf = signer.sign(
+        		testPdfA3B,
+        		DEFAULT_SIGNATURE_ALGORITHM,
+        		pke.getPrivateKey(),
+        		pke.getCertificateChain(),
+        		extraParams
+        		);
+
+        outputFile = File.createTempFile("PDFA3B_SIGNED_", ".pdf"); //$NON-NLS-1$ //$NON-NLS-2$
+
+        try (
+        		final OutputStream fos = new FileOutputStream(outputFile);
+        		) {
+        	fos.write(resPdf);
+        }
+
+        System.out.println("Fichero de firma de PDF/A-3B guardado en: " + outputFile.getAbsolutePath()); //$NON-NLS-1$
 	}
 
 }
