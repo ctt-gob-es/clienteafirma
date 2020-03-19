1e7efd254a34a5139a47a9adcef06172deea93ac
Carlos Gamuci <carlos.gamuci@gmail.com>
2020-03-19 13:33:32 +0100

    Instalación de certificados en la JRE
    - Se incluye el script para la instalación de los certificados que deben incorporarse a la JRE distribuida con AutoFirma.

---
+++ afirma-simple-installer/certificados/insert_cacerts.sh
@@ -0,0 +1,4 @@
+ruta_jre=../AutoFirma_Packages/AutoFirma.app/Contents/Resources/Home
+for c in *.cer; do
+   $ruta_jre/bin/keytool -importcert —keystore $ruta_jre/lib/security/cacerts -storetype JKS -storepass changeit -alias $c -file $c
+done
