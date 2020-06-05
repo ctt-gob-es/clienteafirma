f13c1d43f3cfb7a48be106a0b427eaf5af8a10ee
Carlos Gamuci <carlos.gamuci@gmail.com>
2020-06-05 11:52:40 +0200

    Nuevo certificado de confianza
    - Se incorpora un nuevo certificado de la FNMT al almacén de confianza del JRE que acompaña a AutoFirma.

--- afirma-simple-installer/certificados/insert_cacerts.sh
+++ afirma-simple-installer/certificados/insert_cacerts.sh
@@ -2,3 +2,6 @@ ruta_jre=../AutoFirma_Packages/AutoFirma.app/Contents/Resources/Home
 for c in *.cer; do
    $ruta_jre/bin/keytool -importcert —keystore $ruta_jre/lib/security/cacerts -storetype JKS -storepass changeit -alias $c -file $c
 done
+for c in *.crt; do
+   $ruta_jre/bin/keytool -importcert —keystore $ruta_jre/lib/security/cacerts -storetype JKS -storepass changeit -alias $c -file $c
+done
