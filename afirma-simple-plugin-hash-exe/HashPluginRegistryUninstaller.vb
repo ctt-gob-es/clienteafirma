4bf6b5ecdbde9c42342405e7f71a86c7358a8cf0
José Montero <josemr2601@gmail.com>
2022-03-23 11:27:04 +0100

    Cambio en la instalación de registros para el plugin de huellas digitales. - Los registros se agregarán a los sistemas Windows al instalar el plugin, no al instalar AutoFirma, como ocurría hasta ahora.

---
+++ afirma-simple-plugin-hash-exe/HashPluginRegistryUninstaller.vb
@@ -0,0 +1,18 @@
+Imports Microsoft.Win32
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Se van a eliminar los registros del plugin hash")
+        Registry.ClassesRoot.DeleteSubKey("*\shell\afirma.hashFile\command")
+        Registry.ClassesRoot.DeleteSubKey("*\shell\afirma.hashFile")
+        Registry.ClassesRoot.DeleteSubKey("Directory\shell\afirma.hashDirectory\command")
+        Registry.ClassesRoot.DeleteSubKey("Directory\shell\afirma.hashDirectory")
+        Registry.ClassesRoot.DeleteSubKey(".hash\shell\afirma.hash\command")
+        Registry.ClassesRoot.DeleteSubKey(".hash\shell\afirma.hash")
+        Registry.ClassesRoot.DeleteSubKey(".hashb64\shell\afirma.hasbh64\command")
+        Registry.ClassesRoot.DeleteSubKey(".hashb64\shell\afirma.hasbh64")
+        Registry.ClassesRoot.DeleteSubKey(".hashfiles\shell\afirma.hashfiles\command")
+        Registry.ClassesRoot.DeleteSubKey(".hashfiles\shell\afirma.hashfiles")
+        Console.WriteLine("Registros eliminados correctamente")
+    End Sub
+End Module
---
+++ afirma-simple-plugin-hash/src/main/resources/es/gob/afirma/plugin/hash/registryInstallation/installHashPluginRegs.bat
@@ -0,0 +1,13 @@
+@if (1==1) @if(1==0) @ELSE
+@echo off&SETLOCAL ENABLEEXTENSIONS
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"||(
+    cscript //E:JScript //nologo "%~f0"
+    @goto :EOF
+)
+echo.Performing admin tasks...
+call "$$PATH_EXE$$" "\"$$INSTALL_DIR$$\""
+@goto :EOF
+@end @ELSE
+ShA=new ActiveXObject("Shell.Application")
+ShA.ShellExecute("$$PATH_EXE$$","\"$$INSTALL_DIR$$\"","","runas",5);
+@end
