934c60b8e36391177d22a74db153b8d26ef56657
Carlos Gamuci <carlos.gamuci@gmail.com>
2022-06-24 16:30:42 +0200

    Corrección validación PDF
    - Se corrige un problema grave de bucle infinito al validar documentos PDF.
    - Se realiza una comprobación adicional durante la validación de PDF para comprobar si el algoritmo de hash de la firma está bien declarado.

--- afirma-crypto-validation/src/main/java/es/gob/afirma/signvalidation/ValidatePdfSignature.java
+++ afirma-crypto-validation/src/main/java/es/gob/afirma/signvalidation/ValidatePdfSignature.java
@@ -41,34 +41,46 @@ public final class ValidatePdfSignature implements SignValider {
 
 	private static final String DEFAULT_PAGES_TO_CHECK_PSA = "10"; //$NON-NLS-1$
 
-	/** Valida una firma PDF (PKCS#7/PAdES).
-	 * De los certificados de firma se revisan &uacute;nicamente las fechas de validez.
+	/**
+	 * Valida una firma PDF (PKCS#7/PAdES). En caso de validar los certificados de firma,
+	 * s&oacute;lo se validar&aacute; el periodo de caducidad.
      * @param sign PDF firmado.
      * @return Validez de la firma.
-     * @throws IOException Si ocurren problemas relacionados con la lectura del documento
-     * o si no se encuentran firmas PDF en el documento. */
+     * @throws IOException Si ocurren problemas relacionados con la lectura del documento, si no se
+     * encuentran firmas PDF en el documento o si se requiere m&aacute;s informacion para la validaci&oacute;n.
+     */
 	@Override
 	public SignValidity validate(final byte[] sign) throws IOException {
 		return validate(sign, true);
 	}
 
-	/** Valida una firma PDF (PKCS#7/PAdES).
-	 * De los certificados de firma se revisan &uacute;nicamente las fechas de validez.
+	/**
+	 * Valida una firma PDF (PKCS#7/PAdES). En caso de validar los certificados de firma,
+	 * s&oacute;lo se validar&aacute; el periodo de caducidad.
      * @param sign PDF firmado.
      * @param checkCertificates Indica si debe comprobarse la caducidad de los certificados de firma.
      * @return Validez de la firma.
-     * @throws IOException Si ocurren problemas relacionados con la lectura del documento
-     * o si no se encuentran firmas PDF en el documento. */
+     * @throws IOException Si ocurren problemas relacionados con la lectura del documento, si no se
+     * encuentran firmas PDF en el documento o si se requiere m&aacute;s informacion para la validaci&oacute;n.
+     */
 	@Override
 	public SignValidity validate(final byte[] sign, final boolean checkCertificates) throws IOException {
-		return validate(sign, checkCertificates);
+		final Properties params = new Properties();
+		params.setProperty(PdfExtraParams.CHECK_CERTIFICATES, Boolean.TRUE.toString());
+		try {
+			return validate(sign, params);
+		} catch (final ConfirmationNeededException e) {
+			throw new IOException("No se dispone de la informacion necesaria para completar la validacion", e); //$NON-NLS-1$
+		}
 	}
 
-	/** Valida una firma PDF (PKCS#7/PAdES).
-	 * De los certificados de firma se revisan &uacute;nicamente las fechas de validez.
+	/** Valida una firma PDF (PKCS#7/PAdES). En caso de validar los certificados de firma,
+	 * s&oacute;lo se validar&aacute; el periodo de caducidad.
      * @param sign PDF firmado.
      * @param params Par&aacute;metros a tener en cuenta para la validaci&oacute;n.
      * @return Validez de la firma.
+     * @throws ConfirmationNeededException Cuando para completar la validaci&oacute;n se necesita
+     * que se proporcione m&aacute;s informaci&oacute;n.
      * @throws IOException Si ocurren problemas relacionados con la lectura del documento
      * o si no se encuentran firmas PDF en el documento. */
 	@Override
@@ -91,12 +103,19 @@ public final class ValidatePdfSignature implements SignValider {
 
 		for (final String name : signNames) {
 
+			// Valimamos la firma
 			final PdfPKCS7 pk = af.verifySignature(name);
+
+			// Comprobamos que el algoritmo de hash este bien declarado, supliendo asi la flexibilidad de iText que permite
+			// cargar firmas que usan algoritmos de firma como algoritmos de hash
+			if (pk.getStrictHashAlgorithm() == null) {
+				return new SignValidity(SIGN_DETAIL_TYPE.KO, VALIDITY_ERROR.ALGORITHM_NOT_SUPPORTED);
+			}
 
     		// Comprobamos si es una firma o un sello
     		final PdfDictionary pdfDictionary = af.getSignatureDictionary(name);
 
-    		// En los sellos no comprobamos el PKCS#1
+    		// Si no es un sello, comprobamos el PKCS#1
     		if (!PDFNAME_ETSI_RFC3161.equals(pdfDictionary.get(PdfName.SUBFILTER)) && !PDFNAME_DOCTIMESTAMP.equals(pdfDictionary.get(PdfName.SUBFILTER))) {
 				try {
 					if (!pk.verify()) {
