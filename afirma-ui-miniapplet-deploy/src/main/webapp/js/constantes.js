e93a58634ff1db831444a66fce4a1db2fa1892fa
Carlos Gamuci <carlos.gamuci@gmail.com>
2022-08-10 12:21:23 +0200

    Página prueba lotes pregenerados JSON

--- afirma-ui-miniapplet-deploy/src/main/webapp/js/constantes.js
+++ afirma-ui-miniapplet-deploy/src/main/webapp/js/constantes.js
@@ -5,7 +5,6 @@ var Constants = {
 	// SI NO SE HACE ASI, AUTOFIRMA BLOQUEARA LA FIRMA POR SEGURIDAD
 	
 	/* DESARROLLO */
-	//URL_BASE_SERVICES : "https://192.168.1.11:8443"
-	//URL_BASE_SERVICES : "http://192.168.43.15:8080"
-	URL_BASE_SERVICES : "https://appprueba:8443"
+	//URL_BASE_SERVICES : "https://appprueba:8443"
+		URL_BASE_SERVICES : window.location.origin
 };
