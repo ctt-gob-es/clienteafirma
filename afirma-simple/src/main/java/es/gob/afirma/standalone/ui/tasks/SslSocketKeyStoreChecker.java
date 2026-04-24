342a4ac3195f41a993e5f8e293dcf444014fc353
Carlos Gamuci <carlos.gamuci@gmail.com>
2026-04-24 09:01:48 +0200

    Refactorización
    - Cambios en nombres de clase para corregir erratas y aclaran la utilidad.

--- afirma-simple/src/main/java/es/gob/afirma/standalone/ui/tasks/SslSocketKeyStoreChecker.java
+++ afirma-simple/src/main/java/es/gob/afirma/standalone/ui/tasks/SslSocketKeyStoreChecker.java
@@ -11,26 +11,26 @@ import es.gob.afirma.standalone.SimpleAfirmaMessages;
 import es.gob.afirma.standalone.SimpleErrorCode;
 import es.gob.afirma.standalone.protocol.SecureSocketUtils;
 
-public class CheckTrustKeyStoreTask extends SwingWorker<Void, Void>{
-	
+public class SslSocketKeyStoreChecker extends SwingWorker<Void, Void>{
+
 	private static final Logger LOGGER = Logger.getLogger("es.gob.afirma"); //$NON-NLS-1$
-	
-	private Object parent;
-	
-	public CheckTrustKeyStoreTask(Object parent) {
+
+	private final Object parent;
+
+	public SslSocketKeyStoreChecker(final Object parent) {
 		this.parent = parent;
 	}
 
     @Override
     public Void doInBackground() {
 
-    	LOGGER.info("Iniciando hilo para la comprobacion de la correcta instalacion del almacen de confianza: "); //$NON-NLS-1$  
+    	LOGGER.info("Iniciando hilo para la comprobacion de la correcta instalacion del almacen de confianza: "); //$NON-NLS-1$
 
     	// Si al intentar obtener el contexto SSL, se recibe alguna excepcion, se mostrar el error
     	try {
     		SecureSocketUtils.getSecureSSLContext();
-    	} catch (Exception e) {
-			LOGGER.severe("No se ha encontrado el almacen de claves de Autofirma"); //$NON-NLS-1$
+    	} catch (final Exception e) {
+			LOGGER.severe("No se ha encontrado el almacen de claves de Autofirma para la comunicacion por sockets"); //$NON-NLS-1$
 			AOUIFactory.showErrorMessage(
 					this.parent,
 					SimpleAfirmaMessages.getString("TrustedKeyStoreError.0"), //$NON-NLS-1$
@@ -38,9 +38,9 @@ public class CheckTrustKeyStoreTask extends SwingWorker<Void, Void>{
 					JOptionPane.ERROR_MESSAGE,
 					new AOException(SimpleErrorCode.Internal.TRUSTSTORE_INCORRECT_INSTALLATION));
     	}
-    	
+
     	return null;
-    	
+
     }
-	
+
 }
