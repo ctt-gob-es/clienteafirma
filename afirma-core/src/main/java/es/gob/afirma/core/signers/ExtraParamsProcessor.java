0c39e221cd44c83482062c4198d7e6114decccc1
José Montero <jose.montero@ricoh.es>
2026-06-16 12:13:31 +0200

    - Modificación del configurador de Firefox con Linux para que acepte la ruta alternativa del archivo profiles.ini utilizada por algunas versiones de Linux. - Corrección parámetro comentario

--- afirma-core/src/main/java/es/gob/afirma/core/signers/ExtraParamsProcessor.java
+++ afirma-core/src/main/java/es/gob/afirma/core/signers/ExtraParamsProcessor.java
@@ -288,7 +288,7 @@ public final class ExtraParamsProcessor {
 	 * @param policyName Nombre de la pol&iacute;tica de firma.
 	 * @param params Conjunto de propiedades en donde hay que establecer los atributos
 	 * de la pol&iacute;tica de firma.
-	 * @throws IncompatiblePolicyException Cuando se ha declarado utilizar un subfiltro
+	 * @throws SignaturePolicyIncompatibilityException Cuando se ha declarado utilizar un subfiltro
 	 * no permitido por la pol&iacute;tica de firma de la AGE.
 	 */
 	private static void setPAdESPolicyAGEAttributes(final String policyName, final Properties params)
